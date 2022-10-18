063c24dd57080e92d705964d11a96086d3c86c42
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-10-18 10:06:18 +0530

    Time: 136 ms (26.03%), Space: 146.4 MB (5.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,25 +1,22 @@
 class Solution {
+    public boolean solveR(int nums[],int sum,int index,Boolean dp[][]){
+        if(index>=nums.length || sum <0) return false;
+        if(sum ==0) return true;
+        if(dp[index][sum]!=null){
+            return dp[index][sum];
+        }
+        return dp[index][sum] =(solveR(nums,sum-nums[index],index+1,dp) || solveR(nums,sum,index+1,dp));
+        }
     public boolean canPartition(int[] nums) {
-       int sum =0;
+        int sum=0;
         for(int i=0;i<nums.length;i++){
-            sum +=nums[i];
+           sum+=nums[i];
         }
-        if(sum%2!=0)
+        if(sum%2==0){
+            Boolean dp[][] = new Boolean[nums.length+1][sum+1];
+            return solveR(nums,sum/2,0,dp);
+        }else{
             return false;
-        sum /=2;
-        boolean dp[][] = new boolean[nums.length+1][sum+1];
-        for(int i=0;i<=nums.length;i++){
-            for(int j=0;j<=sum;j++){
-                if(i==0 || j==0)
-                    dp[i][j] = false;
-                else if(nums[i-1] > j)    
-                    dp[i][j] = dp[i-1][j];
-                else if(nums[i-1]==j) 
-                    dp[i][j] = true;
-                else
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-            }
         }
-        return dp[nums.length][sum];
     }
 }
