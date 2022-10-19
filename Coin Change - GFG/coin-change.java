a631905f81434422de2adf40464495c1236b5de8
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-10-19 21:55:01 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.java
+++ Coin Change - GFG/coin-change.java
@@ -71,10 +71,10 @@ class Solution {
         }
         for(int i=1;i<coins.length+1;i++){
             for(int j=1;j<sum+1;j++){
-             if(coins[i]>j){
+             if(coins[i-1]>j){
                 dp[i][j] = dp[i-1][j];
             }else{
-                dp[i][j] = dp[i-1][j] + dp[i][j-coins[i]];
+                dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
                 }
             }
         }
