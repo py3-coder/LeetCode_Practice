3da51dede6155d41c00d1e026f948cfa75548640
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-10-19 20:16:34 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -27,9 +27,36 @@ class Solution{
         int len[] = new int[n];
         for(int i=0;i<n;i++)
         len[i] =i+1;
+        //Recursion
+        //return cutRod_R(price,len,n,n);
+        //memo
+        //int dp[][] = new int[n+1][n+1];
+        //Arrays.stream(dp).forEach(a->Arrays.fill(a,-1));
+        //return cutRod_M(price,len,n,n,dp);
+        //Tabu
         return cutRod_Tabulation(price,len,n);
         
     }
+    //Recursion 
+    public static int cutRod_R(int price[],int len[],int n,int L){
+    if(L==0 || n==0) return 0;
+    if(len[n-1]>L){
+        return cutRod_R(price,len,n-1,L);
+    }else{
+        return Math.max(cutRod_R(price,len,n-1,L),price[n-1]+cutRod_R(price,len,n,L-len[n-1]));
+        }
+    }
+    // Memoization
+    public static int cutRod_M(int price[],int len[],int n,int L,int dp[][]){
+    if(L==0 || n==0) return 0;
+    if(dp[n][L]!=-1) return dp[n][L];
+    if(len[n-1]>L){
+        return dp[n][L]=cutRod_M(price,len,n-1,L,dp);
+    }else{
+        return dp[n][L]=Math.max(cutRod_M(price,len,n-1,L,dp),price[n-1]+cutRod_M(price,len,n,L-len[n-1],dp));
+        }
+    }
+    //Tabulation
     public static int cutRod_Tabulation(int price[],int len[],int n){
         
         int dp[][] = new int[n+1][n+1];
