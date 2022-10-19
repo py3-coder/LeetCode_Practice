b86e0f78849bfcb25db19f038cb2d8e230696238
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-10-20 00:49:35 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (98.51%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    /*
     public List<Integer> inorderTraversal(TreeNode root) {
        List<Integer> res = new ArrayList<>();
         Stack<TreeNode> st =new Stack<>();
@@ -27,5 +28,40 @@ class Solution {
                 root =root.right;
             }
         return res;
+    }
+    */
+    // Practice -Morris Traversal
+    public List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> inorder = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr!=null){
+            // Case 1: When left is null;
+            if(curr.left==null){
+                // if left is null it means it a root so will go right after printing
+                inorder.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                TreeNode prev= curr.left;
+                // Going to right most node of tree of making thread connection just link it with root
+                while(prev.right!=null && prev.right!=curr){
+                    prev = prev.right;
+                }
+                // Right most left == null then we connected to root;
+                if(prev.right==null){
+                    prev.right = curr;
+                    curr =curr.left;
+                }
+                // When we had already visted thr left it means we are root and we had visted the left part
+                else{
+                    prev.right =null;
+                    // adding root to our list
+                    inorder.add(curr.val);
+                    // moving for right
+                    curr =curr.right;
+                }
+            }
+        }
+        return inorder;
     }
 }
