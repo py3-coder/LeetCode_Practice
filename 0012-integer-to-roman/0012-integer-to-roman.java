5c4237a1f5d76779de1f68fe7f49657edbff6b5a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-10-20 11:35:16 +0530

    Time: 8 ms (80.25%), Space: 41.9 MB (94.66%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,26 +1,25 @@
 class Solution {
     public String intToRoman(int num) {
-        HashMap<Integer,String> map = new HashMap<>();
-        map.put(1,"I");
-        map.put(4,"IV");
-        map.put(5,"V");
-        map.put(9,"IX");
-        map.put(10,"X");
-        map.put(40,"XL");
-        map.put(50,"L");
-        map.put(90,"XC");
-        map.put(100,"C");
-        map.put(400,"CD");
-        map.put(500,"D");
-        map.put(900,"CM");
+        HashMap<Integer,String> map = new LinkedHashMap<>();
         map.put(1000,"M");
+        map.put(900,"CM");
+        map.put(500,"D");
+        map.put(400,"CD");
+        map.put(100,"C");
+        map.put(90,"XC");
+        map.put(50,"L");
+        map.put(40,"XL");
+        map.put(10,"X");
+        map.put(9,"IX");
+        map.put(5,"V");
+        map.put(4,"IV");
+        map.put(1,"I");
         
         StringBuilder roman = new StringBuilder();
-        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        for(int i=0;i<values.length;i++){
-            while(num>=values[i]){
-                num -=values[i];
-                roman.append(map.get(values[i]));
+        for(int ele :map.keySet()){
+            while(num>=ele){
+                num -=ele;
+                roman.append(map.get(ele));
             }
         }
         return roman.toString();
