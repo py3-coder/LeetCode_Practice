fab2a9a0833cc9d986c2fa20d6ad074024d797b1
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-10-22 00:58:00 +0530

    Time: 2 ms (52.30%), Space: 43.7 MB (31.17%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public void bfs(TreeNode root , List<List<Integer>> res,int len){
+    /*public void bfs(TreeNode root , List<List<Integer>> res,int len){
         if(root==null) return;
         if(len>=res.size()){
             res.add(new ArrayList<Integer>());
@@ -26,6 +26,24 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         bfs(root,res,0);
+        return res;
+    }
+    */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null)  return res;
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
+        while(!que.isEmpty()){
+            int level = que.size();
+            List<Integer> ans  = new ArrayList<>();
+            for(int i=0;i<level;i++){
+                if(que.peek().left!=null)que.offer(que.peek().left);
+                if(que.peek().right!=null)que.offer(que.peek().right);
+                ans.add(que.poll().val);           
+            }
+            res.add(ans);
+        } 
         return res;
     }
 }
