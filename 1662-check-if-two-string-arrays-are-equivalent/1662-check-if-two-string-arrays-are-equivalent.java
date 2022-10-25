cc8eedc12d7e2e7f0743b3f08e0218110fd23d8a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-10-25 11:30:01 +0530

    Time: 2 ms (62.71%), Space: 41.6 MB (74.62%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-      String sb1 ="";
+        return String.join("", word1).equals(String.join("", word2));
+      
+        /*String sb1 ="";
       String sb2 ="";
         for(String str : word1){
             sb1+=str;
@@ -8,6 +10,8 @@ class Solution {
         for(String str : word2){
             sb2+=str;
         }
-        return (sb1.equals(sb2))==true?true:false;        
+        return (sb1.equals(sb2))==true?true:false;    
+        */
     }
+    
 }
