39a82b0f3ede458608b538a4a1852b4ac7d5cc09
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-11-22 19:59:08 +0530

    Time: 35 ms (85.64%), Space: 43 MB (69.53%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,25 +1,19 @@
 class Solution {
-    public int numSquares(int n) {
-        List<Integer> squares = new ArrayList<>();
-
-        int cur = 1;
-        // Build the squares array
-        while (Math.pow(cur, 2) <= n) {
-            squares.add((int) Math.pow(cur++, 2));
-        }
-
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-
-        // Base case
-        dp[0] = 0;
-        for (int i = 1; i < n+1; i++ ) {
-            // DP transition
-            for (int j = 0; j < squares.size() && squares.get(j) <= i; j++) {
-                dp[i] = Math.min(dp[i], 1 + dp[i-squares.get(j)]);
+    public int numSquares(int n){
+        int dp[] =new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int min=Integer.MAX_VALUE;
+            for(int j=1;j*j<=i;j++){
+                int rem = i-j*j;
+                if(dp[rem]<min){
+                    min =dp[rem];
+                } 
             }
+            dp[i] = min+1;
         }
         return dp[n];
-
     }
 }
