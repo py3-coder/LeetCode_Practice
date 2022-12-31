b17e09edf8c12c1436a785ed7acadf113a49d133
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-12-31 16:46:04 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (68.43%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,15 +1,12 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                return i;
-            }if(nums[i]>target){
-                return i;
-            }
+    public int searchInsert(int[] A, int target) {
+        int low = 0, high = A.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid-1;
+            else low = mid+1;
         }
-        if(nums[nums.length-1]<target){
-            return nums.length;
-        }
-        return 0;
+        return low;
     }
 }
