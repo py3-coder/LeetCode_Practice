9953ef45c91aff8e2cbf2fe7dd57351d58e91a5b
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-01-01 17:50:16 +0530

    Time: 1 ms (95.36%), Space: 39.7 MB (99.38%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -4,9 +4,9 @@ class Solution {
     if (words.length != pattern.length())
         return false;
     Map index = new HashMap();
-    for (Integer i=0; i<words.length; ++i)
-        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+    for (int i=0; i<words.length; ++i)
+        if (!Objects.equals(index.put(pattern.charAt(i), i),index.put(words[i], i)))
             return false;
     return true;
-}
+    }
 }
