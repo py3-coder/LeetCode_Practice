7e3125b967d489d9963898f97ec5885bb6fecb57
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-01-08 19:22:54 +0530

    Time: 22 ms (88.57%), Space: 42.1 MB (85.36%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxPoints(int[][] arr) {
+     //Base cases:
+        if(arr==null) return 0;
+        if(arr.length<=2) return arr.length;
+        int max=1;
+        for(int i=0;i<arr.length;i++){
+            HashMap<Double,Integer> map = new HashMap<>();
+            for(int j=0;j<arr.length;j++){
+                if(i==j) continue;
+                double slope=(arr[j][0]-arr[i][0])!= 0?(arr[j][1] -arr[i][1]) * 1.0 /(arr[j][0] - arr[i][0]) : Integer.MAX_VALUE;
+                map.put(slope, map.getOrDefault(slope, 1) + 1);
+                max = Math.max(max, map.get(slope));
+            }
+        }
+        return max;
+    }
+}
