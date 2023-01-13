2b58721c32414f7fef84847eb98ed9ac842deb7b
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-01-14 00:47:54 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (53.71%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root==null) return new TreeNode(val);
+        TreeNode curr = root;
+        while(true){
+            if(curr.val<val){
+                if(curr.right!=null){
+                    curr = curr.right;
+                }else{
+                    curr.right = new TreeNode(val);
+                    break;
+                }
+            }else{
+                if(curr.left!=null){
+                    curr =curr.left;
+                }else{
+                    curr.left =new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
