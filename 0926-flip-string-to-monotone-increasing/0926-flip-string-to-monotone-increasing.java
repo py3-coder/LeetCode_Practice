aa747c4ef1984efafa007efd87f2ffac9c356d7b
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-01-17 21:10:29 +0530

    Time: 10 ms (84.07%), Space: 43.1 MB (65.76%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -6,6 +6,12 @@ class Solution {
         
         //iterate over loop
         for(int i=0;i<s.length();i++){
+            // As we counter 0 will increase countflip +1 and update 
+            // countflip = min(countflip+1 , countOne)
+            //  at any 0
+            //      --> convert that zero to One
+            //     ---> count no. of 1s upto now
+            //        take min of both...
             if(s.charAt(i)=='0') countflip = Math.min(countflip+1,countOne);
             else{
                 countOne+=1;
@@ -14,3 +20,6 @@ class Solution {
         return countflip;
     }
 }
+// Complexity 
+// TC - O(n);
+// SC -O(1);
