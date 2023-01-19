180205e027c4265257747fb9c8b468b5d296c204
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-01-20 00:31:09 +0530

    Time: 10 ms (5.43%), Space: 42.6 MB (59.20%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int inorder(TreeNode root,List<Integer> res,int k){
+        Stack<TreeNode> st = new Stack<>();
+        while(!st.isEmpty()|| root!=null){
+            while(root!=null){
+                st.push(root);
+                root =root.left;
+            }
+            root =st.pop();
+            res.add(root.val);
+            root =root.right;
+        }
+        System.out.println(res);
+        return  res.get(k-1);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> res =new ArrayList<>();
+        int ans = inorder(root,res,k);
+        return ans;
+    }
+}
