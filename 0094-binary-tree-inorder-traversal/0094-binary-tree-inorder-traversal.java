6aa9f7e3b102bc0cd42925698fbbb89b2d174389
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-01-20 00:56:51 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (85.79%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,54 +14,15 @@
  * }
  */
 class Solution {
-    /*
-    public List<Integer> inorderTraversal(TreeNode root) {
-       List<Integer> res = new ArrayList<>();
-        Stack<TreeNode> st =new Stack<>();
-        while(!st.isEmpty() || root!=null){
-                while(root!=null){
-                    st.push(root);
-                    root =root.left;
-                }
-                root =st.pop();
-                res.add(root.val);
-                root =root.right;
-            }
+    public List<Integer> inorder(TreeNode root,List<Integer> res){
+        if(root==null) return res;
+        inorder(root.left,res);
+        res.add(root.val);
+        inorder(root.right,res);
         return res;
     }
-    */
-    // Practice -Morris Traversal
-    public List<Integer> inorderTraversal(TreeNode root){
-        List<Integer> inorder = new ArrayList<>();
-        TreeNode curr = root;
-        while(curr!=null){
-            // Case 1: When left is null;
-            if(curr.left==null){
-                // if left is null it means it a root so will go right after printing
-                inorder.add(curr.val);
-                curr = curr.right;
-            }
-            else{
-                TreeNode prev= curr.left;
-                // Going to right most node of tree of making thread connection just link it with root
-                while(prev.right!=null && prev.right!=curr){
-                    prev = prev.right;
-                }
-                // Right most left == null then we connected to root;
-                if(prev.right==null){
-                    prev.right = curr;
-                    curr =curr.left;
-                }
-                // When we had already visted thr left it means we are root and we had visted the left part
-                else{
-                    prev.right =null;
-                    // adding root to our list
-                    inorder.add(curr.val);
-                    // moving for right
-                    curr =curr.right;
-                }
-            }
-        }
-        return inorder;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        return inorder(root,res);
     }
 }
