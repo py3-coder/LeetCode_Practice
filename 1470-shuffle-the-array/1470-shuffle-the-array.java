c37236c5bd87711881e1dd4156234595781d561a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-02-06 17:25:28 +0530

    Time: 0 ms (100.00%), Space: 42 MB (98.38%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,15 +1,10 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        ArrayList<Integer> lis = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            lis.add(nums[i]);
-            lis.add(nums[n+i]);
-        }
-        int[] arr = new int[nums.length];
-        int k=0;
-        for(int a : lis){
-            arr[k++] = a; 
-        }
-       return arr; 
+    int[] res = new int[2*n];
+    for(int i = 0; i < n; i++){
+       res[2 * i] = nums[i];
+       res[2 * i + 1] = nums[n+i];
+    }
+    return res;
     }
 }
