a694c87a4e02b6e63e8aef2dd70f191991512a1a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-03-11 23:43:54 +0530

    Time: 0 ms (100.00%), Space: 42 MB (73.24%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int start =0;
+        int end =nums.length-1;
+        while(start<end){
+            int mid = start +(end-start)/2;
+            
+            if(nums[mid]>nums[end]){
+                //Unsorted side 
+                start = mid+1;
+            }else if(nums[mid]<nums[end]){
+                end = mid;
+            }else if(nums[mid]==nums[end]){
+                end--;
+            }
+        }
+        return nums[start];
+    }
+}
