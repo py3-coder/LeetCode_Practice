41d565283b4a838fff73b76da11708362896ab72
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-03-12 19:50:57 +0530

    Time: 0 ms (100.00%), Space: 58.6 MB (85.04%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -3,18 +3,17 @@ class Solution {
         int n = arr.length;
         int start = 0;
         int end =n-1;
-        while(start<=end){
+        while(start<end){
             int mid = start +(end- start)/2;
             int next = (mid+1)%n;
-            int prev = (mid+n-1)%n;
-            if(arr[mid]>arr[next] && arr[mid]>arr[prev]){
-                return mid;
-            }else if(arr[mid]<arr[prev]){
-                end= mid-1;
-            }else if(arr[mid]<arr[next]){
-                start= mid+1;
+            if(arr[mid]<arr[next]){
+                /// next is greater means the maxi must be in right side;
+                start = mid+1;
+            }else if(arr[mid]>arr[next]){
+                // next is lesser means might the mid or any one in left side should be greater
+                end= mid;
             }
         }
-        return 1;
+        return start;
     }
 }
