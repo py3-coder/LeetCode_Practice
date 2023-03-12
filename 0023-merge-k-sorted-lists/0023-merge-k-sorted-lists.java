fcc0384419be3c9c4a211dd843efe7d411d20de7
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-03-12 23:00:26 +0530

    Time: 3 ms (84.23%), Space: 44.5 MB (27.41%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,32 +8,32 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode merge2(ListNode l1,ListNode l2){
-        ListNode fake = new ListNode(Integer.MIN_VALUE);
-        ListNode tail = fake;
-        while(l1!=null && l2!=null){
-            if(l1.val<l2.val){
-                tail.next =l1;
-                l1 =l1.next;
-            }
-            else{
-                tail.next =l2;
-                l2 =l2.next;
-            }
-            tail =tail.next;
-        }
-        tail.next = (l1!=null)?l1:l2;
-        
-        return fake.next;
+public class Solution {
+    public static ListNode mergeKLists(ListNode[] lists){
+    return partion(lists,0,lists.length-1);
+}
+
+public static ListNode partion(ListNode[] lists,int s,int e){
+    if(s==e)  return lists[s];
+    if(s<e){
+        int q=(s+e)/2;
+        ListNode l1=partion(lists,s,q);
+        ListNode l2=partion(lists,q+1,e);
+        return merge(l1,l2);
+    }else
+        return null;
+}
+
+//This function is from Merge Two Sorted Lists.
+public static ListNode merge(ListNode l1,ListNode l2){
+    if(l1==null) return l2;
+    if(l2==null) return l1;
+    if(l1.val<l2.val){
+        l1.next=merge(l1.next,l2);
+        return l1;
+    }else{
+        l2.next=merge(l1,l2.next);
+        return l2;
     }
-    public ListNode mergeKLists(ListNode[] lists) {
-        if(lists == null || lists.length==0) return null;
-        ListNode dummy= new ListNode(Integer.MIN_VALUE);
-        ListNode m1 =dummy;
-        for(int i=0;i<lists.length;i++){
-             m1 = merge2(m1,lists[i]);
-        }
-        return dummy.next;
-    }
+}
 }
