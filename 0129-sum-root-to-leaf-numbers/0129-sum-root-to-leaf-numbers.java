3019b5d21807dbcca38c18c81cd1968dad707a2f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-03-14 21:10:05 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (21.63%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+	return sum(root, 0);
+    }
+
+    public int sum(TreeNode n, int s){
+        if (n == null) return 0;
+        if (n.right == null && n.left == null) return s*10 + n.val;
+        return sum(n.left, s*10 + n.val) + sum(n.right, s*10 + n.val);
+    }
+}
