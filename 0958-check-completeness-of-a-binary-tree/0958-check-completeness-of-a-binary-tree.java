0e7a125ae7f926cf2e60bb1d274ffa983e053e91
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-03-15 17:15:37 +0530

    Time: 1 ms (83.31%), Space: 42.5 MB (13.75%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+        public boolean isCompleteTree(TreeNode root) {
+        boolean end = false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if(cur == null) end = true;
+            else{
+                if(end) return false;
+                queue.add(cur.left);
+                queue.add(cur.right);
+            }
+        }
+        return true;
+    }
+}
