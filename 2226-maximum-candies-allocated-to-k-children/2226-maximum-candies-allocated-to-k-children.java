b2b5e6b2413193a87afd9da13bf5e89d85ccf042
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-03-17 22:43:24 +0530

    Time: 48 ms (57.94%), Space: 58.4 MB (49.21%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,28 +1,29 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-        long sum = 0;
-        for(int i = 0 ; i < candies.length; i++){
-            sum+= candies[i];
+        long sum =0;
+        for(int val : candies){
+            sum +=val;
         }
         int res =0;
-        int start = 1;
-        int end= (int)(sum/k);
-       
-        while(start <= end){
+        int start =1;
+        int end=(int)(sum/k);
+        while(start<=end){
             int mid = start +(end-start)/2;
-            long count = 0;
-            
-            for(int i = 0 ; i < candies.length; i++){
-                count+= candies[i]/mid;
-            }
-            if(count >= k){
+            if(isPossible(candies,k,mid)==true){
                 res = mid;
                 start = mid+1;
-            }
-            else{
+            }else{
                 end = mid-1;
             }
         }
         return res;
+    }
+    public static boolean isPossible(int[] arr , long k,long mid){
+        long count =0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>=mid)
+                count +=arr[i]/mid;
+        }
+        return count>=k;
     }
 }
