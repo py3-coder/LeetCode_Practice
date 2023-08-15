4a4ed99b95807c5f3534cab034faf9245876bb12
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-15 17:55:18 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (91.74%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,25 +1,26 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
+    public int search(int[] arr, int target) {
+        int start =0;
+        int end = arr.length-1;
+        int res =-1;
         while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target){
+            int mid  = start+(end-start)/2;
+            if(arr[mid]==target){
                 return mid;
-            }
-            if (nums[start] <= nums[mid]){
-                 if (target < nums[mid] && target >= nums[start]) 
-                    end = mid - 1;
-                 else
-                    start = mid + 1;
-            } 
-            if (nums[mid] <= nums[end]){
-                if (target > nums[mid] && target <= nums[end])
-                    start = mid + 1;
-                 else
-                    end = mid - 1;
+            }else if(arr[start]<=arr[mid]){
+                if(target<arr[mid] && target>=arr[start]){
+                    end = mid-1;
+                }else{
+                    start =mid+1;
+                }
+            }else if(arr[mid]<=arr[end]){
+                if(target>arr[mid] && target<=arr[end]){
+                    start = mid+1;
+                }else{
+                    end =mid-1;
+                }
             }
         }
-        return -1;
+        return res;
     }
 }
