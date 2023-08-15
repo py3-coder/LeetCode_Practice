03bb28893431e552c9d83ae72d93d3b89865733a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-15 17:30:03 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (86.82%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-       int firstPos =  firstPostion(nums,target);
-       int lastPos = lastPostion(nums,target);
-        
-        int[] res =new int[2];
-        res[0] = firstPos;
-        res[1] =lastPos;
-        return res;
+        int arr[] = new int[2];
+        arr[0]=firstPostion(nums,target);
+        arr[1]=lastPosition(nums,target);
+        return arr;
     }
-    public static  int firstPostion(int[] arr , int target){
+    public static int firstPostion(int arr[],int target){
         int start =0;
-        int end =arr.length-1;
+        int end = arr.length-1;
         int res =-1;
         while(start<=end){
-            int mid = start +(end-start)/2;
-            if(target==arr[mid]){
+            int mid  =start+(end-start)/2;
+            if(target ==arr[mid]){
                 res = mid;
                 end = mid-1;
             }else if(target<arr[mid]){
                 end = mid-1;
-            }else if(target>arr[mid]){
-                start = mid+1;
+            }else{
+                start =mid+1;
             }
         }
         return res;
     }
-    public static  int lastPostion(int[] arr , int target){
+    
+    public static int lastPosition(int arr[],int target){
         int start =0;
         int end =arr.length-1;
         int res =-1;
         while(start<=end){
-            int mid = start +(end-start)/2;
+            int mid = start+(end-start)/2;
             if(target==arr[mid]){
-                res = mid;
-                start= mid+1;
+                res=mid;
+                start=mid+1;
             }else if(target<arr[mid]){
                 end = mid-1;
-            }else if(target>arr[mid]){
-                start = mid+1;
+            }else{
+                start= mid+1;
             }
         }
         return res;
