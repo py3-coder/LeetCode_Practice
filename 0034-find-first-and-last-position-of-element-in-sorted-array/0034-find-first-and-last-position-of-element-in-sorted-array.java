2241e573716ce39c782897be1139cc9baf4ca0c0
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-15 17:33:33 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (93.67%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -11,13 +11,13 @@ class Solution {
         int res =-1;
         while(start<=end){
             int mid  =start+(end-start)/2;
-            if(target ==arr[mid]){
+            if(target<arr[mid]){
+                end = mid-1;
+            }else if(target>arr[mid]){
+                start =mid+1;
+            }else{
                 res = mid;
                 end = mid-1;
-            }else if(target<arr[mid]){
-                end = mid-1;
-            }else{
-                start =mid+1;
             }
         }
         return res;
@@ -29,13 +29,13 @@ class Solution {
         int res =-1;
         while(start<=end){
             int mid = start+(end-start)/2;
-            if(target==arr[mid]){
-                res=mid;
-                start=mid+1;
-            }else if(target<arr[mid]){
+           if(target<arr[mid]){
                 end = mid-1;
-            }else{
+            }else if(target>arr[mid]){
                 start= mid+1;
+            }else{
+               res=mid;
+                start=mid+1; 
             }
         }
         return res;
