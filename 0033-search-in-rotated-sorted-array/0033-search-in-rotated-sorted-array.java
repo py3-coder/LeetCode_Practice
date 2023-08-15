e2d50c9710f9da89bad2f793dc9eaa6c22192979
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-15 18:02:31 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (30.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,19 +5,19 @@ class Solution {
         int res =-1;
         while(start<=end){
             int mid  = start+(end-start)/2;
-            if(arr[mid]==target){
+            if(target==arr[mid]){
                 return mid;
             }else if(arr[start]<=arr[mid]){
-                if(target<arr[mid] && target>=arr[start]){
-                    end = mid-1;
+                if(target<arr[mid] && target >=arr[start]){
+                    end =mid-1;
                 }else{
-                    start =mid+1;
+                    start=mid+1;
                 }
             }else if(arr[mid]<=arr[end]){
                 if(target>arr[mid] && target<=arr[end]){
-                    start = mid+1;
+                    start =mid+1;
                 }else{
-                    end =mid-1;
+                    end = mid-1;
                 }
             }
         }
