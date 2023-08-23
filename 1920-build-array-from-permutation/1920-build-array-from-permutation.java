e01a7ed7229e23c7886a7411f22127a52dea1090
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-23 15:10:57 +0530

    Time: 1 ms (88.69%), Space: 44.1 MB (84.65%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int len = nums.length;
+        int res[] = new int[len];
+        for(int i=0;i<len;i++){
+            res[i] =nums[nums[i]];
+        }
+        return res;
+    }
+}
