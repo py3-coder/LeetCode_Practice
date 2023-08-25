3a51f62364d57397eefe69bf423b92325a20a204
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-25 09:52:08 +0530

    Time: 1 ms (77.94%), Space: 40.4 MB (71.82%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.java
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.java
@@ -2,10 +2,11 @@ class Solution {
     public boolean findSubarrays(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for(int i=1;i<nums.length;i++){
-            if(!set.add(nums[i]+nums[i-1])){
-                 return true;
-            }   
+            if(set.contains(nums[i]+nums[i-1])){
+                return true;
+            }
+            set.add(nums[i]+nums[i-1]);
         }
-       return false; 
-    }
+       return false;
+}
 }
