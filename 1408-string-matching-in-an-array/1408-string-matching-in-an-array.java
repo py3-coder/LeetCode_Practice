b17542cb133abe044833da7c1a295bfe2a059c20
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-25 22:56:49 +0530

    Time: 7 ms (10.49%), Space: 41.2 MB (73.71%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,15 +1,67 @@
 class Solution {
-    public List<String> stringMatching(String[] words) {
-        Set<String> set =new HashSet<>();
-        ArrayList<String> lis = new ArrayList<>();
-        for(String str:words){
-            for(String val:words){
-                if(val.contains(str)==true && val!=str ){
-                    set.add(str);
-                }
-            }
-        }
-        lis.addAll(set);
-        return lis;
-    }
+	public List<String> stringMatching(String[] words) {
+		List<String> result = new ArrayList<>();
+
+		Arrays.sort(words, new java.util.Comparator<String>(){
+			@Override
+			public int compare(String s1, String s2){
+				return s1.length() - s2.length();
+			}
+		});
+
+		for (int i = 0; i < words.length; i++) {
+			int mas[] = longPrefSub(words[i]);
+			for (int j = i + 1; j < words.length; j++) {
+				if (KMP(words[i], words[j], mas) == true) {
+					result.add(words[i]);
+					break;
+				}
+			}    
+		}
+
+		return result;
+	}
+
+	public boolean KMP(String sub, String basic, int[] mas) {
+		int j = 0;
+
+		for (int i = 0; i < basic.length(); i++) {
+			char ch = basic.charAt(i);
+			if (ch != sub.charAt(j)) {
+				while (j > 0 && ch != sub.charAt(j))
+					j = mas[j - 1];
+			}
+
+			if (ch == sub.charAt(j))
+				j++;
+
+			if (j == sub.length())
+				return true;
+		}
+
+		return false;
+	}
+
+
+	public int[] longPrefSub(String word) {
+		int[] mas = new int[word.length()];
+		int j = 0;
+
+		for (int i = 1; i < word.length(); i++) {
+			char a = word.charAt(i);
+			char b = word.charAt(j);
+			if (a != b) {
+				while (j > 0 && a != b) {
+					j = mas[j - 1];
+					b = word.charAt(j);
+				}
+			}
+
+			if (a == b) {
+				mas[i] = ++j;
+			}
+		}
+
+		return mas;
+	}
 }
