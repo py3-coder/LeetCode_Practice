bc4342db004b63fc187f13066a51bc0bd05e55ef
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-26 15:29:23 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (43.76%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,13 +1,21 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // Iterative Approch 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+        // Lets Apply for Binary Search 
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int left=0;
+        int right=m*n-1;
+        while(left<=right){
+            int mid =left+(right-left)/2;
+            if(matrix[mid/m][mid%m]==target){
+                return true;
+            }
+            if(matrix[mid/m][mid%m]<target){
+                left =mid+1;
+            }else{
+                right=mid-1;
             }
         }
-        return false;
+        return false;   
     }
 }
