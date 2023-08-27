fec60aa5df07a0ab4f606884343d0f52de1ecde4
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-27 12:02:12 +0530

    Time: 4 ms (84.07%), Space: 56 MB (16.92%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxArea(int[] height) {
-        int max_area=0;
-        int left =0 ,right=height.length-1;
+        int maxarea=0;
+        int left=0,right=height.length-1;
         while(left<right){
-            max_area = Math.max(max_area,Math.min(height[left],height[right])*(right-left));
-            if(height[left]>height[right]){
+            maxarea = Math.max(maxarea,Math.min(height[left],height[right])*(right-left));
+            if(height[left]<height[right]){
+                left++;
+            }else{
                 right--;
             }
-            else{
-                left++;
-            }
         }
-        return max_area;
+        return maxarea;
     }
 }
