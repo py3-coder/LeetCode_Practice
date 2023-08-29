6a46207aea6ce1d907781e744234bf4ae02df877
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-29 18:54:56 +0530

    Time: 2 ms (82.01%), Space: 43.7 MB (66.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n= nums.length;
-        int res[] = new int[2];
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    res[0]=i;
-                    res[1]=j;
-                    return res;
-                }
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0;i<nums.length;i++){
+            int b = nums[i], a= target-b;
+            if(map.containsKey(a)){
+                return new int[]{map.get(a),i};
             }
+            map.put(b,i);
         }
-        return res;
+        return new int[]{};
     }
 }
