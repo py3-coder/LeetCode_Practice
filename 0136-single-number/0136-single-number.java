0163d68c0812c707eca62f196b393f0282ab220e
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-29 13:51:13 +0530

    Time: 6 ms (37.87%), Space: 44.6 MB (52.35%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,23 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=1;i<nums.length;i+=2){
-            if(nums[i]!=nums[i-1]){
-                return nums[i-1];
-            }
-        }
-        return nums[nums.length-1];
+    public int singleNumber(int[] arr) {
+        Arrays.sort(arr);
+         int low = 0;
+	    int high = arr.length-1;
+	
+	    while(low < high) {
+		int mid = (low+high) / 2;
+
+		if(mid%2 != 0) {
+			mid -= 1;
+		}
+
+		if(arr[mid] == arr[mid+1]) {
+			low = mid+2;
+		} else {
+			high = mid;
+		}
+	}
+
+	return arr[low];
     }
 }
