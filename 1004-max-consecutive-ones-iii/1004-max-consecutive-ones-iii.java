48ff15f6131d493ec01f890d8d1cd0c31bf4d5c0
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-29 10:11:27 +0530

    Time: 3 ms (76.35%), Space: 46.1 MB (73.84%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,14 +1,13 @@
 class Solution {
-    public int longestOnes(int[] A, int K) {
-        int zeroCount=0;
-        int start=0,res=0;
-        for(int end=0;end<A.length;end++){
-            if(A[end] == 0) zeroCount++;
-            if(zeroCount > K){
-                if(A[start] == 0) zeroCount--;
+    public int longestOnes(int[] nums, int k) {
+        int res=0,start=0,zero=0;
+        for(int end=0;end<nums.length;end++){
+            if(nums[end]==0) zero++;
+            while(zero>k){
+                if(nums[start]==0) zero--;
                 start++;
             }
-            res=Math.max(res,end-start+1);
+            res = Math.max(res,end-start+1);
         }
         return res;
     }
