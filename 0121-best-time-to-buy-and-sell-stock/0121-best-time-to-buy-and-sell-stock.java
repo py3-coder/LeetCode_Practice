bfaddab8f9e28853f82d25815f43c6dc0b2fe166
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-08-30 11:58:15 +0530

    Time: 2 ms (83.18%), Space: 61 MB (64.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,10 +1,26 @@
 class Solution {
-    public int maxProfit(int[] prices) {
+    public int maxProfit(int[] prices) {   
+        // Broute Force 
+        // int n = prices.length;
+        // int profit=Integer.MIN_VALUE;
+        // if(n==1){
+        //     return 0;
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         profit = Math.max(profit,prices[j]-prices[i]);
+        //     }
+        // }
+        // return (profit!=-1)?profit:0;
+        // TLE-AYA 
+        
+        // Lets Optimise
         int curr_max=0;
         int max_so_far=0;
         for(int i=1;i<prices.length;i++){
-            curr_max = Math.max(0, curr_max+=prices[i]-prices[i-1]);
-            max_so_far=Math.max(curr_max,max_so_far);
+            curr_max +=prices[i]-prices[i-1];
+            curr_max =Math.max(0,curr_max);
+            max_so_far =Math.max(max_so_far,curr_max);
         }
         return max_so_far;
     }
