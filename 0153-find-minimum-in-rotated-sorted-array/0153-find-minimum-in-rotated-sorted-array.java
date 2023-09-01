9513cedc9e76dd093c5a9d6a20374d87276d103c
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-01 12:47:26 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (94.62%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,21 +1,21 @@
 class Solution {
     public int findMin(int[] arr) {
-        int len = arr.length;
-        if(len==1){
+        int n = arr.length;
+        if(n==1){
             return arr[0];
         }
-        int start=0;
-        int end =len-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            int next =(mid+1)%len;
-            int prev = (mid+len-1)%len;
-            if(arr[mid]<arr[next] && arr[mid]<arr[prev]){
+        int left =0;
+        int right =n-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            int next = ( mid+1)%n;
+            int prev = (mid+n-1)%n;
+            if(arr[prev]>arr[mid] && arr[next]>arr[mid]){
                 return arr[mid];
             }else if(arr[0]<=arr[mid]){
-                start = mid+1;
-            }else if(arr[len-1]>=arr[mid]){
-                end = mid-1;
+                left = mid+1;
+            }else if(arr[n-1]>=arr[mid]){
+                right =mid-1;
             }
         }
         return arr[0];
