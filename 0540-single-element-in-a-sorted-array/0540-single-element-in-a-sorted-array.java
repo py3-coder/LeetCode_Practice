7c4a804f7e55706326a84bb178b53baf3a000a36
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-02 21:53:20 +0530

    Time: 0 ms (100.00%), Space: 50 MB (55.85%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        //Iterative Ways::
         int n = nums.length;
         // if(n==1){
         //     return nums[0];
@@ -25,10 +26,40 @@ class Solution {
         // XOR ways ::
         // a^a=0
         // a^0=a
-         int ans=0;
-        for(int i=0;i<n;i++){
-            ans =ans^nums[i];
+        // int ans=0;
+        // for(int i=0;i<n;i++){
+        //     ans =ans^nums[i];
+        // }
+        // return ans;
+        
+        //Binary Search Way :::
+        //Edge case:
+        if(n==1){
+            return nums[0];
         }
-        return ans;
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+         
+        int left=1;
+        int right=n-2;
+        while(left<=right){
+            int mid =left+(right-left)/2;
+            //if mid is single lets check
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+            //Left side Case ::: Left side is ok
+            if((mid%2!=0 && nums[mid]==nums[mid-1]) ||(mid%2==0 && nums[mid]==nums[mid+1])){
+                left=mid+1;
+            }else{
+                //Right side case ::Right side is ok
+                right = mid-1;
+            }
+        }
+        return -1;
     }
 }
