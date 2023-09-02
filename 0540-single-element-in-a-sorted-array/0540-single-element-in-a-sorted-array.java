067507c5b3f59f38f8e6af332606a97ab01848d0
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-02 20:26:49 +0530

    Time: 1 ms (25.78%), Space: 50.5 MB (9.68%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,24 +1,34 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
-        if(n==1){
-            return nums[0];
-        }
+        // if(n==1){
+        //     return nums[0];
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(i==0){
+        //         if(nums[i]!=nums[i+1]){
+        //             return nums[i];
+        //         }
+        //     }else if(i==n-1){
+        //             if(nums[i]!=nums[i-1]){
+        //                 return nums[i];
+        //             }
+        //     }else{
+        //             if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
+        //                 return nums[i];
+        //             }
+        //         }     
+        // }
+        // return -1;
+        
+        
+        // XOR ways ::
+        // a^a=0
+        // a^0=a
+         int ans=0;
         for(int i=0;i<n;i++){
-            if(i==0){
-                if(nums[i]!=nums[i+1]){
-                    return nums[i];
-                }
-            }else if(i==n-1){
-                    if(nums[i]!=nums[i-1]){
-                        return nums[i];
-                    }
-            }else{
-                    if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
-                        return nums[i];
-                    }
-                }     
+            ans =ans^nums[i];
         }
-        return -1;
+        return ans;
     }
 }
