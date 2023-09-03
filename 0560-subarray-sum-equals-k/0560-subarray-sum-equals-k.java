cbf7fb76ca47779d5c6625b69e0b60d08ad57bde
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-03 23:49:47 +0530

    Time: 24 ms (67.53%), Space: 46.9 MB (13.15%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -5,9 +5,7 @@ class Solution {
         map.put(0,1);
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            if(map.containsKey(sum-k)){
-                res+=map.get(sum-k);
-            }
+            res+=map.getOrDefault(sum-k,0);
             map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return res;
