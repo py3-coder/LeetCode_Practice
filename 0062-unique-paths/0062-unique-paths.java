4430834787c4cf924d5a89b97a88887dd482ee01
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-03 21:23:55 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (97.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,59 @@
-public class Solution {
+class Solution {
+    public int solveR(int m,int n){
+        if(m==0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
+        int up = solveR(m-1,n);
+        int left  = solveR(m,n-1);
+        return up+left;
+    }
+    public int solveM(int m,int n,int dp[][]){
+        if(m==0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
+        if(dp[m][n] !=-1){
+            return dp[m][n];
+        }
+        int up   =solveM(m-1,n,dp);
+        int left =solveM(m,n-1,dp);
+        dp[m][n]=up+left;
+        return dp[m][n];
+    }
+    public int solveTab(int m,int n, int dp[][]){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0 )  dp[i][j] =1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left =dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    public int solveSpaceOpt(int m,int n){
+        int prev[] =new int[n];
+        for(int i=0;i<m;i++){
+            int curr[] =new int[n];
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) curr[i]=1;
+                else{
+                int up =0,left=0;
+                if(i>0) up = prev[j];
+                if(j>0) left =curr[j-1];
+                curr[j] = up+left;
+                }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
     public int uniquePaths(int m, int n) {
-        if(m == 1 || n == 1)
-            return 1;
-        m--;
-        n--;
-        if(m < n) {              // Swap, so that m is the bigger number
-            m = m + n;
-            n = m - n;
-            m = m - n;
-        }
-        long res = 1;
-        int j = 1;
-        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
-            res *= i;
-            res /= j;
-        }
-            
-        return (int)res;
+        //return solveR(m-1,n-1);
+        //int dp[][] =new int[m][n];
+        //Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
+        //return solveM(m-1,n-1,dp);
+        //return solveTab(m,n,dp);
+        return solveSpaceOpt(m,n);
     }
 }
