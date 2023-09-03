140ef24ec88f119b662aa06fefef9d95ad623229
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-03 13:08:38 +0530

    Time: 1 ms (84.74%), Space: 44.3 MB (88.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,34 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
+        int n =matrix.length;
+        int m = matrix[0].length;
+        
+        int[] row = new int[n];
+        int[] col = new int[m];
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    markrow(matrix,n,m,i);
-                    markcol(matrix,n,m,j);
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==-100000){
+                if(row[i]==1 || col[j]==1){
                     matrix[i][j]=0;
                 }
-            }
-        }
-    }
-    public static void markrow(int[][] matrix,int n,int m,int i){
-        for(int j=0;j<m;j++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-100000;
-            }
-        }
-    }
-    public static void markcol(int[][] matrix,int n,int m,int j){
-        for(int i=0;i<n;i++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-100000;
             }
         }
     }
