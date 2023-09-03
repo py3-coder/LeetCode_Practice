cbc2015c4f002fe29df6712404079cbdfbd67b3a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-03 14:42:54 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (28.02%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,15 +1,36 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        int res[][] = new int[n][n];
+        // int res[][] = new int[n][n];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //        res[j][n-i-1]=matrix[i][j]; 
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         matrix[i][j] =res[i][j];
+        //     }
+        // }
+        
+        //Optimal Approch ::
+        //Transpose 
+        //Rotate 
+        
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-               res[j][n-i-1]=matrix[i][j]; 
+            for(int j=i;j<n;j++){
+                int temp =0;
+                temp =matrix[i][j];
+                matrix[i][j] =matrix[j][i];
+                matrix[j][i] =temp;
             }
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j] =res[i][j];
+            for(int j=0;j<n/2;j++){
+                int temp=0;
+                temp =matrix[i][j];
+                matrix[i][j] =matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
             }
         }
     }
