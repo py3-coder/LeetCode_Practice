8e516de9a9943f4682bf8f67f8ed9de0955113b1
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-03 12:05:03 +0530

    Time: 1 ms (84.74%), Space: 44.4 MB (88.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,35 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        ArrayList<Pair<Integer, Integer> > lis = new ArrayList<Pair<Integer, Integer>>();
-        int i=0,j=0;
-        for(i=0;i<matrix.length;i++){
-            for(j=0;j<matrix[0].length;j++){
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    lis.add(new Pair<Integer, Integer>(i, j));
+                    markrow(matrix,n,m,i);
+                    markcol(matrix,n,m,j);
                 }
             }
         }
-       // System.out.println(lis);
-        for (Pair pp:lis) {
-            int row= ((Integer) pp.getKey()).intValue();
-            int col = ((Integer) pp.getValue()).intValue();
-            for(int l=0;l<matrix.length;l++){
-                    matrix[l][col]=0;
-            }
-            for(int m=0;m<matrix[0].length;m++){
-                matrix[row][m]=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==-100000){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
-} 
+    public static void markrow(int[][] matrix,int n,int m,int i){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]!=0){
+                matrix[i][j]=-100000;
+            }
+        }
+    }
+    public static void markcol(int[][] matrix,int n,int m,int j){
+        for(int i=0;i<n;i++){
+            if(matrix[i][j]!=0){
+                matrix[i][j]=-100000;
+            }
+        }
+    }
+}
