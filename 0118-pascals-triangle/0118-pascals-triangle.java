c34e3953bd1a71cf646f02814dee54a1fd3c7933
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-04 23:12:21 +0530

    Time: 1 ms (88.99%), Space: 40.7 MB (93.02%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,43 +1,21 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(numRows==1){
-            List<Integer> newres = new ArrayList<>();
-            newres.add(1);
-            result.add(newres);
-            return result;
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            res.add(genRow(i));
         }
-        if(numRows==2){
-            List<Integer> newres1 = new ArrayList<>();
-            newres1.add(1);
-            result.add(newres1);
-            List<Integer> newres2 = new ArrayList<>();
-            newres2.add(1);
-            newres2.add(1);
-            result.add(newres2);
-            return result;
+        return res;
+        
+    }
+    public List<Integer> genRow(int row){
+        List<Integer> temp = new ArrayList<>();
+        long res=1;
+        temp.add(1);
+        for(int col=1;col<=row;col++){
+            res =res*(row-col+1);
+            res=res/col;
+            temp.add((int)res);
         }
-         List<Integer> newres1 = new ArrayList<>();
-            newres1.add(1);
-            result.add(newres1);
-            List<Integer> newres2 = new ArrayList<>();
-            newres2.add(1);
-            newres2.add(1);
-            result.add(newres2);
-        numRows=numRows-2;
-        while(numRows-->0){
-            List<Integer> newres = new ArrayList<>();
-            List<Integer> last =new ArrayList<>();
-            last =result.get(result.size() - 1);
-            newres.add(1);
-            for(int i=0;i<last.size()-1;i++){
-                int val1 =last.get(i);
-                int val2 =last.get(i+1);
-                newres.add(val1+val2);
-            }
-            newres.add(1);
-            result.add(newres);
-        }
-        return result;
+        return temp;
     }
 }
