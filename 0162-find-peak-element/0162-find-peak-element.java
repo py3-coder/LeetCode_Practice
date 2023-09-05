87724e241e3775a72e9ae1e6c9fa5f2fba7b8324
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-05 10:02:37 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (31.54%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -4,17 +4,15 @@ class Solution {
         if(n==1){
             return 0;
         }
-        int low=0;
-        int high=n-1;
+        //Handle index 0;
+        if(nums[0]>nums[1])return 0;
+        //Hanlde index n-1;
+        if(nums[n-1]>nums[n-2])return n-1;
+        int low=1;
+        int high=n-2;
         while(low<=high){
             int mid =low+(high-low)/2;
-            //Handle index 0;
-            if(mid==0 && nums[mid]>nums[mid+1]){
-                return mid;
-            }//Hanlde index n-1;
-            if(mid==n-1 && nums[mid]>nums[mid-1]){
-                return mid;
-            }
+    
             if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
                 return mid;
             }else if(nums[mid]>nums[mid+1]){
