fcbff984cb2c566fa4defd426def9a447caa8ab1
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-05 20:27:25 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (97.97%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,8 +1,16 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        for(int i : arr){
-			if(i <= k) k++; else break;
-		}
-        return k;
+        int low =0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid =low+(high-low)/2;
+            int missing = arr[mid]-(mid+1);
+            if(missing<k){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return high+1+k;
     }
 }
