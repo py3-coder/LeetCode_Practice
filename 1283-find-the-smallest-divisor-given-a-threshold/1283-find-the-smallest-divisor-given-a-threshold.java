3034dbbf2bde6f238d7e07dcf9fa1c08e039f7cd
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-05 14:53:38 +0530

    Time: 30 ms (60.75%), Space: 47 MB (53.78%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,32 +1,30 @@
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-        int max =0;
-        for(int val :nums){
-            max = Math.max(val,max);
+        //edge case ::
+        if(nums.length-1==threshold){
+            return threshold;
         }
-        if(nums.length==threshold){
-            return max;
+        int low=1;
+        int high=Integer.MIN_VALUE;
+        for(int val:nums){
+            high=Math.max(val,high);
         }
-        int res =0;
-        int start=1;
-        int end = max;
-        while(start<=end){
-            int mid = start +(end-start)/2;
-            if(isPossible(nums,threshold,mid)==true){
-                res = mid;
-                end = mid-1;
-            }else {
-                start = mid+1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int ans=check(mid,nums);
+            if(ans<=threshold){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-        return res;
+        return low;
     }
-    public static boolean isPossible(int[] arr, int threshold,int divisor){
-        int newthreshold =0;
-        for(int i=0;i<arr.length;i++){
-            newthreshold +=(int)Math.ceil(arr[i]*1.0/divisor);
+    public static int check(int mid,int[] arr){
+        int ans=0;
+        for(int val:arr){
+            ans+=Math.ceil((double)(val)/(double)(mid));
         }
-        return newthreshold<=threshold;
-        
+        return ans;
     }
 }
