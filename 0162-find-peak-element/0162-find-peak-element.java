97129a92f9d0550bf3de6b28d1ab21f9f316d144
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-05 10:00:17 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (16.47%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,38 +1,28 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-     int  n = nums.length;
-     //Base Case if length==1
-     if(n==1){
-         return 0;
-     }
-     int start = 0;
-     int end = n-1;
-     while(start<=end){
-         int mid = start +(end-start)/2;
-         if(mid>0 && mid<n-1){
-             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-                 return mid;
-             }else if(nums[mid]<nums[mid+1]){
-                 start = mid+1;
-             }else{
-                 end = mid-1;
-             }
-         }
-         else if(mid==0){
-             if(nums[0]>nums[1]){
-                 return 0;
-             }else {
-                 return 1;
-             }
-         }
-         else if(mid==n-1){
-             if(nums[n-1] > nums[n-2]){
-                 return n-1;
-             }else{
-                 return n-2;
-             }
-         }
-     }
-     return 0;
+        int n=nums.length;
+        if(n==1){
+            return 0;
+        }
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid =low+(high-low)/2;
+            //Handle index 0;
+            if(mid==0 && nums[mid]>nums[mid+1]){
+                return mid;
+            }//Hanlde index n-1;
+            if(mid==n-1 && nums[mid]>nums[mid-1]){
+                return mid;
+            }
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+                return mid;
+            }else if(nums[mid]>nums[mid+1]){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return -1;
     }
 }
