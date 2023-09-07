250178db27096f7a1f00c50314d83207d95e6122
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-07 16:10:31 +0530

    Time: 26 ms (95.90%), Space: 50.4 MB (80.05%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,20 +1,63 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
-        int n=arr.length;
-         Set<List<Integer>> set =new HashSet<>();
-        for(int i=0;i<n;i++){
-            HashSet<Integer> hs = new HashSet<>();
-            for(int j=i+1;j<n;j++){
-                int target = -(arr[i]+arr[j]);
-                if(hs.contains(target)){
-                    List<Integer> temp = Arrays.asList(arr[i], arr[j], target);
-                        temp.sort(null);
-                        set.add(temp);
-                }
-                hs.add(arr[j]);
+        //     Set<List<Integer>> set =new HashSet<>();
+    //      //Broute Force::
+    int n= arr.length;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i+1;j<n;j++){
+    //             for(int k=j+1;k<n;k++){
+    //                 if(arr[i]+arr[j]+arr[k]==0){
+    //                     List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+    //                     temp.sort(null);
+    //                     set.add(temp);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //    List<List<Integer>> res = new ArrayList<>(set);
+    // return res;
+
+    //optimised...
+    //     for(int i=0;i<n;i++){
+    //         HashSet<Integer> hs = new HashSet<>();
+    //         for(int j=i+1;j<n;j++){
+    //             int target = -(arr[i]+arr[j]);
+    //             if(hs.contains(target)){
+    //                 List<Integer> temp = Arrays.asList(arr[i], arr[j], target);
+    //                     temp.sort(null);
+    //                     set.add(temp);
+    //             }
+    //             hs.add(arr[j]);
+    //         }
+    //     }
+    //    List<List<Integer>> res = new ArrayList<>(set);
+    //    return res;
+
+    //better 
+    List< List < Integer > > ans= new ArrayList<>();
+    Arrays.sort(arr);
+    for(int i=0;i<n;i++){
+        //remove duplicate i value
+        if(i!=0 && arr[i]==arr[i-1]) continue;
+        int j=i+1;
+        int k=n-1;
+        while(j<k){
+            int sum = arr[i]+arr[j]+arr[k];
+            if(sum<0){
+                j++;
+            }else if(sum>0){
+                k--;
+            }else{
+                List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    //skip the duplicates:
+                    while (j < k && arr[j] == arr[j - 1]) j++;
+                    while (j < k && arr[k] == arr[k + 1]) k--;
             }
         }
-       List<List<Integer>> res = new ArrayList<>(set);
-       return res;
+    }
+    return ans;
     }
 }
