93ff2e17e8d4ef3705b62f73a9bc7479f8ab6ce7
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-07 13:19:55 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (6.44%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int k=0;
-        for(int i=m;i<m+n;i++){
-            nums1[i]=nums2[k];
-            k++;
+     int p1 = m-1 , p2 = n-1 ,i = m+n-1;
+        while(p2 >=0 ){
+            if(p1 >=0 && nums1[p1] > nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            } 
+            else{
+                nums1[i--] = nums2[p2--];
+            }
         }
-        Arrays.sort(nums1);
     }
 }
