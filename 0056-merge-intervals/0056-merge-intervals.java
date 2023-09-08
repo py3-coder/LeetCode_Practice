629836bf61e8195f7fe1378c561e171ecc3d7cf0
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-08 11:40:23 +0530

    Time: 8 ms (93.52%), Space: 45.2 MB (90.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -6,23 +6,39 @@ class Solution {
         Arrays.sort(arr , (a,b)->Integer.compare(a[0],b[0]));
         
         //Broute Force Approch :::
+//         for(int i=0;i<n;i++){
+//             int start =arr[i][0];
+//             int end =arr[i][1];
+            
+//             //skip the case when [1,4] ,[2,3] == [1,4]..
+//             if(!result.isEmpty() && end<=result.get(result.size()-1)[1]) continue;
+            
+//             for(int j=i+1;j<n;j++){
+//                 if(end>=arr[j][0]){
+//                     end = Math.max(end,arr[j][1]);
+//                 }else{
+//                     break;
+//                 }
+//             }
+//             int[] temp ={start,end};
+//             result.add(temp);
+//         }
+//         return result.toArray(new int[result.size()][]);
+        
+        //TC : O(nlogn)+O(2n)
+        // why 2n is just we are checking every pair 2 time do run you will get it..
+        //SC : O(n) --> To return result--
+        
+        
+        // Lets Optimise :::
         for(int i=0;i<n;i++){
-            int start =arr[i][0];
-            int end =arr[i][1];
-            
-            //skip the case when [1,4] ,[2,3] == [1,4]..
-            if(!result.isEmpty() && end<=result.get(result.size()-1)[1]) continue;
-            
-            for(int j=i+1;j<n;j++){
-                if(end>=arr[j][0]){
-                    end = Math.max(end,arr[j][1]);
-                }else{
-                    break;
-                }
+            if(result.isEmpty() || result.get(result.size()-1)[1]<arr[i][0]){
+                int[] temp ={arr[i][0],arr[i][1]};
+                result.add(temp);
+            }else{
+                result.get(result.size()-1)[1]=Math.max(result.get(result.size()-1)[1],arr[i][1]);
             }
-            int[] temp ={start,end};
-            result.add(temp);
         }
-        return result.toArray(new int[result.size()][]);
+        return result.toArray(new int[result.size()-1][]);
     }
 }
