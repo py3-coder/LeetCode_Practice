cecf26ddae7582b3db3dec213995454d9368d241
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-09 20:22:47 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (83.88%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,14 +1,23 @@
 class Solution {
-    public int combinationSum4(int[] nums, int target) {
-    int[] comb = new int[target + 1];
-    comb[0] = 1;
-    for (int i = 1; i < comb.length; i++) {
-        for (int j = 0; j < nums.length; j++) {
-            if (i - nums[j] >= 0) {
-                comb[i] += comb[i - nums[j]];
+    public static int solve(int[] nums,int target,int dp[]){
+        if(dp[target] !=-1){
+            return dp[target];
+        }
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            if(target>=nums[i]){
+                 res += solve(nums,target-nums[i],dp);
             }
         }
+        dp[target] =res;
+        return dp[target];
     }
-    return comb[target];
+    public int combinationSum4(int[] nums, int target) {
+        int dp[] = new int[target+1];
+        Arrays.fill(dp,-1);
+        dp[0] =1;
+        int res = solve(nums,target,dp);
+        return res;
+
     }
 }
