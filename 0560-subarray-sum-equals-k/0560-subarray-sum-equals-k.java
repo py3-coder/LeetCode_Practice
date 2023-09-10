6f8b28a8e41eece2673b7b7db4873e28c9238751
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-10 14:10:19 +0530

    Time: 26 ms (49.27%), Space: 45.9 MB (59.64%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,13 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int sum=0,res=0;
-        HashMap<Integer,Integer> map =new HashMap<>();
+        int count=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum=0;
         map.put(0,1);
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            res+=map.getOrDefault(sum-k,0);
+            int due=sum-k;
+            if(map.containsKey(due)){
+                count+=map.get(due);
+            }
             map.put(sum,map.getOrDefault(sum,0)+1);
         }
-        return res;
+        return count;
     }
 }
