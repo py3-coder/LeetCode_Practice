71add05c708220bacea99eafde6739820878cccc
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-11 14:09:14 +0530

    Time: 28 ms (85.21%), Space: 59.1 MB (71.88%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -9,13 +9,16 @@ class Solution {
         int p=0;
         int i=0,j=0;
         while(j<n){
+            //remove all those who are lesser than current::
             while(deque.size()>0 && deque.getLast()<arr[j]){
                 deque.removeLast();
             }
+            //add at last
             deque.offer(arr[j]);
-            
+            //window size is lesss so j++;
             if(j-i+1<k) j++;
             else if(j-i+1==k){
+                //the first ele will be max for that window
                 res[p++]=deque.getFirst();
                 if(arr[i]==deque.getFirst()){
                     deque.removeFirst();
