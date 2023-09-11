d5aa06d32e72f8bde8993bfc9b79a6088650bfe5
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-11 19:32:30 +0530

    Time: 1 ms (99.92%), Space: 55.7 MB (51.60%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] arr) {
+        int n=arr.length;
+        int mini =Integer.MAX_VALUE;
+        int sum=0;
+        int end=0,start=0;
+        while(end<n){
+            if(sum<target){
+                sum+=arr[end];
+                end++;
+            }
+            while(sum>=target){
+                mini =Math.min(end-start,mini);
+                sum-=arr[start];
+                start++;
+            }
+            
+        }
+        return  mini==Integer.MAX_VALUE?0:mini;
+    }
+}
