b5c067d8ebae8ec05ca21491b9315c0160bbef38
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-13 09:57:12 +0530

    Added solution - LeetHub

---
+++ GCD of two numbers - GFG/gcd-of-two-numbers.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int A = sc.nextInt();
+                    int B = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.gcd(A,B));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int gcd(int A , int B) 
+    { 
+        //code here
+        return A==0?B:gcd(B%A,A);
+    } 
+    
+}
