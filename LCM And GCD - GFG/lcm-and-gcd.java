0e62c475773ee3a8d0a863edcdb887a52258ec8f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-13 17:32:41 +0530

    Added solution - LeetHub

---
+++ LCM And GCD - GFG/lcm-and-gcd.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            Long A = Long.parseLong(S[0]);
+            Long B = Long.parseLong(S[1]);
+
+            Solution ob = new Solution();
+            Long[] ptr = ob.lcmAndGcd(A,B);
+            
+            System.out.print(ptr[0]);
+            System.out.print(" ");
+            System.out.println(ptr[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static Long[] lcmAndGcd(Long A , Long B) {
+        // code here
+        long val =gcd(A,B);
+        long lcm =(A/val)*(B);
+        return new Long[]{lcm,val};
+    }
+    static long gcd(long a,long b){
+        return a==0?b:gcd(b%a,a);
+    }
+}
