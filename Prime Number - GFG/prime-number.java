dca708360a965ca8be8d4280b6a9eb1c46d377bf
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-13 17:23:03 +0530

    Added solution - LeetHub

---
+++ Prime Number - GFG/prime-number.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.isPrime(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static int isPrime(int N){
+        // code here
+        if(N==1) return 0;
+        for(int i=2;i<=Math.sqrt(N);i++){
+            if(N%i==0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
