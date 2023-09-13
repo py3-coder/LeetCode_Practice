8802873cf77237044d3aaf18cbf39ec212f02f3c
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-13 10:28:46 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (55.97%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -12,15 +12,31 @@ class Solution {
         //TC : O(n)
         //SC :O(1)
         
+//         int count=0;
+//         int num=gcd(a,b);
+//         for(int i=1;i<=num;i++){
+//             if(a%i==0 && b%i==0){
+//                 count++;
+//             }
+//         }
+//         return count;
+        
+        //TC : O(log(min(a,b)))+O(log(min(a,b)))
+        //SC : O(1)
+        
+        
         int count=0;
         int num=gcd(a,b);
-        for(int i=1;i<=num;i++){
-            if(a%i==0 && b%i==0){
-                count++;
+        for(int i=1;i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                if(i*i==num){
+                    count+=1;
+                }else{
+                    count+=2;
+                }
             }
         }
         return count;
-        
         
     }
     public static int gcd(int a,int b){
