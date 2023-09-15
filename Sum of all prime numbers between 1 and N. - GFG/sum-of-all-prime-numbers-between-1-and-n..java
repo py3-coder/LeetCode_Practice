6b2010870e85d5810e7596c4174f27190353be59
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-16 00:12:43 +0530

    Added solution - LeetHub

--- Sum of all prime numbers between 1 and N. - GFG/sum-of-all-prime-numbers-between-1-and-n..java
+++ Sum of all prime numbers between 1 and N. - GFG/sum-of-all-prime-numbers-between-1-and-n..java
@@ -27,11 +27,17 @@ class Solution
     {
         // code here
         //Broute Force ::
-        long sum=0;
-        for(int i=1;i<=n;i++){
-            sum+=checkprime(i);
-        }
-        return sum;
+        // long sum=0;
+        // for(int i=1;i<=n;i++){
+        //     sum+=checkprime(i);
+        // }
+        // return sum;
+        // TC : O(nlogn)
+        //SC : O(1) 
+        
+        //Sieve of Eratathonoes  -- TC - O(n*log(logn))
+        //SC : O(n)
+        return sieve(n);
     }
     public static long checkprime(int num){
         if(num==1) return 0;
@@ -41,5 +47,26 @@ class Solution
             }
         }
         return num;
+    }
+    
+    public  static long sieve(int num){
+        long sum=0;
+        boolean prime[] = new boolean[num+1];
+        for(int i=0;i<=num;i++){
+            prime[i] =true;
+        }
+        for(int p=2;p<=Math.sqrt(num);p++){
+            if(prime[p] ==true){
+                for(int i=p*p;i<=num;i+=p){
+                    prime[i] =false;
+                }
+            }
+        }
+        for(int i=2;i<=num;i++){
+            if(prime[i]==true){
+                sum+=i;
+            }
+        }
+        return sum;
     }
 }
