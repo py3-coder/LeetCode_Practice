c985a968f072d258876b8f74297839b3f2f53f5a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-17 10:14:55 +0530

    Time: 1 ms (97.06%), Space: 44.1 MB (16.70%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,13 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int count=0;
+        int count=1;
+        int k=1;
+        int val=nums[0];
         for(int i=1;i<nums.length;i++){
-            if(nums[i] == nums[i-1]){
+            if(nums[i]>nums[i-1]){
                 count++;
-            }else{
-                nums[i-count]=nums[i];
+                nums[k++]=nums[i];
             }
         }
-        return nums.length-count;
+        return count;
     }
 }
