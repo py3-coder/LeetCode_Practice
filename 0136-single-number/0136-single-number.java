5a0f5244276541a1d1b3b0961899a1f80aa7faa3
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-17 11:03:22 +0530

    Time: 6 ms (38.78%), Space: 45.3 MB (6.53%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,23 +1,38 @@
 class Solution {
-    public int singleNumber(int[] arr) {
-        Arrays.sort(arr);
-         int low = 0;
-	    int high = arr.length-1;
-	
-	    while(low < high) {
-		int mid = (low+high) / 2;
-
-		if(mid%2 != 0) {
-			mid -= 1;
-		}
-
-		if(arr[mid] == arr[mid+1]) {
-			low = mid+2;
-		} else {
-			high = mid;
-		}
-	}
-
-	return arr[low];
+    public int singleNumber(int[] nums) {
+        //edge case 
+        int n= nums.length;
+        if(nums.length==1){
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        // for(int i=0;i<n-1;i+=2){
+        //     if(nums[i]!=nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // if(nums[n-1]!=nums[n-2]){
+        //     return nums[n-1];
+        // }
+        // return 0;
+        
+        //TC : O(nlogn)+O(n)
+        //SC : O(1);
+        
+        // Lets thing for Optimise::
+        int low =0;
+        int high=n-1;
+        while(low<high){
+            int mid =low+(high-low)/2;
+            if(mid%2!=0){
+                mid-=1;
+            }
+            if(nums[mid]==nums[mid+1]){
+                low=mid+2;
+            }else{
+                high=mid;
+            }
+        }
+        return nums[low];
     }
 }
