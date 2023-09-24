fb17bf8a5913c93140afdf43fcfbd1eea1981757
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-24 23:00:06 +0530

    Added solution - LeetHub

---
+++ Next Greater Element - GFG/next-greater-element.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    long[] res = new Solution().nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        System.out.print(res[i] + " ");
+		    System.out.println();
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        // Your code here
+        //Lets play with Stack::
+        //Brute Force :::
+        long[] res = new long[n];
+        int k=n-1;
+        Stack<Long> st = new Stack();
+        for(int i=n-1;i>=0;i--){
+            if(st.isEmpty()){
+                res[i] =-1;
+            }else if(st.size()>0 && st.peek()>arr[i]){
+                res[i] =st.peek();
+            }else if(st.size()>0 && st.peek()<=arr[i]){
+                while(st.size()>0 && st.peek()<=arr[i]){
+                    st.pop();
+                }
+                if(st.size()==0){
+                    res[i] =-1;
+                }else{
+                    res[i] =st.peek();
+                }
+            }
+            st.push(arr[i]);
+        }
+        return res;
+    } 
+}
