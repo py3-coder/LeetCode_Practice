950068f9dbd74748a15160fe88ca49a884283395
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-26 14:05:22 +0530

    Time: 4 ms (84.99%), Space: 55.8 MB (21.77%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,15 +1,21 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxarea=0;
-        int left=0,right=height.length-1;
-        while(left<right){
-            maxarea = Math.max(maxarea,Math.min(height[left],height[right])*(right-left));
-            if(height[left]<height[right]){
+        //Lets Play ::: 
+        //Broute Force :: TC : O(n^2) SC : O(1)
+        
+        //lets try two pointer::
+        int left=0;
+        int right=height.length-1;
+        int area=0;
+        while(left<=right){
+            if(height[left]<=height[right]){
+                area =Math.max(Math.min(height[left],height[right])*(right-left),area);
                 left++;
             }else{
+                area =Math.max(Math.min(height[left],height[right])*(right-left),area);
                 right--;
             }
         }
-        return maxarea;
+        return area;
     }
 }
