aa4f4dd45258ce0ab9f4f79d3035fa99ef9292db
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-27 16:12:29 +0530

    Time: 28 ms (58.59%), Space: 58.2 MB (46.09%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -4,19 +4,45 @@ class Solution {
         if(n==0 || n==1){
             return n;
         }
-        Arrays.sort(a);
-        int counter=0;
-        int maxi=1;
-        int lastele=Integer.MIN_VALUE;
-        for(int i=0;i<a.length;i++){
-            if(a[i]-1==lastele){
-                counter++;
-            }else if(a[i]!=lastele){
-                counter=1;
-            }
-            lastele=a[i];
-            maxi=Math.max(counter,maxi);
+        // Arrays.sort(a);
+        // int counter=0;
+        // int maxi=1;
+        // int lastele=Integer.MIN_VALUE;
+        // for(int i=0;i<a.length;i++){
+        //     if(a[i]-1==lastele){
+        //         counter++;
+        //     }else if(a[i]!=lastele){
+        //         counter=1;
+        //     }
+        //     lastele=a[i];
+        //     maxi=Math.max(counter,maxi);
+        // }
+        // return maxi;
+        
+        //TC : O(nlogn)
+        //SC : O(1)
+        
+        int max=0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int ele:a){
+            set.add(ele);
         }
-        return maxi;
+        for(int i=0;i<n;i++){
+            int count=1;
+            //left ::
+            int num =a[i];
+            while(set.contains(--num)){
+                count++;
+                set.remove(num);
+            }
+            //right side::
+            num =a[i];
+            while(set.contains(++num)){
+                count++;
+                set.remove(num);
+            }
+            max = Math.max(max,count);
+        }
+        return max;
     }
 }
