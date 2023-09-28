499775bb02ba5765134d63f0a83e1b629a73c1c4
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-28 23:34:15 +0530

    Time: 1 ms (81.28%), Space: 44 MB (63.24%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,23 +1,33 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int res[] = new  int[nums.length];
-        ArrayList<Integer> liseven = new ArrayList<>();
-        ArrayList<Integer> lisodd = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                liseven.add(nums[i]);
-            }else{
-                lisodd.add(nums[i]);
+        // ArrayList<Integer> liseven = new ArrayList<>();
+        // ArrayList<Integer> lisodd = new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]%2==0){
+        //         liseven.add(nums[i]);
+        //     }else{
+        //         lisodd.add(nums[i]);
+        //     }
+        // }
+        // int k=0;
+        // for(int i=0;i<liseven.size();i++){
+        //     res[k++] =liseven.get(i);
+        // }
+        // for(int i=0;i<lisodd.size();i++){
+        //     res[k++] =lisodd.get(i);
+        // }
+        // return res;
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            if(nums[left]%2==0) left++;
+            else if(nums[right]%2!=0) right--;
+            else{
+                int temp=nums[left];
+                nums[left] =nums[right];
+                nums[right]=temp;
             }
         }
-        int k=0;
-        for(int i=0;i<liseven.size();i++){
-            res[k++] =liseven.get(i);
-        }
-        for(int i=0;i<lisodd.size();i++){
-            res[k++] =lisodd.get(i);
-        }
-        return res;
-        
+        return nums;
     }
 }
