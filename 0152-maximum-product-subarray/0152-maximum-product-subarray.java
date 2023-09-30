e55fdcb136edcbd58cae3b6cba09ec9cf9a36f2a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-30 09:03:05 +0530

    Time: 0 ms (100%), Space: 43.3 MB (38.88%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -6,18 +6,47 @@ class Solution {
         // TC : O(n^2)
         //SC : O(1)
 
+
+        int prod=1;
         int maxi =Integer.MIN_VALUE;
-        //Based on Some Observation 
-        int prefix=1;
-        int suffix=1;
         for(int i=0;i<n;i++){
-            prefix*=arr[i];
-            suffix*=arr[n-i-1];
-            maxi =Math.max(maxi,Math.max(prefix,suffix));
-            if(prefix==0) prefix=1;
-            if(suffix==0) suffix=1;
+            prod*=arr[i];
+            maxi=Math.max(prod,maxi);
+            if(prod==0){
+                prod=1;
+            }
+            
+        }
+        prod=1;
+        for(int i=n-1;i>=0;i--){
+            prod*=arr[i];
+            maxi=Math.max(prod,maxi);
+            if(prod==0){
+                prod=1;
+            }
             
         }
         return maxi;
+        
+
+
+
+
+        // int maxi =Integer.MIN_VALUE;
+        // //Based on Some Observation 
+        // int prefix=1;
+        // int suffix=1;
+        // for(int i=0;i<n;i++){
+        //     prefix*=arr[i];
+        //     suffix*=arr[n-i-1];
+        //     maxi =Math.max(maxi,Math.max(prefix,suffix));
+        //     if(prefix==0) prefix=1;
+        //     if(suffix==0) suffix=1;
+            
+        // }
+        // return maxi;
+
+        //TC : O(n)
+        //SC : O(1)
     }
 }
