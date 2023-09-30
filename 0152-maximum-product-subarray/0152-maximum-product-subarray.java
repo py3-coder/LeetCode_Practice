78ea001c3f02a659933cd9f0bb218e3374dfead4
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-30 09:28:16 +0530

    Time: 2 ms (55.28%), Space: 43.5 MB (28.74%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -7,31 +7,31 @@ class Solution {
         //SC : O(1)
 
 
-        int prod=1;
-        int maxi =Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            prod*=arr[i];
-            maxi=Math.max(prod,maxi);
-            if(prod==0){
-                prod=1;
-            }
+        //Improvoised Kadane::
+        // int prod=1;
+        // int maxi =Integer.MIN_VALUE;
+        // for(int i=0;i<n;i++){
+        //     prod*=arr[i];
+        //     maxi=Math.max(prod,maxi);
+        //     if(prod==0){
+        //         prod=1;
+        //     }
             
-        }
-        prod=1;
-        for(int i=n-1;i>=0;i--){
-            prod*=arr[i];
-            maxi=Math.max(prod,maxi);
-            if(prod==0){
-                prod=1;
-            }
+        // }
+        // prod=1;
+        // for(int i=n-1;i>=0;i--){
+        //     prod*=arr[i];
+        //     maxi=Math.max(prod,maxi);
+        //     if(prod==0){
+        //         prod=1;
+        //     }
             
-        }
-        return maxi;
+        // }
+        // return maxi;
+
+        //TC : O(n)+O(n)
+        //SC : O(1)
         
-
-
-
-
         // int maxi =Integer.MIN_VALUE;
         // //Based on Some Observation 
         // int prefix=1;
@@ -48,5 +48,19 @@ class Solution {
 
         //TC : O(n)
         //SC : O(1)
+
+        //Lets Deep Dive to kadance again with optimal approch ::::
+
+        int ans=arr[0];
+        int currMax =arr[0];
+        int currMin =arr[0];
+        for(int i=1;i<n;i++){
+            int temp=currMax;
+            currMax =Math.max(temp*arr[i],Math.max(currMin*arr[i],arr[i]));
+            currMin =Math.min(temp*arr[i],Math.min(currMin*arr[i],arr[i]));
+            
+            ans =Math.max(ans,currMax);
+        }
+        return ans;
     }
 }
