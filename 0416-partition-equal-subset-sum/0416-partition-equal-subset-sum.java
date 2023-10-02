c05caade4a5dcbc8e43fb65e60d6f8a05d538139
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-02 15:58:30 +0530

    Time: 48 ms (44.08%), Space: 107.8 MB (5.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,22 +1,68 @@
 class Solution {
-    public boolean solveR(int nums[],int sum,int index,Boolean dp[][]){
-        if(index>=nums.length || sum <0) return false;
-        if(sum ==0) return true;
-        if(dp[index][sum]!=null){
-            return dp[index][sum];
+    static int[][] memo = new int[101][100001];
+	public static boolean canPartition(int[] arr) {
+		// Write your code here.
+		int sum =0;
+        for(int ele:arr){
+            sum+=ele;
         }
-        return dp[index][sum] =(solveR(nums,sum-nums[index],index+1,dp) || solveR(nums,sum,index+1,dp));
+        if(sum%2==1) return false;
+        else{
+            sum =sum/2;
+            int n=arr.length;
+            return solveTab(n,arr,sum)==0?false:true;
         }
-    public boolean canPartition(int[] nums) {
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-           sum+=nums[i];
-        }
-        if(sum%2==0){
-            Boolean dp[][] = new Boolean[nums.length+1][sum+1];
-            return solveR(nums,sum/2,0,dp);
+	}
+	public static int solveRec(int n,int arr[] ,int target){
+        if(target==0) return 1;
+        if(n==0) return 0;
+
+        if(arr[n-1]>target){
+            return solveRec(n-1,arr,target);
         }else{
-            return false;
+            return Math.max(solveRec(n-1,arr,target-arr[n-1]),solveRec(n-1,arr,target));
         }
+    }
+	public static int solvememo(int n,int arr[],int target){
+        //Base ::
+        if(target==0) return 1;
+        if(n==0) return 0;
+        
+        //check pre-camputed ?
+        if(memo[n][target]!=-1){
+            return memo[n][target];
+        }
+        if(arr[n-1]>target){
+            //not possible to pick --> Not pick Case ::
+            return memo[n][target]=solvememo(n-1,arr,target);
+        }else{
+            //pick and not pick case::
+        return memo[n][target]=Math.max(solvememo(n-1,arr,target-arr[n-1]),solvememo(n-1,arr,target));
+        }
+    }
+    public static int solveTab(int n,int  arr[] ,int target){
+        //Base --> initlization
+        int tab[][] = new int[n+1][target+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<target+1;j++){
+                if(i==0){
+                    tab[i][j]=0;
+                }
+                if(j==0){
+                    tab[i][j]=1;
+                }
+            }
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<target+1;j++){
+                if(arr[i-1]>j){
+                    tab[i][j]=tab[i-1][j];
+                }else{
+                    tab[i][j] =Math.max(tab[i-1][j-arr[i-1]],tab[i-1][j]);
+                }
+            }
+        }
+        return tab[n][target];
     }
 }
