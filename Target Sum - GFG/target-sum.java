6e51170f8821a6d62bae927138d97eebe5855145
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-02 22:36:56 +0530

    Added solution - LeetHub

--- Target Sum - GFG/target-sum.java
+++ Target Sum - GFG/target-sum.java
@@ -39,10 +39,15 @@ class Solution {
         //1. Recursion:
         // TC : O(2^n)
         //SC :O(1)
-        return solveRec(arr,n,target);
-        //2. Memo:
-        // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
-        // return solveMemo(arr,n,target);
+        // TestCase Passed All:)
+        //return solveRec(arr,n,target);
+        //2. Memo :) Since target range => -1000<=Traget <= 1000::
+        // Can stote in 2d matrix : Think of other data structure -> HashMap ?
+        ArrayList<Map<Integer,Integer>> memo = new ArrayList<>(n+1);
+        for(int i=0;i<n+1;i++){
+            memo.add(new HashMap<Integer,Integer>());
+        }
+        return solveMemo(arr, n, target,memo);
         
     }
     public static int solveRec(int[] arr,int n,int target){
@@ -55,16 +60,19 @@ class Solution {
         //add or substract::
         return solveRec(arr,n-1,target-arr[n-1])+solveRec(arr,n-1,target+arr[n-1]);
     }
-    public static int solveMemo(int[] arr,int n,int target){
-       if(n==0){
+    public static int solveMemo(int[] arr, int n,int target,ArrayList<Map<Integer,Integer>> memo){
+        if(n==0){
             if(target==0){
                 return 1;
             }
             return 0;
-        } 
-        if(memo[n][target]!=-1){
-            return memo[n][target];
         }
-        return memo[n][target] =solveMemo(arr,n-1,target-arr[n-1])+solveMemo(arr,n-1,target+arr[n-1]);
+        if(memo.get(n).containsKey(target)){
+            return memo.get(n).get(target);
+        }
+        int ans=solveMemo(arr,n-1,target-arr[n-1],memo)+solveMemo(arr,n-1,target+arr[n-1],memo);
+        //update the nth - map:
+        memo.get(n).put(target,ans);
+        return ans;
     }
 }
