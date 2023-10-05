cb6c561c56accbc2e6142bc7cfd51ceca093f1d7
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-05 15:45:20 +0530

    Time: 0 ms (100%), Space: 43.4 MB (30.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,12 +1,23 @@
 class Solution {
     public int searchInsert(int[] A, int target) {
-     int low = 0, high = A.length-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(A[mid] == target) return mid;
-            else if(A[mid] > target) high = mid-1;
-            else low = mid+1;
+    //  int low = 0, high = A.length-1;
+    //     while(low<=high){
+    //         int mid = (low+high)/2;
+    //         if(A[mid] == target) return mid;
+    //         else if(A[mid] > target) high = mid-1;
+    //         else low = mid+1;
+    //     }
+    //     return low;
+    return recursiveBinarySearch(A,0,A.length-1,target);
+    }
+    public static int recursiveBinarySearch(int nums[],int low,int high,int target){
+        if(low>high) return low;
+        int mid =low+(high-low)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]>target){
+            return recursiveBinarySearch(nums,low,mid-1,target);
+        }else{
+            return recursiveBinarySearch(nums,mid+1,high,target);
         }
-        return low;
     }
 }
