49af2229d6fa380b953082cf027849b71a4f7a26
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-07 10:50:22 +0530

    Time: 42 ms (5.48%), Space: 39.1 MB (63.91%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -6,16 +6,45 @@ class Solution {
         for(int i=0;i<n;i++){
             arr[i]=i+1;
         }
-        return solveRec(n,arr,n,1);
+        return solveRec(n,arr,n);
 
-        
+        // HashMap<String,Integer> map = new HashMap<>();
+        // return solveMemo(n,arr,n,1,map);
+
+        //return solveRecDiff(n);
+
     }
-    public static int solveRec(int n,int[] arr,int target,int maxi){
-        if(n==0 || target==0 ) return maxi;
+    public static int solveRec(int n,int[] arr,int target){
+        if(n==0 || target==0 ) return 1;
         if(arr[n-1]>target){
-            return solveRec(n-1,arr,target,maxi);
+            return solveRec(n-1,arr,target);
         }else{
-        return Math.max(solveRec(n,arr,target-arr[n-1],maxi*arr[n-1]),solveRec(n-1,arr,target,maxi));
+            return Math.max(arr[n-1]*solveRec(n,arr,target-arr[n-1]),solveRec(n-1,arr,target));
         }
+    }
+    public static int solveMemo(int n,int[] arr,int target,int maxi,HashMap<String,Integer> map ){
+        if(n==0 || target==0 ) return maxi;
+        String key =n+"n"+target+"t"+maxi+"m";
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+        if(arr[n-1]>target){
+            int val=solveMemo(n-1,arr,target,maxi,map);
+            map.put(key,val);
+            return val;
+        }else{
+            int val=Math.max(solveMemo(n,arr,target-arr[n-1],maxi*arr[n-1],map),solveMemo(n-1,arr,target,maxi,map));
+            map.put(key,val);
+            return val;
+        }  
+    }
+    public static int solveRecDiff(int n){
+        if(n<=3) return n-1;
+        int ans = n;
+        for(int i=2;i<n;i++){
+            ans= Math.max(ans,i*solveRecDiff(n-i));
+        }
+        return ans;
+
     }
 }
