ba408b2d03b13f01bd86ad22925ce00f0556eb45
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-07 13:54:11 +0530

    Time: 42 ms (5.48%), Space: 38.6 MB (97.3%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,4 +1,5 @@
 class Solution {
+    static int memo[][] =new int[59][59];
     public int integerBreak(int n) {
         if(n==2) return 1;
         if(n==3) return 2;
@@ -6,12 +7,10 @@ class Solution {
         for(int i=0;i<n;i++){
             arr[i]=i+1;
         }
-        return solveRec(n,arr,n);
-
-        // HashMap<String,Integer> map = new HashMap<>();
-        // return solveMemo(n,arr,n,1,map);
-
-        //return solveRecDiff(n);
+        // return solveRec(n,arr,n);
+        //2. Memoization::
+        Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
+        return solveMemo(n,arr,n);
 
     }
     public static int solveRec(int n,int[] arr,int target){
@@ -19,32 +18,19 @@ class Solution {
         if(arr[n-1]>target){
             return solveRec(n-1,arr,target);
         }else{
-            return Math.max(arr[n-1]*solveRec(n,arr,target-arr[n-1]),solveRec(n-1,arr,target));
+        return Math.max(arr[n-1]*solveRec(n,arr,target-arr[n-1]),solveRec(n-1,arr,target));
         }
     }
-    public static int solveMemo(int n,int[] arr,int target,int maxi,HashMap<String,Integer> map ){
-        if(n==0 || target==0 ) return maxi;
-        String key =n+"n"+target+"t"+maxi+"m";
-        if(map.containsKey(key)){
-            return map.get(key);
+    public static int solveMemo(int n,int[] arr,int target){
+        if(n==0 || target==0 ) return 1;
+        if(memo[n][target]!=-1){
+            return memo[n][target];
         }
         if(arr[n-1]>target){
-            int val=solveMemo(n-1,arr,target,maxi,map);
-            map.put(key,val);
-            return val;
+            return memo[n][target]=solveRec(n-1,arr,target);
         }else{
-            int val=Math.max(solveMemo(n,arr,target-arr[n-1],maxi*arr[n-1],map),solveMemo(n-1,arr,target,maxi,map));
-            map.put(key,val);
-            return val;
-        }  
-    }
-    public static int solveRecDiff(int n){
-        if(n<=3) return n-1;
-        int ans = n;
-        for(int i=2;i<n;i++){
-            ans= Math.max(ans,i*solveRecDiff(n-i));
+        return memo[n][target]=Math.max(arr[n-1]*solveRec(n,arr,target-arr[n-1]),solveRec(n-1,arr,target));
         }
-        return ans;
-
     }
+    
 }
