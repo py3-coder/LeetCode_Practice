a1af774d9484a5893f4d980d25abcdc920baa8ec
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-13 12:00:47 +0530

    Time: 71 ms (14.41%), Space: 47.3 MB (5.04%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.java
+++ 0087-scramble-string/0087-scramble-string.java
@@ -1,46 +1,61 @@
 class Solution {
-    Map<String, Boolean> mp = new HashMap<>();
-    
-    public boolean helper(String a, String b)
-    {
-        if(a.equals(b) == true) return true;
+    static HashMap<String,Boolean> map =new HashMap<>();
+    public boolean isScramble(String s1, String s2) {
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        if(s1=="" && s2==""){
+            return true;
+        }
+        //return solveRec(A,B)==true?1:0;
+        return solveMemo(s1,s2);
+    }
+    public static boolean solveRec(String a, String b){
+        //Base Case ::
+        int n=a.length();
+        if(a.equals(b)) return true;
+        if(a.length()<=1) return false;
         
-        if(a.length() <= 1) return false;
-        
-        int n = a.length();
-        boolean check = false;
-        
-        String key = a;
-        
-        key += ' ' + b;
-        
-        if(mp.containsKey(key))
-             return mp.get(key);
-        
-        for(int i = 1; i < n; i++)
-        {
-            boolean swap = helper(a.substring(0, i), b.substring(n - i)) && helper(a.substring(i), b.substring(0, n - i));
-            boolean unswap = helper(a.substring(0,i), b.substring(0,i)) && helper(a.substring(i), b.substring(i));
-            
-            if(swap || unswap){
-                check = true;
+        //k loop
+        boolean flag=false;
+        for(int k=1;k<=n-1;k++){
+            boolean test1=false,test2=false;
+            if((solveRec(a.substring(0,k),b.substring(n-k,n))) && (solveRec(a.substring(k,n),b.substring(0,n-k)))){
+                test1=true;
+            }
+            if((solveRec(a.substring(0,k),b.substring(0,k))) && (solveRec(a.substring(k+1,n),b.substring(k+1)))){
+                test2=true;
+            }
+            if(test1 || test2){
+                flag=true;
                 break;
             }
+            
         }
-        
-        mp.put(key, check);
-        
-        return check;
+        return flag;
     }
-    
-    public boolean isScramble(String a, String b)
-    {
-        if(a.equals(b) == true) return true;
-        
-        if(a.length() != b.length()) return false;
-        
-        if(a.isEmpty() == true) return true;
-        
-        return helper(a, b);
+    public static boolean solveMemo(String a, String b){
+        //Base Case ::
+        int n=a.length();
+        if(a.equals(b)) return true;
+        if(a.length()<=1) return false;
+        String key =a+" "+b;
+        if(map.containsKey(key)){
+            return (boolean)map.get(key);
+        }
+        //k loop
+        boolean flag=false;
+        for(int k=1;k<=n-1;k++){
+            boolean test1=false,test2=false;
+            test1 =((solveMemo(a.substring(0,k),b.substring(n-k,n)))&& (solveMemo(a.substring(k,n),b.substring(0,n-k))));
+            test2 =((solveMemo(a.substring(0,k),b.substring(0,k))) && (solveMemo(a.substring(k,n),b.substring(k,n))));
+            if(test1 || test2){
+                flag=true;
+                break;
+            }
+            
+        }
+        map.put(key,flag);
+        return flag;
     }
 }
