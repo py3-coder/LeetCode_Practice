a555e41437d3d80266939b945768786a729bd4c0
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-17 11:50:28 +0530

    Time: 43 ms (47.46%), Space: 71.6 MB (18.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -8,7 +8,11 @@ class Solution {
         // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
         // return solveMemo(0,-1,nums);
 
-        return solveTab(nums);
+        //return solveTab(nums);
+
+        //return solveOptimisationSpace(nums);
+
+        return solveLCS(nums);
 
     }
     public static int solveRec(int indx,int prev,int nums[]){
@@ -42,7 +46,7 @@ class Solution {
     public static int solveTab(int nums[]){
         //Tabulation::
         //Base :-> already handle due deafult value::
-        //since we did co-ordinate shift:: 
+        //since we did co-ordinate shift:: prev index = index+1:
         int n=nums.length;
         int tab[][] = new int[n+1][n+1];
 
@@ -58,8 +62,44 @@ class Solution {
             }
         }
         return tab[0][-1+1];
-         
-
-
     }
+    public static int solveOptimisationSpace(int nums[]){
+        //Observation :: 
+        int n=nums.length;
+        int curr[] = new int[n+1];
+        int next[] =new int[n+1];
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                int pick =0;
+                if(j==-1 || nums[j]<nums[i]){
+                    pick =1+next[i+1];
+                }
+                int notpick =next[j+1];
+                curr[j+1] =Math.max(pick,notpick);
+            }
+            next = curr.clone();
+        }
+        return curr[0];
+    }
+    public static int solveLCS(int nums[]){
+        int n=nums.length;
+        int lcs[] = new int[n+1];
+        Arrays.fill(lcs,1);
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    lcs[i] =Math.max(lcs[i],lcs[j]+1);
+                }
+            }
+        }
+        int maxi=0;
+        for(int ele:lcs){
+            maxi=Math.max(ele,maxi);
+        }
+        return maxi;
+    }
+
+    
 }
