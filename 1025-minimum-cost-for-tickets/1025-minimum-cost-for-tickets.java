d0b69b7cace22cf4629d071a9386655844f4521d
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-17 11:07:05 +0530

    Time: 1 ms (80.14%), Space: 40.6 MB (31.89%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -1,11 +1,18 @@
 class Solution {
     static int[][] memo= new int[500][500];
+    static int[]memoB= new int[366];
     public int mincostTickets(int[] days, int[] costs) {
+        //Recursion :: TC =O(3^n)
+        // SC : O(1)+O(n)
         //return solveRec(0,-1,days,costs);
 
-        Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
-        return solveMemo(0,0,days,costs);
-        
+        // Memoisation ::
+        // TC : O(n*n)
+        // SC: O(n*n) +O(n)
+        // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
+        // return solveMemo(0,0,days,costs);
+        Arrays.fill(memoB,-1);
+        return solveMemoBS(0,days,costs);
     }
     public static int solveRec(int indx,int validity,int[] days,int[] cost){
         //Base Case::
@@ -38,6 +45,39 @@ class Solution {
         }else{
             return memo[indx][validity] =solveMemo(indx+1,validity,days,cost);
         }
+    }
+    public static int solveMemoBS(int indx,int[] days,int[] cost){
+        //Base Case::
+        if(indx>=days.length) return 0;
+        if(memoB[indx]!=-1){
+            return memoB[indx];
+        }
+        int day = binarySearch(indx,days[indx]+1,days);
+        int oneday = cost[0]+solveMemoBS(day,days,cost);
+        
+        day =binarySearch(indx,days[indx]+7,days);
+        int sevenday =cost[1] +solveMemoBS(day,days,cost);
+
+        day =binarySearch(indx,days[indx]+30,days);
+        int thirtyday =cost[2]+solveMemoBS(day,days,cost);
+
+        return memoB[indx]=Math.min(oneday,Math.min(sevenday,thirtyday));
+        
+    }
+    //trying to find the first target index if not present first greatest value index to it::
+    public static int binarySearch(int low,int target,int days[]){
+        int high=days.length-1;
+        while(low<=high){
+            int mi =low+(high-low)/2;
+            if(days[mi]==target){
+                return mi;
+            }else if(days[mi]>target){
+                high=mi-1;
+            }else{
+                low=mi+1;
+            }
+        }
+        return low;
     }
 
 }
