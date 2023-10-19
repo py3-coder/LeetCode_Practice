6a9b5c7e54526b6e07ac3c941fa66d38abf0702b
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-19 16:13:28 +0530

    Time: 0 ms (100%), Space: 43.5 MB (42.5%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,7 +10,24 @@
  * }
  */
 public class Solution {
+    
     public boolean hasCycle(ListNode head) {
+        //edge case::
+        if(head==null || head.next ==null) return false;
+
+        //Two pointer Approch ::
+        // TC : O(N)  SC :O(1)
+        //return check(head);
+
+        // Using Set::
+        // TC : O(N) SC: O(N)
+        //return solveSet(head);
+
+        // Recursive 
+        // TC : O(n)   SC:O(1)
+        return solveRec(head.next,head.next.next);
+    }
+    public static boolean check(ListNode head){
         ListNode slow = head ,fast = head;
         while(fast !=null && fast.next !=null){
             slow = slow.next;
@@ -20,5 +37,27 @@ public class Solution {
             return true;
         }
         return false;
+    }
+    public static boolean solveSet(ListNode head){
+        HashSet<ListNode> set = new HashSet<>();
+        
+
+        while(head.next!=null){
+            if(set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head=head.next;
+        }
+        return false;
+    }
+    // Lets Try Recursion::
+    // TC : O(n)  SC : O(1)
+    public static boolean solveRec(ListNode slow,ListNode fast){
+        if(slow == fast){
+            return true;
+        }
+        if(fast==null || slow==null || fast.next==null) return false;
+        return solveRec(slow.next,fast.next.next);
     }
 }
