d20746ee99f9c4672ff655c359621b3c86738685
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-20 17:35:35 +0530

    Time: 1 ms (89.41%), Space: 43.3 MB (51.32%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,36 +1,30 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        ArrayList<String> res = new ArrayList<>();
-        if(n==0){
-            return res;
-        }
-        int open=n;
-        int close=n;
-        String op="";
-        genrate(open, close,op, res);
+        List<String> res = new ArrayList<>();
+        String str="";
+        solveRec(n,n,str,res);
         return res;
         
     }
-    public static void genrate(int open,int close,String op,ArrayList<String> res){
+    public static void solveRec(int open,int close,String op,List<String> temp){
         if(open==0 && close==0){
-            res.add(op);
-            return;
+            temp.add(op);
+            return ;
         }
-        if(open==close){
-            op+="(";
-            genrate(open-1, close, op, res);
+        if(open==0 && close!=0){
+            String op2 =op+')';
+            solveRec(open,close-1,op2,temp);
         }
-        if(open<close){
-            String op1 =op;
-            String op2 =op;
-            //pick close::
-            op1+=")";
-            genrate(open, close-1, op1, res);
-            if(open!=0){
-                //not pick close means we have to pick open :)
-                op2+="(";
-                genrate(open-1, close, op2, res);
-            }
+        else if(open<close){
+            //2 choices ::
+            String op1=op+'(';
+            String op2 =op+')';
+            solveRec(open-1,close,op1,temp);
+            solveRec(open,close-1,op2,temp);
+            
+        }else if(open==close){
+            String op1 =op+'(';
+            solveRec(open-1,close,op1,temp);
         }
     }
 }
