a59393fa179c77870088ba11e8288d4455abe62f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-20 12:23:08 +0530

    Time: 8 ms (98.39%), Space: 56 MB (45.12%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,63 +10,77 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        //Brute Force ::
-        // ListNode temp = head;
-        // ArrayList<Integer> lis = new ArrayList<>();
-        // while(temp!=null){
-        //     lis.add(temp.val);
-        //     temp=temp.next;
-        // }
-        // temp=head;
-        // Collections.sort(lis);
-        // for(int val:lis){
-        //     temp.val =val;
-        //     temp=temp.next;
-        // }
-        // return head;
+        //Apprcoh 1
+        // Traverse the linklist till last and put into list sort it and create head pointer append new node
+        // till it have data::
         // TC : O(n)+O(nlogn)+O(n)
-        //SC : O(n)
+        //SC : O(n) +O(n) ~ (if we talk of n new pointer we need as O(1) for each nd for n we need O(n) i.e
+       // return sort(head);
+
+        //Approch 2:
+        //Apply Merge Sort - Best Sorting Algo
+        // TC : O(nlogn)
+        // SC : O(logn)
+        return mergeSortRec(head);
         
-        if(head==null || head.next==null){
-            return head;
+        
+    }
+    public ListNode sort(ListNode head){
+        ListNode temp = head;
+        ArrayList<Integer> lis = new ArrayList<>();
+        while(temp!=null){
+            lis.add(temp.val);
+            temp=temp.next;
         }
-        ListNode fast=head;
+        temp=head;
+        Collections.sort(lis);
+        for(int val:lis){
+            temp.val =val;
+            temp=temp.next;
+        }
+        return head;
+    }
+    //2. Approch ::
+    public static ListNode mergeSortRec(ListNode head){
+        // Base Case:
+        if(head==null || head.next==null) return head;
+
         ListNode slow=head;
-        ListNode prev =null;
-        
+        ListNode prev=null;
+        ListNode fast=head;
         while(fast!=null && fast.next!=null){
             prev=slow;
             slow=slow.next;
             fast=fast.next.next;
         }
-        prev.next=null;
-        ListNode l1 =sortList(head);
-        ListNode l2 =sortList(slow);
-        
-        return merge(l1,l2);
-        
+        prev.next =null;
+
+        ListNode part1 =mergeSortRec(head);
+        ListNode part2 =mergeSortRec(slow);
+        return merge(part1,part2);
+
     }
-    public static ListNode merge(ListNode l1,ListNode l2){
-        ListNode l =new ListNode(0);
-        ListNode p=l;
-        
-        while(l1!=null && l2!=null){
-            if(l1.val<l2.val){
-                p.next =l1;
-                l1=l1.next;
+    public static ListNode merge(ListNode temp1,ListNode temp2){
+        ListNode dummy =new ListNode(0);
+        ListNode temp =dummy;
+
+        while(temp1!=null && temp2!=null){
+            if(temp1.val<temp2.val){
+                temp.next =temp1;
+                temp1 =temp1.next;
             }else{
-                p.next=l2;
-                l2 =l2.next;
+                temp.next =temp2;
+                temp2 =temp2.next;
             }
-            p =p.next;
+            temp=temp.next;
         }
-        if(l1!=null){
-        p.next=l1;
-    }
-    if(l2!=null){
-        p.next=l2;
-    }
-    return l.next;
+        if(temp1!=null){
+            temp.next=temp1;
+        }
+        if(temp2!=null){
+            temp.next=temp2;
+        }
+        return dummy.next;
     }
     
 }
