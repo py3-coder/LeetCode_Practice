def452b6dda1868a484c6f8724f88cce49887259
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-20 11:36:08 +0530

    Time: 0 ms (100%), Space: 40.5 MB (41.34%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,6 +10,17 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 1. Approch :: find len and find len-k:
+        // traverse form head to len-k-1 :: update the pointer:
+        // TC : O(n) +O(n-len-1)
+        // SC : O(1)
+
+
+        // 2. Approch Gap Method ::
+        // take two pointer create k node gap::
+        /// when first reach at last then sec must be at before k node at back:
+
+
         //Gap type concept let make fast k node ahead from start .. that we will mentain till last::
         ListNode start = new ListNode();
         start.next = head;
