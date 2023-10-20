1e5a1648a59bbda9fa28f49f255cbff3cdab0b15
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-20 14:10:21 +0530

    Time: 15 ms (14.16%), Space: 54 MB (93.69%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -21,9 +21,12 @@ class Solution {
         //Apply Merge Sort - Best Sorting Algo
         // TC : O(nlogn)
         // SC : O(logn)
-        return mergeSortRec(head);
-        
-        
+        //return mergeSortRec(head);
+
+        // Approch 3:
+        //Apply Merge Sort  -- trying to optimise space:
+        //Iterative approch we can save recursion stack call::
+        return sortIter(head);
     }
     public ListNode sort(ListNode head){
         ListNode temp = head;
@@ -82,5 +85,63 @@ class Solution {
         }
         return dummy.next;
     }
+    // Approch 3::
+    // Iterative Merge Sort LinkList::
+    public static ListNode sortIter(ListNode head){
+        if(head==null || head.next==null) return head;
+        ListNode cur = head;
+        int len = length(head);
+        ListNode left,right,dummy,tail;
+        for(int i=1;i<len;i*=2){
+            dummy =new ListNode(0);
+            cur=head;
+            tail =dummy;
+            while(cur!=null){
+                left =cur;
+                right =split(left,i);
+                cur =split(right,i);
+                tail=mergenew(left,right,tail);
+            }
+            head=dummy.next;
+        }
+        return head;
+    }
+    public static int length(ListNode head){
+        int len=0;
+        while(head!=null){
+            head=head.next;
+            len++;
+        }
+        return len;
+    }
+    public static ListNode split(ListNode head,int n){
+        for(int i=1;head!=null && i<n;i++){
+            head=head.next;
+        }
+        if(head==null) return null;
+        ListNode right =head.next;
+        head.next=null;
+        return right;
+    }
+    public static ListNode mergenew(ListNode l1, ListNode l2, ListNode head) {
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) cur.next = l1;
+        if (l2 != null) cur.next = l2;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+
     
 }
