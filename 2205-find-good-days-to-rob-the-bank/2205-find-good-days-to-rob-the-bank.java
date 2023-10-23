5a350ebc7700bb7d5ec74b77de153ab0e430cb0d
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-23 11:42:13 +0530

    Time: 6 ms (65.83%), Space: 58 MB (55.78%) - LeetHub

---
+++ 2205-find-good-days-to-rob-the-bank/2205-find-good-days-to-rob-the-bank.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> goodDaysToRobBank(int[] arr, int time) {
+        //Broute Force ::
+        // TC : O(n*t)   SC: O(1)+O(k) k-- good days count:
+
+        //Optimal ::
+        int len =arr.length;
+        int increase[] = new int[len];
+        int decrease[] = new int[len];
+
+        for(int i=1;i<len;i++){
+            if(arr[i]<=arr[i-1]){
+                decrease[i]=1+decrease[i-1];
+            }
+        }
+
+        for(int j=len-2;j>=0;j--){
+            if(arr[j]<=arr[j+1]){
+                increase[j] =1+increase[j+1];
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<len;i++){
+            if(increase[i]>=time && decrease[i]>=time){
+                ans.add(i);
+            }
+        }
+        return ans;
+
+
+
+    }
+}
