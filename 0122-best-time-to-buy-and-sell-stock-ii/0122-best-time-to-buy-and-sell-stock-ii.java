be6f1149e43c9a311d0993d0187cee7b4ad9921c
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-25 16:22:12 +0530

    Time: 4 ms (6.31%), Space: 44.6 MB (19.13%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -4,8 +4,10 @@ class Solution {
         //Recursion::)
         //return solveRec(prices,0,1);
 
-        Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
-        return solveMemo(prices,0,1);
+        // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
+        // return solveMemo(prices,0,1);
+
+        return solveTab(prices);
         
     }
     public static int solveRec(int[] prices ,int indx,int buy){
@@ -34,5 +36,26 @@ class Solution {
         }
         int skip =solveMemo(prices,indx+1,buy);
         return memo[indx][buy]=Math.max(profit,skip);
+    }
+    public static int solveTab(int[] prices){
+        int n=prices.length;
+        int tab[][] =new int[n+1][2];
+        Arrays.stream(tab).forEach(a->Arrays.fill(a,-1));
+        tab[n][0]=0;
+        tab[n][1]=0;
+
+        int profit=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                if(j==1){
+                    profit = Math.max(prices[i]+tab[i+1][0],tab[i+1][1]);
+                }
+                if(j==0){
+                    profit = Math.max(-prices[i]+tab[i+1][1],tab[i+1][0]);
+                }
+                tab[i][j]=profit;
+            }
+        }  
+        return tab[0][0];      
     }
 }
