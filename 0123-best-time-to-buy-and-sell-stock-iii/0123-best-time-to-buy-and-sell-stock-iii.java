958256fe389bea66231caab7420b7225427b6162
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-25 20:19:55 +0530

    Time: 10 ms (49.66%), Space: 56.1 MB (63.75%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -8,7 +8,9 @@ class Solution {
         // HashMap<String,Integer> map =new HashMap<>();
         // return solveMemo(prices,0,0,0,map);
 
-        return solveTab(prices);
+        //return solveTab(prices);
+
+        return solveTabSpaceOptmised(prices);
         
     }
     public static int solveRec(int[] prices,int indx,int buy,int cnt){
@@ -26,26 +28,6 @@ class Solution {
         int skip =solveRec(prices,indx+1,buy,cnt);
         return Math.max(skip,profit);
     }
-    public static int solveMemo(int[] prices,int indx,int buy,int cnt,HashMap<String,Integer> map){
-        if(indx==prices.length) return 0;
-        if(cnt>2) return 0;
-
-        String key = indx+"-"+buy+"-"+cnt;
-        if(map.containsKey(key)){
-            return map.get(key);
-        }
-        int profit=0;
-        if(buy==0){
-            // buy::
-            profit =-prices[indx]+solveMemo(prices,indx+1,1,cnt+1,map);
-        }else{
-            //sell::
-            profit =prices[indx]+solveMemo(prices,indx+1,0,cnt,map);
-        }
-        int skip =solveMemo(prices,indx+1,buy,cnt,map);
-        map.put(key,Math.max(skip,profit));
-        return Math.max(skip,profit);
-    }
     public static int solveTab(int[] prices){
         //Tabulation::)
         int n=prices.length;
@@ -54,12 +36,11 @@ class Solution {
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 for(int cap=1;cap<=2;cap++){
-                     if (buy == 0) { // We can buy the stock
+                     if (buy == 0) {
                         tab[ind][buy][cap] = Math.max(0 + tab[ind + 1][0][cap],
                                 -prices[ind] + tab[ind + 1][1][cap]);
                     }
-
-                    if (buy == 1) { // We can sell the stock
+                    if (buy == 1) { 
                         tab[ind][buy][cap] = Math.max(0 + tab[ind + 1][1][cap],
                                 prices[ind] + tab[ind + 1][0][cap - 1]);
                     }
@@ -67,5 +48,34 @@ class Solution {
             }
         }
         return  tab[0][0][2];
+    }
+    public static int solveTabSpaceOptmised(int[] prices){
+        //Sapce Optimied:: Tabulation::)
+        int n=prices.length;
+        
+        int ahead[][] = new int[2][3];
+        int curr[][] = new int[2][3];
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                     if (buy == 0) {
+                        curr[buy][cap] = Math.max(0 + ahead[0][cap],
+                                -prices[ind] + ahead[1][cap]);
+                    }
+                    if (buy == 1) { 
+                        curr[buy][cap] = Math.max(0 + ahead[1][cap],
+                                prices[ind] + ahead[0][cap - 1]);
+                    }
+                }
+            }
+            for (int i = 0; i < 2; i++) {
+                for (int j = 1; j < 3; j++) {
+                    ahead[i][j] = curr[i][j];
+                }
+            }
+        }
+        
+        return  ahead[0][2];
     }
 }
