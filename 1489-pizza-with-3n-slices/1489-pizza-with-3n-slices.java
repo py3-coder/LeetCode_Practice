d9c2ccef6efb827fb37f107ec9bcb17014ea8047
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-26 17:10:31 +0530

    Time: 7 ms (75%), Space: 73.1 MB (5.47%) - LeetHub

--- 1489-pizza-with-3n-slices/1489-pizza-with-3n-slices.java
+++ 1489-pizza-with-3n-slices/1489-pizza-with-3n-slices.java
@@ -3,17 +3,29 @@ class Solution {
     public int maxSizeSlices(int[] slices) {
         // Similar to House Robber 2 :
         int n=slices.length;
+        // k= n/3 always:
+
+        // TC : O(2^n)  SC:O(1)+O(n)
         // int p1 =solveRec(slices,0,n-2,n/3);
         // int p2 =solveRec(slices,1,n-1,n/3);
+        // return Math.max(p1,p2);
 
+        // TC : O(n*k)  SC:O(n*k)+O(n)
         // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
         // int p1 =solveMemo(slices,0,n-2,n/3);
         // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
         // int p2 =solveMemo(slices,1,n-1,n/3);
+        // return Math.max(p1,p2);
+
+        // TC : O(n*k) SC: O(n*k)
+        // int p1 =solveTab(slices,0,n-2,n/3);
+        // int p2 =solveTab(slices,1,n-1,n/3);
+        // return Math.max(p1,p2);
 
 
-        int p1 =solveTab(slices,0,n-2,n/3);
-        int p2 =solveTab(slices,1,n-1,n/3);
+        // TC: O(n*k)  SC:O(k)
+        int p1 =solveTabOptmised(slices,0,n-2,n/3);
+        int p2 =solveTabOptmised(slices,1,n-1,n/3);
         return Math.max(p1,p2);
     }
     public static int solveRec(int[] slices,int indx,int n,int k){
@@ -44,5 +56,21 @@ class Solution {
             }
         }
         return tab[i][s];
+    }
+    public static int solveTabOptmised(int[] slices,int i,int n,int s){
+        int curr[]= new int[s+1];
+        int ahead[]= new int[s+1];
+        int ahead2[]= new int[s+1];
+
+        for(int indx=n;indx>=i;indx--){
+            for(int k=1;k<=s;k++){
+                int pick = slices[indx]+ahead2[k-1];
+                int skip = ahead[k];
+                curr[k]=Math.max(pick,skip);
+            }
+            System.arraycopy(ahead,0,ahead2,0,s+1);
+            System.arraycopy(curr,0,ahead,0,s+1);
+        }
+        return curr[s];
     }
 }
