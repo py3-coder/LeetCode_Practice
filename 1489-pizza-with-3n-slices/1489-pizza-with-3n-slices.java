dd137293d2bd5ad4368187ad5270dd343f30b08f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-26 16:43:12 +0530

    Time: 8 ms (44.53%), Space: 73 MB (5.47%) - LeetHub

--- 1489-pizza-with-3n-slices/1489-pizza-with-3n-slices.java
+++ 1489-pizza-with-3n-slices/1489-pizza-with-3n-slices.java
@@ -1,15 +1,19 @@
 class Solution {
     static int memo[][] =new int[15001][501];
     public int maxSizeSlices(int[] slices) {
+        // Similar to House Robber 2 :
         int n=slices.length;
         // int p1 =solveRec(slices,0,n-2,n/3);
         // int p2 =solveRec(slices,1,n-1,n/3);
 
-        Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
-        int p1 =solveMemo(slices,0,n-2,n/3);
-        Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
-        int p2 =solveMemo(slices,1,n-1,n/3);
+        // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
+        // int p1 =solveMemo(slices,0,n-2,n/3);
+        // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
+        // int p2 =solveMemo(slices,1,n-1,n/3);
 
+
+        int p1 =solveTab(slices,0,n-2,n/3);
+        int p2 =solveTab(slices,1,n-1,n/3);
         return Math.max(p1,p2);
     }
     public static int solveRec(int[] slices,int indx,int n,int k){
@@ -29,5 +33,16 @@ class Solution {
         int pick = slices[indx] +solveMemo(slices,indx+2,n,k-1);
         int skip =solveMemo(slices,indx+1,n,k);
         return memo[indx][k]=Math.max(pick,skip);
+    }
+    public static int solveTab(int[] slices,int i,int n,int s){
+        int tab[][]= new int[n+3][s+1];
+        for(int indx=n;indx>=i;indx--){
+            for(int k=1;k<=s;k++){
+                int pick = slices[indx]+tab[indx+2][k-1];
+                int skip = tab[indx+1][k];
+                tab[indx][k]=Math.max(pick,skip);
+            }
+        }
+        return tab[i][s];
     }
 }
