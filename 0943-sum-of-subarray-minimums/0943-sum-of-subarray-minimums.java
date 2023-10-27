dc0cc309531289c4a5cb131f979e341ceab5e3f0
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-27 16:20:25 +0530

    Time: 66 ms (15.08%), Space: 46.1 MB (79.26%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -1,47 +1,49 @@
 class Solution {
+    
     public int sumSubarrayMins(int[] arr) {
-        //Broute Force :: 
-        // TC :O(n^2) 
-        // SC : O(1)
-        long mod = (long)1000000007;;
-        //Lets Implement Using Stack::
+        //Broute Force ::
+        // TC: O(n^2) SC: O(1);;
+        // Approch : i loop and take j loop iterate over it:) from 0 to i
+        // J loop depends on i::) Stack Can helps us to optimised:)
+        long mod = (long)1000000007;
         int n=arr.length;
-        int left[] =new int[n];
-        int right[] = new int[n];
-
-        Stack<Integer> st =new Stack<>();
+        int nsl[] = new int[n];
+        int nsr[] =new int[n];
+        Stack<Integer> st = new Stack<>();
+        // nsl
         for(int i=0;i<n;i++){
             if(st.isEmpty()){
-                left[i] =i+1;
+                nsl[i] = i+1;
             }else if(arr[st.peek()]<arr[i]){
-                left[i] =i-st.peek();
-            }else{
+                nsl[i] =i-st.peek();
+            }else {
                 while(!st.isEmpty() && arr[st.peek()]>arr[i]){
                     st.pop();
                 }
                 if(st.isEmpty()){
-                    left[i] =i+1;
+                    nsl[i]=i+1;
                 }else{
-                    left[i]=i-st.peek();
+                    nsl[i] =i-st.peek();
                 }
-                
             }
             st.push(i);
         }
+
         st.clear();
+        //nsr::
         for(int i=n-1;i>=0;i--){
             if(st.isEmpty()){
-                right[i] =n-i;
+                nsr[i] = n-i;
             }else if(arr[st.peek()]<arr[i]){
-                right[i] =st.peek()-i;
+                nsr[i]=st.peek()-i;
             }else{
                 while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
                     st.pop();
                 }
                 if(st.isEmpty()){
-                    right[i]=n-i;
+                    nsr[i]=n-i;
                 }else{
-                    right[i]=st.peek()-i;
+                    nsr[i]=st.peek()-i;
                 }
             }
             st.push(i);
@@ -49,11 +51,12 @@ class Solution {
 
         int res=0;
         for(int i=0;i<n;i++){
-            long temp=((long)arr[i]*left[i]*right[i])%mod;
-            res+=temp;
+            //System.out.println(nsl[i]+" "+nsr[i]);
+            long ans=((long)arr[i]*nsl[i]*nsr[i])%mod;
+            res+=ans;
             res%=mod;
         }
         return res;
-
+        
     }
 }
