a9d7a8714997cfb9f6325b690865cb6efab3a794
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-27 13:57:09 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.java
+++ Next Greater Element - GFG/next-greater-element.java
@@ -9,35 +9,37 @@ class GFG {
     
 	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter ot = new PrintWriter(System.out);
 		int t = Integer.parseInt(br.readLine().trim());
 		while(t-->0){
 		    int n = Integer.parseInt(br.readLine().trim());
 		    String inputLine[] = br.readLine().trim().split(" ");
 		    long[] arr = new long[n];
 		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
-		    Solution ob = new Solution();
-		    long[] res = ob.nextLargerElement(arr, n);
+		    long[] res = new Solution().nextLargerElement(arr, n);
 		    for (int i = 0; i < n; i++) 
-		        ot.print(res[i] + " ");
-		    ot.println();
+		        System.out.print(res[i] + " ");
+		    System.out.println();
 		}
-        ot.close();
 	}
 }
+
 
 
 
 // } Driver Code Ends
 
 
-// User Function Template for JAVA
-
-class Solution{
-    public static long[] nextLargerElement(long[] arr, int n) { 
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
         // Your code here
+        //Lets play with Stack::
+        //Brute Force :::
         long[] res = new long[n];
-        Stack<Long> st = new Stack<>();
+        int k=n-1;
+        Stack<Long> st = new Stack();
         for(int i=n-1;i>=0;i--){
             if(st.isEmpty()){
                 res[i] =-1;
@@ -50,7 +52,7 @@ class Solution{
                 if(st.isEmpty()){
                     res[i] =-1;
                 }else{
-                    res[i] = st.peek();
+                    res[i] =st.peek();
                 }
             }
             st.push(arr[i]);
