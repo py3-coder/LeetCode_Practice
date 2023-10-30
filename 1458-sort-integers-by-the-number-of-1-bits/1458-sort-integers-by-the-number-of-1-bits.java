709f0583a99ba04dfe9820d345b7b75ab5d4b1a9
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-30 22:23:05 +0530

    Time: 3 ms (99.13%), Space: 44.2 MB (13.79%) - LeetHub

---
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        int n = arr.length;
+        int res[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = countBit(arr[i]) * 10001 + arr[i];
+        }
+        Arrays.sort(res);
+        for (int i = 0; i < n; i++) {
+            res[i] %= 10001;
+        }
+        return res;
+    }
+
+    private int countBit(int n) {
+        int res = 0;
+        while (n != 0) {
+            res += (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+}
