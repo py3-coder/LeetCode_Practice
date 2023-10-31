3e31d3b61dd508656b46076a4862c65888c5a88a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-10-31 12:07:12 +0530

    Time: 2 ms (59.65%), Space: 59 MB (46.81%) - LeetHub

--- 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.java
+++ 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.java
@@ -1,12 +1,20 @@
 class Solution {
+    // public int[] findArray(int[] pref) {
+    //     int prev =pref[0];
+    //     int res[] =new int[pref.length];
+    //     res[0] =prev;
+    //     for(int i=1;i<pref.length;i++){
+    //         res[i] =prev^pref[i];
+    //         prev = pref[i];
+    //     }
+    //     return res;
+    // }
     public int[] findArray(int[] pref) {
         int prev =pref[0];
-        int res[] =new int[pref.length];
-        res[0] =prev;
         for(int i=1;i<pref.length;i++){
-            res[i] =prev^pref[i];
-            prev = pref[i];
+            pref[i] =pref[i]^prev;
+            prev ^= pref[i];
         }
-        return res;
+        return pref;
     }
 }
