6670470c736940c89cb50328db5c4e07ecfc9d5e
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-07 11:55:28 +0530

    Added solution - LeetHub

---
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        ArrayList<Integer> res = new ArrayList<>();
+        int start =0;
+        int end=0;
+        int sum=0;
+        while(end<arr.length){
+            sum+=arr[end];
+            if(sum>=s){
+                while(sum>s && start<end){
+                    sum-=arr[start];
+                    start++;
+                }
+                if(sum==s){
+                res.add(start+1);
+                res.add(end+1);
+                return res;
+                }
+            }
+            end++;
+        }
+        res.add(-1);
+        return res;
+    }
+}
