6e7d64b0d2683e23ce3d59681e6341bd078a5753
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-08 16:20:42 +0530

    Time: 7 ms (39.45%), Space: 43.6 MB (58.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -2,31 +2,32 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         //Sliding Window--Template ---Varibale Size window....
         int n=s.length();
-        int res =0;
-        HashMap<Character,Integer> map = new HashMap<>();
-        int start =0;
+        int start=0;
         int end=0;
+        int maxi=0;
+        HashMap<Character,Integer> map =new HashMap<>();
         while(end<n){
-            char ch = s.charAt(end);
+            char ch =s.charAt(end);
             map.put(ch,map.getOrDefault(ch,0)+1);
-            if(end-start+1<map.size()){
-                end++;
-            }
-            else if(end-start+1==map.size()){
-                res=Math.max(res,end-start+1);
-                end++;
+            if(end-start+1==map.size()){
+                maxi =Math.max(maxi,end-start+1);
             }else if(end-start+1>map.size()){
                 while(end-start+1>map.size()){
-                    char ss = s.charAt(start);
-                    map.put(ss,map.get(ss)-1);
-                    if(map.get(ss)==0){
-                        map.remove(ss);
+                    char cc =s.charAt(start);
+                    if(map.get(cc)==1){
+                        map.remove(cc);   
+                    }
+                    else{
+                        map.put(cc,map.get(cc)-1);
                     }
                     start++;
                 }
-                end++;
+                if(end-start+1==map.size()){
+                    maxi =Math.max(maxi,end-start+1);
+                }
             }
+            end++;
         }
-        return res;
+        return maxi;
     }
 }
