cb0ef2bc0e1394f29f782fff23707ef49b281bf2
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-08 13:43:04 +0530

    Added solution - LeetHub

--- Count the triplets - GFG/count-the-triplets.java
+++ Count the triplets - GFG/count-the-triplets.java
@@ -32,19 +32,43 @@ class GFG {
 class Solution {
     int countTriplet(int arr[], int n) {
         // code here
-        HashSet<Integer> set = new HashSet<>();
-        for(int ele:arr){
-            set.add(ele);
-        }
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int ele:arr){
+        //     set.add(ele);
+        // }
+        // int cnt=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         int val=arr[i]+arr[j];
+        //         if(set.contains(val)){
+        //             cnt++;
+        //         }
+        //     }
+        // }
+        // return cnt;
+        // TC : O(n^2) 
+        // SC : O(n)
+        
         int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int val=arr[i]+arr[j];
-                if(set.contains(val)){
+        Arrays.sort(arr);
+        for(int i=n-1;i>=0;i--){
+            int s =0;
+            int e =i-1;
+            while(s<e){
+                int temp=arr[i];
+                int dummy =arr[s]+arr[e];
+                if(temp==dummy){
                     cnt++;
+                    s++;
+                    e--;
+                }else if(dummy>temp){
+                    e--;
+                }else{
+                    s++;
                 }
             }
         }
         return cnt;
+        // 
     }
 }
