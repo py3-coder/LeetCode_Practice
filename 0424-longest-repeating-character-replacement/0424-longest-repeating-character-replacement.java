6d89717222700b7bbeef681af80f4c9402cd32c4
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-08 17:22:12 +0530

    Time: 40 ms (13.21%), Space: 43.9 MB (12.6%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,10 +1,10 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        // Sliding Window ---
         int n=s.length();
         int res=0;
         HashMap<Character,Integer> map  = new HashMap<>();
         int start=0,end=0;
-        
         while(end<n){
             char ch = s.charAt(end);
             map.put(ch,map.getOrDefault(ch,0)+1);
@@ -19,9 +19,6 @@ class Solution {
                 while((end-start+1)-mostFrequentCharacter>k){
                     char ss = s.charAt(start);
                     map.put(ss,map.get(ss)-1);
-                    if(map.get(ss)==0){
-                         map.remove(ss);
-                    }
                     start++;
                 }
                 end++;
