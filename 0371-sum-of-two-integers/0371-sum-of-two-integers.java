d47d04b185e116521914ad19fb1a060c577ca88b
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-16 17:29:03 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (67.05%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -2,11 +2,11 @@ class Solution {
     public int getSum(int a, int b) {
         //Not Solved :: Copied Idea  form other ::)
         //Cheated IDEA ::
-        return solve(a,b);
+        return solveADD(a,b);
     }
     //TC : O(1)
     // SC: O(1)
-    //Iterative::
+    //Iterative::Addtion::
     public static int solve(int a,int b){
         int carry=0;
         while(b!=0){
@@ -16,6 +16,28 @@ class Solution {
         }
         return a;
     }
-    
-    
+    //Recursive ::)
+    public static int solveADD(int a,int b){
+        return (b==0)?a:solveADD(a^b,(a&b)<<1);
+    }
+    //Iterative Substract ::)
+    public static int solvesub(int a,int b){
+        while(b!=0){
+            int borrow=(~a)&b;
+            a=a^b;
+            b=borrow<<1;
+        }
+        return a;
+    }
+    //Recursive Substract ::
+    public static int solveSUB(int a,int b){
+        return (b==0)?a:solveSUB(a^b ,((~a)&b)<<1);
+    }
+    // Get negative number
+    public static int negate(int x) {
+        return ~x + 1;
+    }
 }
+
+
+
