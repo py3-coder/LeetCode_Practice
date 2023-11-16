b81c59352e1dc87a5311d34c9f4d204f24441050
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-16 11:49:25 +0530

    Time: 2 ms (81.88%), Space: 43.7 MB (32.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        //Broute Force =
+        // TC :O(n^2) -- 2 for loop check to each 2 pairs possible --
+        //SC :O(1)
+        
+        //better -->
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i=0;i<nums.length;i++){
             int b = nums[i], a= target-b;
@@ -9,5 +15,27 @@ class Solution {
             map.put(b,i);
         }
         return new int[]{};
+    
+    //TC :  O(n)
+    //SC : O(n)
+    
+    //Lets Optimise :: Using Two pointer ::
+           
+//         int left =0;
+//         int right =n-1;
+//         while(left<right){
+//             int sum =nums[left]+nums[right];
+//             if(sum==target){
+//                 return new int[]{left,right};
+//             }else if(sum>target){
+//                 right--;
+//             }else{
+//                 left++;
+//             }
+//         }
+//         return new int[]{};
+        
+        //TC : O(nlogn)
+        //Sc: O(1)
     }
 }
