29c5f8d07859ea53500b5a53fb2e99c9785e64fa
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-16 23:12:09 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (33.18%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,12 +1,10 @@
 class Solution {
+    //TC :O(1)
     public int minFlips(int a, int b, int c) {
         int num=(a|b);
         int and=(a&b);
         int cnt=0;
-        int maxi=Math.max(num,c);
-        while(maxi!=0){
-            // System.out.print((c&1)+"-");
-            // System.out.print((num&1)+",");
+        while(c>0 || num>0){
             if((c&1)!=(num&1)){
                 if((c&1)==1){
                     cnt++;
@@ -21,7 +19,6 @@ class Solution {
             c>>=1;
             num>>=1;
             and>>=1;
-            maxi>>=1;
         }
         return cnt;
     }
