a4afb24c038023e12e378a015c33d88f61a9a636
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-17 13:01:34 +0530

    Time: 8 ms (98.06%), Space: 56.9 MB (51.15%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -1,13 +1,40 @@
 class Solution {
     public int minPairSum(int[] nums) {
-        Arrays.sort(nums);
+        //TC :O(nlogn) +O(n)
+        // SC :O(1)
+        // Arrays.sort(nums);
+        // int maxi=0;
+        // int left=0;
+        // int right=nums.length-1;
+        // while(left<=right){
+        //     maxi =Math.max(nums[left]+nums[right],maxi);
+        //     left++;
+        //     right--;
+        // }
+        // return maxi;
+        
+        //TC : O(Math.max(k,max)) 
+        //SC: O(1)
+        
         int maxi=0;
-        int left=0;
-        int right=nums.length-1;
-        while(left<=right){
-            maxi =Math.max(nums[left]+nums[right],maxi);
-            left++;
-            right--;
+        int mini=Integer.MAX_VALUE;
+        int[] cnt=new int[100001];
+        for(int i=0;i<nums.length;i++){
+            cnt[nums[i]]++;
+            maxi =Math.max(nums[i],maxi);
+            mini =Math.min(nums[i],mini);
+        }
+        int l=mini;
+        int h=maxi;
+        maxi=0;
+        while(l<=h){
+            if(cnt[l]==0) l++;
+            else if(cnt[h]==0) h--;
+            else{
+                maxi =Math.max(h+l,maxi);
+                cnt[l]--;
+                cnt[h]--;
+            }
         }
         return maxi;
     }
