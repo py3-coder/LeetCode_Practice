b9d6b09f26e7a5c750c64595196fa69aba199b21
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-17 17:32:38 +0530

    Time: 1 ms (39.21%), Space: 39.8 MB (96.97%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> left=new Stack<>();
+        Stack<Integer> star =new Stack<>();
+        int i=0;
+        for(char ch : s.toCharArray()){
+            if(ch=='('){
+                left.push(i);
+                i++;
+            }else if(ch=='*'){
+                star.push(i);
+                i++;
+            }else{
+                if(left.isEmpty() && star.isEmpty()) return false;
+                if(!left.isEmpty()){
+                    left.pop();
+                }else{
+                    star.pop();
+                }
+                
+            }
+        }
+        while(!left.isEmpty() && !star.isEmpty()){
+            if(left.pop()>star.pop()){
+                return false;
+            }
+        }
+        return left.isEmpty();
+        
+    }
+}
