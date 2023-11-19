94004fd3569b73921d51f7bf57489998f478977a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-19 10:20:27 +0530

    Time: 34 ms (81.58%), Space: 54.7 MB (48.03%) - LeetHub

---
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int reductionOperations(int[] nums) {
+        Arrays.sort(nums);
+        int[] arr =new int[nums.length];
+        int prev =nums[0];
+        arr[0]=0;
+        int cnt=0;
+        for(int i=1;i<nums.length;i++){
+            if(prev<nums[i]){
+                cnt++;
+                arr[i]=cnt;
+                prev =nums[i];
+            }else{
+                arr[i]=cnt;
+            }
+        }
+        int res=0;
+        for(int i=0;i<arr.length;i++){
+            res+=arr[i];
+        }
+        return res;
+    }
+}
