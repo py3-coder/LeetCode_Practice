a3f3dc41c14bcb06a861f4f97ac7955c1ae55f3f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-19 10:25:45 +0530

    Time: 34 ms (81.58%), Space: 54.6 MB (55.26%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
@@ -1,23 +1,30 @@
 class Solution {
     public int reductionOperations(int[] nums) {
         Arrays.sort(nums);
-        int[] arr =new int[nums.length];
-        int prev =nums[0];
-        arr[0]=0;
-        int cnt=0;
-        for(int i=1;i<nums.length;i++){
-            if(prev<nums[i]){
-                cnt++;
-                arr[i]=cnt;
-                prev =nums[i];
-            }else{
-                arr[i]=cnt;
+        // int[] arr =new int[nums.length];
+        // int prev =nums[0];
+        // arr[0]=0;
+        // int cnt=0;
+        // for(int i=1;i<nums.length;i++){
+        //     if(prev<nums[i]){
+        //         cnt++;
+        //         arr[i]=cnt;
+        //         prev =nums[i];
+        //     }else{
+        //         arr[i]=cnt;
+        //     }
+        // }
+        // int res=0;
+        // for(int i=0;i<arr.length;i++){
+        //     res+=arr[i];
+        // }
+        // return res;
+        int minOps = 0;
+        for (int i=nums.length-1; i>0; i--) {
+            if (nums[i] != nums[i-1]) {
+                minOps += nums.length - i;
             }
         }
-        int res=0;
-        for(int i=0;i<arr.length;i++){
-            res+=arr[i];
-        }
-        return res;
+        return minOps;
     }
 }
