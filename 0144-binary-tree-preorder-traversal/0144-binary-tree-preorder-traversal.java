406ec13f1aa0d5b015c5dc9abd761c76f386b981
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-23 13:35:19 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (40.69%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,24 +14,58 @@
  * }
  */
 class Solution {
-    //Recurisive Soln:
-    //PreOrder - Root Left Right...
+    /*
     public List<Integer> solveR(TreeNode root,List<Integer> res){
-        if(root ==null)  return res; //Base Case
-        // Add root to list
+        if(root == null) return res;
         res.add(root.val);
-        // Left Recursive call
         solveR(root.left,res);
-        //Right Recursive call
         solveR(root.right,res);
         
-        //retturn list after all call finished
         return res;
     }
     
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        return solveR(root, res);
-        
+        if(root == null) return res;
+        Stack<TreeNode> st = new Stack<>();
+        Stack<TreeNode> st2 = new Stack
+        st.push(root);
+        while(!st.isEmpty()){
+            root =st.pop();
+            res.add(root.val);
+            if(root.right !=null){
+                st.push(root.right);
+            }
+            if(root.left!=null){
+                st.push(root.left);
+            }
+        }
+        return res;
+    }
+    */
+    // Morris Traversal Practise
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> pre = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.left==null){
+                pre.add(curr.val);
+                curr =curr.right;
+            }else{
+                TreeNode prev = curr.left;
+                while(prev.right!=null && prev.right!=curr){
+                    prev=prev.right;
+                }
+                if(prev.right==null){
+                    prev.right =curr;
+                    pre.add(curr.val);
+                    curr =curr.left;
+                }else{
+                    prev.right =null;
+                    curr =curr.right;
+                }
+            }
+        }
+        return pre;
     }
 }
