38068f8cced847ed1f7d0b57c351da94343b87ff
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-23 17:02:48 +0530

    Time: 1 ms (21.67%), Space: 41.3 MB (9.81%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -19,28 +19,55 @@ class Solution {
         
         
         //2. Iterative - 2 stack ::
-        List<Integer> res = new ArrayList<>();
-        if(root==null) return res;
+         List<Integer> res = new ArrayList<>();
+//         if(root==null) return res;
         
-        Stack<TreeNode> st =new Stack<>();
-        Stack<TreeNode> st1 =new Stack<>();
-        st.push(root);
+//         Stack<TreeNode> st =new Stack<>();
+//         Stack<TreeNode> st1 =new Stack<>();
+//         st.push(root);
         
-        while(!st.isEmpty()){
-            root =st.pop();
-            st1.push(root);
-            if(root.left!=null){
-                st.push(root.left);
+//         while(!st.isEmpty()){
+//             root =st.pop();
+//             st1.push(root);
+//             if(root.left!=null){
+//                 st.push(root.left);
+//             }
+//             if(root.right!=null){
+//                 st.push(root.right);
+//             }
+//         }
+//         while(!st1.isEmpty()){
+//             root =st1.pop();
+//             res.add(root.val);
+//         }
+//         return res;
+        
+        //Using Single Stack ::Iterative ::
+        
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr =root;
+        while(!st.isEmpty() || curr!=null){
+            if(curr!=null){
+                st.push(curr);
+                curr=curr.left;
+            }else{
+                 TreeNode temp=st.peek();
+                 temp=temp.right;
+                if(temp==null){
+                    temp =st.peek();
+                    st.pop();
+                    res.add(temp.val);
+                    while(!st.isEmpty() && temp==st.peek().right){
+                        temp =st.pop();
+                        res.add(temp.val);
+                    }
+                }else{
+                    curr=temp;
+                }
             }
-            if(root.right!=null){
-                st.push(root.right);
-            }
-        }
-        while(!st1.isEmpty()){
-            root =st1.pop();
-            res.add(root.val);
         }
         return res;
+        
         
     }
 }
