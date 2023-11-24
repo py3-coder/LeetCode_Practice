f9d85def58948bd3bc0de70b7bdc615a176e2b6a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-24 11:04:40 +0530

    Time: 1 ms (11.04%), Space: 42 MB (17.19%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,14 +14,42 @@
  * }
  */
 class Solution {
-    public int depth(TreeNode root){
-        if(root == null) return 0;
-        int left = depth(root.left);
-        int right = depth(root.right);
-        
-        return 1+Math.max(left,right);
-    }
     public int maxDepth(TreeNode root) {
-        return depth(root);
+        //1.Recursive ::
+        //TC : O(n)
+        //SC :O(1)+O(n) --aux space :-
+        // return getheight(root);
+        
+        //Iterative ::
+        //TC :O(n)
+        //SC :O(n)
+        return getHeight(root);
+    }
+    public static int getheight(TreeNode root){
+        //base case ::
+        if(root ==null) return 0;
+        return 1+Math.max(getheight(root.left),getheight(root.right));
+    }
+    public static int getHeight(TreeNode root){
+        
+        if(root==null) return 0;
+        int depth=1;
+        Queue<TreeNode> qu =new ArrayDeque<>();
+        qu.offer(root);
+        while(!qu.isEmpty()){
+            int size =qu.size();
+            for(int i=0;i<size;i++){
+                TreeNode temp =qu.poll();
+                if(temp.left!=null){
+                    qu.offer(temp.left);
+                }
+                if(temp.right!=null){
+                    qu.offer(temp.right);
+                }
+            }
+            if(qu.size()==0) return depth;
+            depth++;
+        }
+        return depth;
     }
 }
