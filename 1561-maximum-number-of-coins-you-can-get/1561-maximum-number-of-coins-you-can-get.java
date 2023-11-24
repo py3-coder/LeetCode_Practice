bfdfedd1927d2f78fc2cb96b5e454c87d8081a2c
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-11-24 10:22:02 +0530

    Time: 11 ms (99.12%), Space: 56.6 MB (7.41%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -1,14 +1,42 @@
 class Solution {
     public int maxCoins(int[] piles) {
-        Arrays.sort(piles);
+        //TC : O(nlogn)+O(n/3)
+        //SC :O(1)
+        // Arrays.sort(piles);
+        // int i=0,j=piles.length-1;
+        // int sum=0;
+        // while(i<j){
+        //     sum+=piles[j-1];
+        //     j-=2;
+        //     i+=1;
+        // }
+        // return sum;
         
-        int i=0,j=piles.length-1;
-        int sum=0;
-        while(i<j){
-            sum+=piles[j-1];
-            j-=2;
-            i+=1;
-        }
-        return sum;
+        //Lets Try to optimise::
+        // TC : O(n)
+        // SC : O(1) 
+         int[]  freq =new int[100001];
+         for(int i=0;i<piles.length;i++){
+             freq[piles[i]]++;
+         }
+         int i=freq.length-1;
+         int chance =piles.length/3;
+         int turn=1;
+         int ans=0;
+         while(chance!=0){
+             if(freq[i]>0){
+                 if(turn==1){
+                     turn=0;
+                 }else{
+                     ans+=i;
+                     turn=1;
+                     chance--;
+                 }
+                 freq[i]--;
+             }else{
+                 i--;
+             }
+         }
+         return ans;
     }
 }
