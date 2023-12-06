c72cb59b23f127d527a616db7c796d35be757c16
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-12-06 14:08:11 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (13.57%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,17 +1,21 @@
 class Solution {
     public int totalMoney(int n) {
-        int rem =n%7;
-        int num=n/7;
-        int sum=0;
-        for(int j=num+1;j<=num+rem;j++){
-            sum+=j;
-        }
-        while(num>0){
-            for(int i=num;i<num+7;i++){
-                sum+=i;
-            }
-            num--;
-        }
-        return sum;
+        // int rem =n%7;
+        // int num=n/7;
+        // int sum=0;
+        // for(int j=num+1;j<=num+rem;j++){
+        //     sum+=j;
+        // }
+        // while(num>0){
+        //     for(int i=num;i<num+7;i++){
+        //         sum+=i;
+        //     }
+        //     num--;
+        // }
+        // return sum;
+        
+        //Pattern : Copied :: Logic 
+        int extra = n%7, weeks = n/7;
+        return 28 * weeks + 7 * (weeks)*(weeks-1)/2 + weeks*extra + (extra)*(extra+1)/2;
     }
 }
