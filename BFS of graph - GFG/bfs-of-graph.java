ab2a92126b209625894ba2a4a8171778376e6608
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-12-11 23:17:32 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> res =new ArrayList<>();
+        Queue<Integer> que = new LinkedList<>();
+        Set<Integer> vis = new HashSet<>();
+        que.offer(0);
+        vis.add(0);
+        while(!que.isEmpty()){
+            int node =que.poll();
+            //System.out.print(node);
+            res.add(node);
+            for(int nodes :adj.get(node)){
+                if(!vis.contains(nodes)){
+                    que.offer(nodes);
+                    vis.add(nodes);
+                }
+            }
+        }
+        return res;
+    }
+}
