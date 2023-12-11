264fc964398cd1afc05a438852d59e8d162a6c53
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-12-11 16:44:30 +0530

    Time: 1 ms (93.55%), Space: 43.7 MB (24.19%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,22 +15,28 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map  = new HashMap<>();
         for(int i=0;i<inorder.length;i++){
             map.put(inorder[i],i);
         }
-        TreeNode root =build(preorder,0,preorder.length-1,inorder,0,inorder.length-1,map);
+        
+        int prestart=0,preend=preorder.length-1;
+        int instart=0,inend=inorder.length-1;
+        TreeNode root =build(0,preorder.length-1,preorder,0,inorder.length-1,inorder,map);
         return root;
+        
     }
-    public TreeNode build(int[] preorder,int prestart,int preend,int[] inorder,int instart,int inend,HashMap<Integer,Integer> map){
+    public static TreeNode build(int prestart,int preend,int[] preorder ,int instart,int inend,int[] inorder,HashMap<Integer,Integer> map){
         if(prestart>preend || instart>inend) return null;
-        TreeNode root = new TreeNode(preorder[prestart]);
+        
+        TreeNode root =new TreeNode(preorder[prestart]);
         int inRoot =map.get(root.val);
         int numleft =inRoot-instart;
         
-        root.left =build(preorder,prestart+1,prestart+numleft,inorder,instart,inRoot-1,map);
-        root.right = build(preorder,prestart+numleft+1,preend,inorder,inRoot+1,inend,map);
+        root.left =build(prestart+1,prestart+numleft,preorder,instart,inRoot-1,inorder,map);
+        root.right =build(prestart+numleft+1,preend,preorder,inRoot+1,inend,inorder,map);
         
         return root;
+        
     }
 }
