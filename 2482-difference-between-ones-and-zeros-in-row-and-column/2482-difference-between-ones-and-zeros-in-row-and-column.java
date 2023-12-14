1bbbe5ba43ebaa29dd8fb1ed9d4133d7293c2aa1
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-12-14 09:54:34 +0530

    Time: 14 ms (24.60%), Space: 71.4 MB (72.82%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
@@ -8,28 +8,34 @@ class Solution {
         
         for(int i=0;i<n;i++){
             int cnt1=0;
+            int cnt0=0;
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
                     cnt1++;
+                }else{
+                    cnt0++;
                 }
             }
-            rowi[i]=cnt1;
+            rowi[i]=cnt1-cnt0;
         }
         for(int j=0;j<m;j++){
             int cnt1=0;
+            int cnt0=0;
             for(int i=0;i<n;i++){
                 if(grid[i][j]==1){
                     cnt1++;
+                }else{
+                    cnt0++;
                 }
             }
-            coli[j]= cnt1;
+            coli[j]= cnt1-cnt0;;
         }
         
         int[][] diff = new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 //System.out.println(rowi[i]+","+coli[j]);
-                diff[i][j] = rowi[i]+coli[j]-(n-rowi[i])-(m-coli[j]);
+                diff[i][j] = rowi[i]+coli[j];
             }
         }
         return diff;
