476b8c85f042ba13e2a6b47dd3b981c425eaa53f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-12-19 10:31:18 +0530

    Time: 6 ms (80.11%), Space: 44.5 MB (57.42%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int n =img.length;
+        int m =img[0].length;
+        
+        int[][] res = new int[n][m];
+        int[] drow={0,0,1,1,1,0,-1,-1,-1};
+        int[] dcol={0,1,1,0,-1,-1,-1,0,1};
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int cnt=0;
+                int sum=0;
+                for(int k=0;k<9;k++){
+                    int nrow =i+drow[k];
+                    int ncol =j+dcol[k];
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                        cnt++;
+                        sum+=img[nrow][ncol];
+                    }
+                }
+                
+                res[i][j]=sum/cnt;
+            }
+        }
+        return res;
+    }
+}
