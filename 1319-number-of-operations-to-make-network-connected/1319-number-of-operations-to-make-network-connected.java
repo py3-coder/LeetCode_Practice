088867d9473a7ad92197f5651a644253c2d0366e
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-12-20 15:28:59 +0530

    Time: 21 ms (26.45%), Space: 68.4 MB (25.50%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,21 +1,82 @@
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        if (connections.length < n - 1) return -1; // To connect all nodes need at least n-1 edges
-        int[] parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
-        int components = n;
-        for (int[] c : connections) {
-            int p1 = findParent(parent, c[0]);
-            int p2 = findParent(parent, c[1]);
-            if (p1 != p2) {
-                parent[p1] = p2; // Union 2 component
-                components--;
+        
+        DisjointSet ds =new DisjointSet(n);
+        
+        int cntExtraEdge=0;
+        int cntdissConnected =0;
+        
+        for(int i=0;i<connections.length;i++){
+            int u =connections[i][0];
+            int v =connections[i][1];
+            
+            if(ds.findPar(u) != ds.findPar(v)){
+                ds.unionByRank(u,v);
+            }else{
+                cntExtraEdge++;
             }
         }
-        return components - 1; // Need (components-1) cables to connect components together
+        
+        for(int i=0;i<n;i++){
+            if(ds.findPar(i)==i) cntdissConnected++;
+        }
+        int res =cntdissConnected-1;
+        if(cntExtraEdge>=res){
+            return res;
+        }
+        return -1;
+        
     }
-    private int findParent(int[] parent, int i) {
-        while (i != parent[i]) i = parent[i];
-        return i; // Without Path Compression
+}
+class DisjointSet{
+    List<Integer> size = new ArrayList<>();
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent =new ArrayList<>();
+    
+    public DisjointSet(int n){
+        for(int i=0;i<=n;i++){
+            rank.add(0);
+            size.add(1);
+            parent.add(i);
+        }
+    }
+    
+    public int findPar(int node){
+        if(parent.get(node)==node){
+            return node;
+        }
+        int par =findPar(parent.get(node));
+        parent.set(node,par);
+        return parent.get(par);
+    }
+    public void unionByRank(int u,int v){
+        int ulp_u =findPar(u);
+        int ulp_v =findPar(v);
+        
+        if(ulp_u == ulp_v) return ;
+        
+        if(rank.get(ulp_u)>rank.get(ulp_v)){
+            parent.set(ulp_v,ulp_u);
+        }else if(rank.get(ulp_u)<rank.get(ulp_v)){
+            parent.set(ulp_u,ulp_v);
+        }else{
+            parent.set(ulp_v,ulp_u);
+            rank.set(ulp_u,rank.get(ulp_u)+1);
+        }
+    }
+    
+    public void unionBySize(int u,int v){
+        int ulp_u =findPar(u);
+        int ulp_v =findPar(v);
+        
+        if(ulp_u == ulp_v) return ; 
+        
+        if(size.get(ulp_u)>size.get(ulp_v)){
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
+        }else{
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
+        }
     }
 }
