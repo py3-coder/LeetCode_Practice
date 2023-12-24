07daf43c138806752e0715ea502867311877f134
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-12-24 22:10:05 +0530

    Time: 3 ms (92.13%), Space: 42.2 MB (8.85%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minOperations(String s) {
+        int res = 0, n = s.length();
+        for (int i = 0; i < n; ++i)
+            if (s.charAt(i) - '0' != i % 2)
+                res++;
+        return Math.min(res, n - res);
+    }
+}
