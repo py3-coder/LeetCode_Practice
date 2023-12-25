6bce57b748cf74dc9e2657da4dce7a20972ef034
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-12-25 11:24:07 +0530

    Time: 2 ms (22.84%), Space: 42 MB (7.17%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,32 +1,38 @@
 class Solution {
     public int numDecodings(String s) {
-        Map<Integer,Integer> memo = new HashMap<>();
-        return Solve(s,0,memo);
+        HashMap<Integer,Integer> map =new HashMap<>();
+        if(s.charAt(0)=='0') return 0;
+        return way(s,0,map);
         
     }
-    public static int Solve(String s,int i,Map<Integer,Integer> memo){
-        // Base Case for way 1
-        if(i==s.length()){
-            return 1;
+    public static int way(String s ,int indx,HashMap<Integer,Integer> map){
+        if(indx>s.length()) return 0;
+        if(indx==s.length()) return 1;
+        if(s.charAt(indx)=='0') return 0;
+        if(indx==s.length()-1) return 1;
+        
+        if(map.containsKey(indx)){
+            return map.get(indx);
         }
-        // When ever we enconter 0 --> "01" = 0 , "102"=0 "120"=0  
-        if(s.charAt(i)=='0'){
-            return 0;
+        if(indx+1<s.length()){
+            String str =s.substring(indx,indx+2);
+            int val =Integer.valueOf(str);
+            if(val<=26){
+                int waycnt =way(s,indx+1,map)+way(s,indx+2,map);
+                map.put(indx,waycnt);
+                return waycnt;
+               
+            }else{
+                int waycnt =way(s,indx+1,map);
+                map.put(indx,waycnt);
+                return waycnt;
+            }
+        }else{
+            String str =s.substring(indx,indx+1);
+            int waycnt =way(s,indx+1,map);
+            map.put(indx,waycnt);
+            return waycnt;
         }
-        if(memo.containsKey(i)){
-            return memo.get(i);
-        }
-        // Base Case for way 2
-        if(i==s.length()-1) return 1;
-        // focus on only single elememts
-        int way1 = Solve(s,i+1,memo);
-        // focus on pair element 
-        int way2 =0;
-        // pair only considered when it int value is lower than 27.
-        if(Integer.parseInt(s.substring(i,i+2))<=26){
-            way2 =Solve(s,i+2,memo);
-        }
-        memo.put(i,way1+way2);
-        return way1+way2;
     }
+    
 }
