163042c8230bc8788b94936bfee3e80ecea6f22a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-01-11 11:51:32 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (27.46%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -18,8 +18,10 @@ class Solution {
     public int maxAncestorDiff(TreeNode root) {
         if(root==null) return 0;
         maxi=0;
-        path(root,new ArrayList<>());
-        return maxi;
+        // path(root,new ArrayList<>());
+        // return maxi;
+        
+        return solve(root,root.val,root.val);
         
     }
     public static void path(TreeNode root,List<Integer> temp){
@@ -40,5 +42,15 @@ class Solution {
         path(root.left,temp);
         path(root.right,temp);
         temp.remove(temp.size()-1);
+    }
+    
+    public static int solve(TreeNode root, int max ,int min){
+        //Base Case ::
+        if(root==null) return max-min;
+        
+        max =Math.max(root.val, max);
+        min =Math.min(root.val,min);
+        
+        return Math.max(solve(root.left,max,min),solve(root.right,max,min));
     }
 }
