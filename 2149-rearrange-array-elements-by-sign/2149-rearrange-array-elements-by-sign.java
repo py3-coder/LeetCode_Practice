07806157931296c2521368f442409a174f3df272
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-02-14 10:50:29 +0530

    Time: 3 ms (100.00%), Space: 76.8 MB (74.37%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,5 +1,13 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        //Broute Force :: make n/2 size 2 array put +ve in first and -ve in sec::
+        //make new array insert carefully 1-1 from each::
+        
+        //TC : O(n)+O(n) -- atleast 2 traversal
+        //SC : O(n/2)+O(n/2) -O(n) 
+        
+        
+        //Optimised :: In Single Traversal:
         int res[] = new int[nums.length];
         int pos=0,neg=1;
         for(int i=0;i<nums.length;i++){
@@ -12,5 +20,8 @@ class Solution {
             }
         }
         return res;
+        
+        //TC : O(n)
+        //SC : O(1)
     }
 }
