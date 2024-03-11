4e41573edcf891ade8ffc91ddd5c27ed50efa971
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-03-11 21:34:46 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (75.87%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String customSortString(String S, String T) {
+        int[] bucket = new int[26];
+        for (char c : T.toCharArray()) {
+            bucket[c - 'a']++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (char c : S.toCharArray()) {
+            for (int i = 0; i < bucket[c - 'a']; i++) {
+                sb.append(c);
+            }
+            bucket[c - 'a'] = 0;
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < bucket[i]; j++) {
+                sb.append((char) (i + 'a'));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
