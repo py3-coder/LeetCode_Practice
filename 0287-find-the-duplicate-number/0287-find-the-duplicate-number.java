2be5366c89f7ce83d36df1e5b91d0358cb5c75c5
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-03-24 07:56:41 +0530

    Time: 4 ms (85.61%), Space: 56.9 MB (87.09%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,14 +1,39 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int val:nums){
-            map.put(val, map.getOrDefault(val,0)+1);
-        }
-        for (Map.Entry<Integer,Integer> entry : map.entrySet()){
-            if(entry.getValue()>=2){
-                return entry.getKey();
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int val:nums){
+        //     map.put(val, map.getOrDefault(val,0)+1);
+        // }
+        // for (Map.Entry<Integer,Integer> entry : map.entrySet()){
+        //     if(entry.getValue()>=2){
+        //         return entry.getKey();
+        //     }
+        // }
+        // return 0;
+        
+        //TC : O(n)+O(n)
+        //SC: O(n)
+        
+        
+        //Lets Apply LinkList Connect of Cycle ::
+        if(nums.length>1){
+            int slow =nums[0];
+            int fast=nums[nums[0]];
+            while(fast!=slow){
+                slow =nums[slow];
+                fast=nums[nums[fast]];
             }
+            fast=0;
+            while(fast!=slow){
+                fast =nums[fast];
+                slow=nums[slow];
+            }
+            return slow;
         }
-        return 0;
+        return -1;
+        
+        
+        //TC : O(n)::
+        //SC : O(1)::
     }
 }
