ab8a40d49fbaa458ace0fdbb8437f7c8874a2386
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-15 16:20:35 +0530

    Time: 3 ms (34.80%), Space: 45 MB (82.55%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,52 +1,63 @@
 class Solution {
     public void solve(char[][] board) {
+        // Boundary DFS ::
         int n =board.length;
-        int m=board[0].length;
+        int m =board[0].length;
         
         int[][] vis = new int[n][m];
         
-        //Boundary Row:
-        for(int j=0;j<m;j++){
-            if(board[0][j]=='O' && vis[0][j]!=1){
-                dfs(0,j,vis,board);
-            }
-            if(board[n-1][j]=='O' && vis[n-1][j]!=1){
-                dfs(n-1,j,vis,board);
+        // Topleft->right
+        for(int i=0;i<m;i++){
+            if(board[0][i]=='O' && vis[0][i]!=1){
+                dfs(0,i,vis,board);
             }
         }
-        for(int i=0;i<n;i++){
+        //Topleft -> bottom left
+        for(int i=1;i<n;i++){
             if(board[i][0]=='O' && vis[i][0]!=1){
                 dfs(i,0,vis,board);
             }
+        }
+        
+        // bootom left -> bottom right
+        for(int i=0;i<m;i++){
+            if(board[n-1][i]=='O' && vis[n-1][i]!=1){
+                dfs(n-1,i,vis,board);
+            }
+        }
+        
+        // Top right -> bottom right
+        for(int i=0;i<n;i++){
             if(board[i][m-1]=='O' && vis[i][m-1]!=1){
                 dfs(i,m-1,vis,board);
             }
         }
         
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                if(vis[i][j]!=1 && board[i][j]=='O'){
+                    board[i][j]='X';
+                }
                 if(board[i][j]=='O' && vis[i][j]==0){
                     board[i][j]='X';
                 }
             }
         }
-        return ;
     }
-    public static void dfs(int row,int col,int[][] vis,char[][] board){
+    
+    public void dfs(int row,int col ,int[][] vis,char[][] board){
         vis[row][col]=1;
         
-        int n =board.length;
-        int m=board[0].length;
-        
-        int drow[] ={0,1,0,-1};
-        int dcol[] ={1,0,-1,0};
+        int[] dx ={1,0,-1,0};
+        int[] dy ={0,1,0,-1};
         
         for(int i=0;i<4;i++){
-            int nrow =row+drow[i];
-            int ncol =col+dcol[i];
+            int nr =row+dx[i];
+            int nc =col+dy[i];
             
-            if(nrow<n && nrow>=0 && ncol>=0 && ncol<m && vis[nrow][ncol]!=1 && board[nrow][ncol]=='O'){
-                dfs(nrow,ncol,vis,board);
+            if(nr<board.length && nr>=0 && nc<board[0].length && nc>=0 && board[nr][nc]=='O' && vis[nr][nc]!=1){
+                dfs(nr,nc,vis,board);
             }
         }
     }
