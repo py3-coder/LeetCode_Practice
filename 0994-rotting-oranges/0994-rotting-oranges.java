c9fed2a31dac183f9ac367f5e3ce836ebfb0f675
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-15 10:59:48 +0530

    Time: 2 ms (72.87%), Space: 42.4 MB (46.81%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,46 +1,63 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n =grid.length;
-        int m=grid[0].length;
-        int cntFresh=0;
-        Queue<List<Integer>> que = new LinkedList<>();
-        int[][] vis = new int[n][m];
+        int n  = grid.length;
+        int m =  grid[0].length;
+        
+        int[][] vis = new int[n+1][m+1];
+        int max =0;
+        Queue<Triple> que = new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    que.offer(new ArrayList<>(Arrays.asList(i, j, 0)));
-                    vis[i][j]=2;
-                }
-                if(grid[i][j]==1){
-                    cntFresh++;
+                    que.offer(new Triple(i,j,0));
+                    vis[i][j]=1;  
                 }
             }
         }
-        int cnt=0;
-        int time=0;
-        int drow[] ={0,1,0,-1};
-        int dcol[] ={1,0,-1,0};
+        max =bfs(que,vis,grid);
+        //check ::
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && vis[i][j]!=1){
+                    return -1;
+                }
+            }
+        }
+        return  max;
+        
+    }
+    public int bfs(Queue<Triple> que,int[][] vis,int[][] grid){
+        int[] dx ={0,1,0,-1};
+        int[] dy ={1,0,-1,0};
+        int maxi =0;
         while(!que.isEmpty()){
-            List<Integer> temp =que.poll();
-            int row =temp.get(0);
-            int col =temp.get(1);
-            int t =temp.get(2);
-            time =Math.max(time,t);
+            Triple tt = que.poll();
+            int x =tt.x;
+            int y =tt.y;
+            int time =tt.time;
+            maxi =Math.max(time,maxi);
             for(int i=0;i<4;i++){
-                int nrow =row+drow[i];
-                int ncol =col+dcol[i];
+                int new_x = x+dx[i];
+                int new_y =y+dy[i];
                 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
-                    que.offer(new ArrayList<>(Arrays.asList(nrow,ncol,t+1)));
-                    vis[nrow][ncol]=2;
-                    cnt++;
+                if(new_x<grid.length && new_x>=0 && new_y<grid[0].length && new_y>=0 && grid[new_x][new_y]==1 &&vis[new_x][new_y]!=1){
+                    que.offer(new Triple(new_x,new_y,time+1));
+                    vis[new_x][new_y]=1;
                 }
-                
             }
-            
         }
-        if(cntFresh!=cnt) return -1;
-        return time;
+        return maxi;
     }
     
+    
+}
+class Triple{
+    int x;
+    int y;
+    int time;
+    Triple(int _x,int _y,int _time){
+        this.x=_x;
+        this.y=_y;
+        this.time =_time;
+    }
 }
