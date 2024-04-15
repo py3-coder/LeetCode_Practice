1a588482a0608ac4c78492be27c461660983d67f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-15 14:44:25 +0530

    Time: 14 ms (66.45%), Space: 46.6 MB (91.46%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,50 +1,56 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        //BFS ::
-        int n=mat.length;
-        int m=mat[0].length;
-        int vis[][] = new int[n][m];
-        int res[][] = new int[n][m];
+        int n = mat.length;
+        int m = mat[0].length;
         
-        Queue<Pair> que = new LinkedList<>();
+        int[][] vis =new int[n][m];
+        int[][] res =new int[n][m];
+        
+        Queue<Triple> que = new LinkedList<>();
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]!=1 && mat[i][j]==0){
+                if(mat[i][j]== 0 && vis[i][j]!=1){
                     vis[i][j]=1;
                     res[i][j]=0;
-                    que.offer(new Pair(i,j,0));
+                    que.offer(new Triple(i,j,0));
                 }
             }
         }
-        int drow[] ={0,1,0,-1};
-        int dcol[] ={1,0,-1,0};
+    
+        int dx[] ={0,1,0,-1};
+        int dy[] ={1,0,-1,0};
         while(!que.isEmpty()){
-            Pair pp =que.poll();
-            int row = pp.val1;
-            int col =pp.val2;
-            int dis =pp.dist;
+            Triple tt = que.poll();
+            int x = tt.x;
+            int y = tt.y;
+            int dis =tt.d;
             
             for(int i=0;i<4;i++){
-                int nrow =row+drow[i];
-                int ncol =col+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=1 && mat[nrow][ncol]!=0){
-                    res[nrow][ncol] =dis+1;
+                int nrow = x+dx[i];
+                int ncol = y+dy[i];
+                
+                if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && vis[nrow][ncol]!=1 && mat[nrow][ncol]!=0){
+                    res[nrow][ncol] = dis+1;
                     vis[nrow][ncol] =1;
-                    que.offer(new Pair(nrow,ncol,dis+1));
+                    que.offer(new Triple(nrow,ncol,dis+1));
                 }
             }
         }
-        return res;
+    
+       return res; 
+        
     }
-    class Pair{
-        int val1;
-        int val2;
-        int dist;
-        Pair(int _v1,int _v2,int _dis){
-            this.val1=_v1;
-            this.val2=_v2;
-            this.dist=_dis;
-        }
+    
+}
+class Triple{
+    int x;
+    int y;
+    int d;
+    
+    Triple(int _x,int _y,int _d){
+        this.x =_x;
+        this.y =_y;
+        this.d=_d;
     }
 }
