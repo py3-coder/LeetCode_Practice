9b4f9ea751ae121be0c4042496645de414306ade
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-16 15:18:34 +0530

    Time: 6 ms (48.67%), Space: 45.2 MB (42.21%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -8,36 +8,34 @@ class Solution {
         for(int i=0;i<prerequisites.length;i++){
             adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
-        int[] indegree =new int[numCourses];
+        int[] indeg =new int[numCourses];
         for(int i=0;i<numCourses;i++){
             for(int ele:adj.get(i)){
-                indegree[ele]++;
+                indeg[ele]++;
             }
         }
         
-        Queue<Integer> que =new LinkedList<>();
         
+        Queue<Integer> que = new LinkedList<>();
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
+            if(indeg[i]==0){
                 que.offer(i);
             }
         }
         int cnt=0;
         while(!que.isEmpty()){
-            int node=que.poll();
+            int nodes  = que.poll();
             cnt++;
-            
-            for(int currNode :adj.get(node)){
-                indegree[currNode]--;
-                if(indegree[currNode]==0){
-                    que.offer(currNode);
+            for(int node : adj.get(nodes)){
+                indeg[node]--;
+                if(indeg[node]==0){
+                    que.offer(node);
                 }
             }
         }
-        if(cnt==numCourses){
+        if(cnt == numCourses){
             return true;
-        }else{
-            return false;
         }
+        return false;
     }
 }
