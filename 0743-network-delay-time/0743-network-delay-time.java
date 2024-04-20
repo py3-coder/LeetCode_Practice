89d0ece6a65b7e716241de00aaa0a9082bb1081d
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-20 12:51:18 +0530

    Time: 20 ms (44.53%), Space: 47.1 MB (83.55%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,43 +1,58 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        // Plan BFS ::
-         int[] dist = new int[n+1];
-         Arrays.fill(dist, Integer.MAX_VALUE);
-         List<List<Pair>> adj = new ArrayList<>();
-         for (int i = 0; i <= n; i++) adj.add(new ArrayList<>());
-         for (int[] time: times) {
-             adj.get(time[0]).add(new Pair(time[1], time[2]));
-         }
-         Queue<Pair> q = new LinkedList<>();
-         dist[k] = 0;
-         q.offer(new Pair(k, 0));
-         while (!q.isEmpty()) {
-             Pair p = q.poll();
-             int node = p.first;
-             int time = p.second;
-             for (Pair pair: adj.get(node)) {
-                 int adjNode = pair.first;
-                 int t = pair.second;
-                 if (dist[node] + t < dist[adjNode]) {
-                     dist[adjNode] = dist[node] + t;
-                     q.offer(new Pair(adjNode, dist[adjNode]));
-                 }
-             }
-         }
-         int max = 0;
-         for (int i = 1; i <= n; i++) {
-             if (dist[i] == Integer.MAX_VALUE) return -1;
-             max = Math.max(max, dist[i]);
-         }
-         return max;
+        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
         
-    }
-    public class Pair{
-        int first;
-        int second;
-        Pair(int first,int second){
-            this.first = first;
-            this.second =second;
+        for(int i=0;i<n+1;i++){
+            adj.add(new ArrayList<>());
         }
+        for(int[] edges : times){
+            int u = edges[0];
+            int v = edges[1];
+            int cost =edges[2];
+            
+            adj.get(u).add(new ArrayList<>(Arrays.asList(v,cost)));
+        }
+        int[] dist = new int[n+1];
+        Arrays.fill(dist,(int)1e6);
+        dist[k] =0;
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->(a.time-b.time));
+        
+        pq.offer(new Pair(k,0));
+        
+        while(!pq.isEmpty()){
+            Pair pp = pq.peek();
+            pq.poll();
+            
+            int currNode = pp.node;
+            int timetake =pp.time;
+            
+            for(ArrayList<Integer> list : adj.get(currNode)){
+                if(dist[list.get(0)] > timetake +list.get(1)){
+                    dist[list.get(0)]= timetake +list.get(1);
+                    
+                    pq.offer(new Pair(list.get(0),dist[list.get(0)]));
+                }
+            }
+        }
+        int mini =0;
+        for(int i=1;i<=n;i++){
+            if(dist[i]==(int)1e6){
+                return -1;
+            }
+            mini = Math.max(dist[i],mini);
+        }
+        return  mini;
+    }
+    
+    
+}
+class Pair{
+    int node;
+    int time;
+    
+    Pair(int _n,int _t){
+        this.node =_n;
+        this.time =_t;
     }
 }
