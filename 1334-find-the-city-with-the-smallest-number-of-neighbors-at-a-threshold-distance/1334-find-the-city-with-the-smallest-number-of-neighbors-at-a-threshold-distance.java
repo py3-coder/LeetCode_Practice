3212de3e310b63c1b0a59e94d7e9e79693e78e8b
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-23 19:32:32 +0530

    Time: 74 ms (12.65%), Space: 45.1 MB (16.38%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,39 +1,77 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-         // Lets Implement Floyd Warshall ::
-        int[][] dist =new int[n][n];
-        Arrays.stream(dist).forEach(a->Arrays.fill(a,(int)1e9));
-        for(int i=0;i<edges.length;i++){
-            int[] temp =edges[i];
-            dist[temp[0]][temp[1]] = temp[2];
-            dist[temp[1]][temp[0]] = temp[2];
-        }
-        for(int i=0;i<n;i++){
-            dist[i][i]=0;
-        }
-        for(int k=0;k<n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(dist[i][k]==(int)1e9 || dist[k][j]==(int)1e9) continue;
-                    dist[i][j] =Math.min(dist[i][j],dist[i][k]+dist[k][j]);
-                }
-            }
-        }
-        int cntcity =n;
-        int cityNo=-1;
+        // Lets try with Dijkestra ::
         
+        //1. Lets Make adj ::
+        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
         for(int i=0;i<n;i++){
-            int cnt=0;
-            for(int j=0;j<n;j++){
-                if(dist[i][j]<=distanceThreshold){
-                    cnt++;
-                }
-            }
-            if(cnt<=cntcity){
-                cntcity=cnt;
-                cityNo=i;
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int[] ed : edges){
+            int u = ed[0];
+            int v = ed[1];
+            int wt = ed[2];
+            adj.get(u).add(new ArrayList<>(Arrays.asList(v,wt)));
+            adj.get(v).add(new ArrayList<>(Arrays.asList(u,wt)));
+            
+        }
+        int[] cityCnt = new int[n];
+        int city =n;
+        for(int i=0;i<n;i++){
+            cityCnt[i] =dijkestra(i,adj,n,distanceThreshold);
+            city = Math.min(city,cityCnt[i]);
+        }
+        
+        List<Integer> test  = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(cityCnt[i]==city){
+                test.add(i);
             }
         }
-        return cityNo;
+        return test.get(test.size()-1);
+        
+        
+        
+        
+    }
+    public int dijkestra(int src ,ArrayList<ArrayList<ArrayList<Integer>>> adj,int n,int disth){
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->(a.wt-b.wt));
+        int[] dist = new int[n];
+        Arrays.fill(dist,(int)1e9);
+        dist[src]=0;
+        pq.offer(new Pair(src,0));
+        
+        while(!pq.isEmpty()){
+            Pair pp = pq.peek();
+            pq.poll();
+            int node =pp.n;
+            
+            for(ArrayList<Integer> ad : adj.get(node)){
+                int currNode = ad.get(0);
+                int currWt =ad.get(1);
+                
+                if(dist[currNode]>dist[node]+currWt){
+                    dist[currNode] =dist[node]+currWt;
+                    pq.offer(new Pair(currNode,dist[currNode]));
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(dist[i]<=disth){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+class Pair{
+    int n;
+    int wt;
+    
+    Pair(int _n,int _wt){
+        this.n =_n;
+        this.wt=_wt;
     }
 }
