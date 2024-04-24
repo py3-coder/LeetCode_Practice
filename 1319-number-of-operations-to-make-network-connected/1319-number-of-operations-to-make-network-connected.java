d2b71858c703205cca49f55a3b550d711d12e1f6
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-24 12:47:59 +0530

    Time: 19 ms (30.23%), Space: 66.8 MB (35.54%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -10,7 +10,7 @@ class Solution {
             int v =connections[i][1];
             
             if(ds.findPar(u) != ds.findPar(v)){
-                ds.unionByRank(u,v);
+                ds.unionBySize(u,v);
             }else{
                 cntExtraEdge++;
             }
