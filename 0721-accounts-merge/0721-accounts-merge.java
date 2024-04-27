04900c81fbaa37920a98be0486f8db0e9c84d35a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-27 15:12:51 +0530

    Time: 30 ms (76.43%), Space: 46.6 MB (99.77%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,15 +1,14 @@
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        int n = accounts.size();
-        DisjoinSet ds =new DisjoinSet(n);
-        HashMap<String,Integer> map =new HashMap<>();
+        int  n = accounts.size();
+        DisjointSet ds = new DisjointSet(n);
+        HashMap<String,Integer> map = new HashMap<>();
         for(int i=0;i<n;i++){
             for(int j=1;j<accounts.get(i).size();j++){
-                String mail =accounts.get(i).get(j);
-                if(!map.containsKey(mail)){
-                    map.put(mail,i);
+                if(!map.containsKey(accounts.get(i).get(j))){
+                    map.put(accounts.get(i).get(j),i);
                 }else{
-                    ds.unionbySize(i,map.get(mail));
+                    ds.unionBySize(i,map.get(accounts.get(i).get(j)));
                 }
             }
         }
@@ -29,70 +28,75 @@ class Solution {
             if(res.get(i).size()==0){
                continue;
             }
-                Collections.sort(res.get(i));
-                List<String> temp = new ArrayList<>();
-                temp.add(accounts.get(i).get(0));
-                for(int p=0;p<res.get(i).size();p++){
-                    temp.add(res.get(i).get(p));
-                }
-                ans.add(temp);
+            Collections.sort(res.get(i));
+            List<String> temp = new ArrayList<>();
+            temp.add(accounts.get(i).get(0));
+            for(int p=0;p<res.get(i).size();p++){
+                temp.add(res.get(i).get(p));
+            }
+            ans.add(temp);
             
         }        
         return ans;
     }
-    
 }
-class DisjoinSet{
-    List<Integer> rank  = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    List<Integer> parent =new ArrayList<>();
+
+
+
+class DisjointSet{
+    ArrayList<Integer> rank ;
+    ArrayList<Integer> parent;
+    ArrayList<Integer> size;
     
     
-    DisjoinSet(int n){
-        for(int i=0;i<=n;i++){
-            size.add(1);
+    DisjointSet(int n){
+        rank = new ArrayList<>();
+        parent = new ArrayList<>();
+        size = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            rank.add(1);
             parent.add(i);
+            size.add(1);
         }
     }
+    
     
     public int findPar(int node){
-        if(node == parent.get(node)){
+        if(parent.get(node) == node){
             return node;
         }
-        
         int par = findPar(parent.get(node));
+        //path compression ::
         parent.set(node,par);
-        return parent.get(par);
+        return parent.get(node);
     }
     
-    public void unionbySize(int u,int v){
-        int ulp_u =findPar(u);
-        int ulp_v =findPar(v);
+    public void unionBySize(int u,int v){
+        int utp_u = findPar(u);
+        int utp_v = findPar(v);
         
-        if(ulp_u == ulp_v) return ;
-        
-        if(size.get(ulp_u)>size.get(ulp_v)){
-            parent.set(ulp_v,ulp_u);
-            size.set(ulp_u , size.get(ulp_u)+size.get(ulp_v));
+        if(utp_u == utp_v) return ;
+        if(size.get(utp_u)>size.get(utp_v)){
+            parent.set(utp_v, utp_u);
+            size.set(utp_u,size.get(utp_u)+size.get(utp_v));
         }else{
-            parent.set(ulp_u,ulp_v);
-            size.set(ulp_v , size.get(ulp_u)+size.get(ulp_v));
+            parent.set(utp_v, utp_u);
+            size.set(utp_u,size.get(utp_u)+size.get(utp_v));
         }
     }
-    
-    public void unionbyRank(int u,int v){
-        int ulp_u =findPar(u);
-        int ulp_v =findPar(v);
+    public void unionByRank(int u,int v){
+        int utp_u =findPar(u);
+        int utp_v =findPar(v);
         
-        if(ulp_u == ulp_v) return ;
+        if(utp_u == utp_v) return;
         
-        if(rank.get(ulp_u)>rank.get(ulp_v)){
-            parent.set(ulp_v,ulp_u);
-        }else if(rank.get(ulp_v)>rank.get(ulp_u)){
-             parent.set(ulp_u,ulp_v);
-        }else{
-            parent.set(ulp_v,ulp_u);
-            rank.set(ulp_u,rank.get(ulp_u)+1);
+        if(rank.get(utp_u)==rank.get(utp_v)){
+            parent.set(utp_v, utp_u);
+            rank.set(utp_u , rank.get(utp_u)+1);
+        }else if(rank.get(utp_u) >rank.get(utp_v)){
+            parent.set(utp_u,utp_v);
+        }else if(rank.get(utp_u) < rank.get(utp_v)){
+            parent.set(utp_v,utp_u);
         }
     }
 }
