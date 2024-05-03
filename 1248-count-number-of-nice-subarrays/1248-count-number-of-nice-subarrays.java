75bfd02ebdb81590f4e973434233b51ff4f048f3
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-03 22:29:04 +0530

    Time: 38 ms (22.67%), Space: 55.5 MB (11.16%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,7 +1,30 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         //
-        return solve(nums,k)-solve(nums,k-1);
+        // Solved using Sliding Windows::
+        //return solve(nums,k)-solve(nums,k-1);
+        
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        // Replace odd with 1 and even with 0
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                nums[i] = 1;
+            } else {
+                nums[i] = 0;
+            }
+        }
+        
+        mp.put(0, 1);
+        int sum = 0;
+        int count = 0;
+        for (int num : nums) {
+            sum += num;
+            count += mp.getOrDefault(sum - k, 0);
+            mp.put(sum, mp.getOrDefault(sum, 0) + 1);
+        }
+        
+        return count;
     }
     public static int solve(int[] arr,int k){
         int n=arr.length;
