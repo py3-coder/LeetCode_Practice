832fa3010340694c78937735e74f60ae9ec8db92
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-03 10:44:43 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (83.58%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,29 +1,51 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        List<String> ver1 = new ArrayList<>(Arrays.asList(version1.split("[.]")));
-        List<String> ver2 = new ArrayList<>(Arrays.asList(version2.split("[.]")));
+        // TC : O(N) 
+        // SC : O(2N)
+//         List<String> ver1 = new ArrayList<>(Arrays.asList(version1.split("[.]")));
+//         List<String> ver2 = new ArrayList<>(Arrays.asList(version2.split("[.]")));
         
-        // revison is missing is anyone:-
-        if(ver1.size()<ver2.size()){
-            for(int j=ver1.size();j<ver2.size();j++){
-                ver1.add("0");
-            }
-        }else if(ver1.size()>ver2.size()){
-            for(int j=ver2.size();j<ver1.size();j++){
-                ver2.add("0");
-            }
-        }
         
-        for(int i=0;i<ver1.size();i++){
-            int v1 = Integer.valueOf(ver1.get(i));
-            int v2 = Integer.valueOf(ver2.get(i));
+//         for(int i=0;i<Math.max(ver1.size(),ver2.size());i++){
+//             int v1 = i<ver2.size()?Integer.valueOf(ver1.get(i)):0;
+//             int v2 = i<ver2.size()?Integer.valueOf(ver2.get(i)):0;
 
-            if(v1>v2){
-                return 1;
-            }else if(v2>v1){
-                return -1;
+//             if(v1>v2){
+//                 return 1;
+//             }else if(v2>v1){
+//                 return -1;
+//             }
+//         }
+//         return 0;
+        
+        
+        
+        
+        
+        
+        int temp1 = 0,temp2 = 0;
+        int len1 = version1.length(),len2 = version2.length();
+        int i = 0,j = 0;
+        while(i<len1 || j<len2) {
+            temp1 = 0;
+            temp2 = 0;
+            while(i<len1 && version1.charAt(i) != '.') {
+                temp1 = temp1*10 + version1.charAt(i++)-'0';
+
             }
+            while(j<len2 && version2.charAt(j) != '.') {
+                temp2 = temp2*10 + version2.charAt(j++)-'0';
+
+            }
+            if(temp1>temp2) return 1;
+            else if(temp1<temp2) return -1;
+            else {
+                i++;
+                j++;
+
+            }
+
         }
         return 0;
-    }
+        }
 }
