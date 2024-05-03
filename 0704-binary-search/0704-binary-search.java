f21d5e715075d516844a5f0f6b3c70046ea10b18
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-03 14:44:03 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (95.06%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,34 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // //Iterative
-        // int n = nums.length;
-        // int start =0;
-        // int end = n-1;
-        // while(start<=end){
-        //     int mid = start +(end-start)/2;
-        //     if(nums[mid]==target){
-        //         return mid;
-        //     }else if(target<nums[mid]){
-        //         end = mid-1;
-        //     }else{
-        //         start=mid+1;
-        //     }
-        // }
-        // return -1;
+        int start =0;
+        int end =nums.length-1;
         
-        return binarysearch(nums,0,nums.length-1,target);
-    }
-    public static int binarysearch(int[] nums,int low,int high,int target){
-        if(low>high){
-            return -1;
+        while(start<=end){
+            int mid =  start+(end-start)/2;
+            
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
         }
-        int mid  =low+(high-low)/2;
-        if(nums[mid]==target){
-            return mid;
-        }else if(nums[mid]>target){
-            return binarysearch(nums,low,mid-1,target);
-        }else{
-            return binarysearch(nums,mid+1,high,target);
-        }
+        return -1;
     }
 }
