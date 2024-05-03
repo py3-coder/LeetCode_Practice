1d644de50c7aeae0bc509032805462ab333e8a7f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-03 17:48:03 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (95.71%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,6 +1,6 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
-        return bs(nums,target);
+    public boolean search(int[] A, int key) {
+        return bs(A,key);
     }
     public static boolean bs(int[] arr,int target){
         int start=0;
@@ -32,32 +32,5 @@ class Solution {
             }
         }
         return false;
-    }
-    
-    
-    
-    public static boolean binary_search(int[] arr,int target){
-        int start=0;
-        int end = arr.length-1;
-        while(start<=end){
-            int mid = start +(end- start)/2;
-            if(target==arr[mid]){
-                return true;
-            }
-            if(arr[mid]>arr[start]){
-                // means left side is sorted and lets check the target in left side..
-                if (target < arr[mid] && target >= arr[start]) end = mid;
-                else start = mid + 1;
-            } else if (arr[mid] < arr[start]) { 
-                // right side  [mid..end] is sorted
-                // check if target in right half
-                if (target > arr[mid] && target < arr[start]) start = mid + 1;
-                else end = mid;
-            }else {  
-                // have no idea about the array, but we can exclude arr[start] 
-                start++;
-            }
-        }
-    return false;
     }
 }
