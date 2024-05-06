3506b78249642d88be810cdea6c7955baf462e8d
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-06 18:48:07 +0530

    Time: 1 ms (68.15%), Space: 42.3 MB (14.00%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,20 +14,73 @@
  * }
  */
 class Solution {
-    
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res =new ArrayList<>();
-        solve(0,root,res);
+        
+        //edges ::
+        if(root == null) return new ArrayList<>();
+        List<Integer> lis = new ArrayList<>();
+//         lis.add(root.val);
+//         if(root.left == null && root.right==null) return lis;
+//         rightView(root.right,lis);
+//         return lis;
+        
+        List<List<Integer>> level  = levelorder(root);
+        for(List<Integer> list : level ){
+            lis.add(list.get(list.size()-1));
+        }
+        return lis;
+    }
+     
+    public void rightView(TreeNode root, List<Integer> arr){
+        if(root.left == null && root.right==null){
+            arr.add(root.val);
+            return;
+        }
+        if(root.right==null){
+            rightView(root.left,arr);
+        }
+        arr.add(root.val);
+        rightView(root.right,arr);
+    }
+    
+    public List<List<Integer>> levelorder(TreeNode root){
+        if(root == null) return new ArrayList<>();
+        
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+        while(!que.isEmpty()){
+            int size = que.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode re = que.poll();
+               
+                if(re.left!=null){
+                    que.offer(re.left);
+                }
+                if(re.right!=null){
+                    que.offer(re.right);
+                }
+                level.add(re.val);
+            }
+            res.add(level);
+        }
         return res;
     }
-    
-    public static void solve(int l,TreeNode root,List<Integer> res){
-        if(root==null) return ;
-        
-        if(res.size()==l) res.add(root.val);
-        
-        solve(l+1,root.right,res);
-        solve(l+1,root.left,res);
-        
-    }
+    // public static List<List<Integer>> level(TreeNode root,List<List<Integer>> res) {
+    //     if(root==null)  return res;
+    //     Queue<TreeNode> que = new LinkedList<>();
+    //     que.add(root);
+    //     while(!que.isEmpty()){
+    //         int level = que.size();
+    //         List<Integer> ans  = new ArrayList<>();
+    //         for(int i=0;i<level;i++){
+    //             if(que.peek().left!=null)que.offer(que.peek().left);
+    //             if(que.peek().right!=null)que.offer(que.peek().right);
+    //             ans.add(que.poll().val);           
+    //         }
+    //         res.add(ans);
+    //     } 
+    //     return res;
+    // }
 }
