59598a0438d98c348e44b57a0ba1e066a20f48f8
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-08 13:05:56 +0530

    Time: 7 ms (93.93%), Space: 45.8 MB (17.05%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,33 +1,43 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         String[] res = new String[score.length];
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(b-a));
-        for(int ele :score){
-            pq.offer(ele);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(score[b]-score[a]));
+        for(int i=0;i<score.length;i++){
+            pq.offer(i);
         }
         int k=1;
         while(!pq.isEmpty()){
             int ele =pq.poll();
-            //System.out.println(ele);
-            pushEle(ele,score,res,k);
+            if(k==1){
+                res[ele] = "Gold Medal";
+            }
+            else if(k==2){
+                res[ele] = "Silver Medal";
+            }
+            else if(k==3){
+                res[ele] = "Bronze Medal";
+            }
+            else if(k>3){
+                res[ele] =String.valueOf(k);
+            }
             k++;
         }
         return res;
     }
-    public void pushEle(int ele,int[] arr,String[] res,int k){
-        for(int i=0;i<arr.length;i++){
-            if(ele == arr[i] && k==1){
-                res[i] = "Gold Medal";
-            }
-            else if(ele == arr[i] && k==2){
-                res[i] = "Silver Medal";
-            }
-            else if(ele == arr[i] && k==3){
-                res[i] = "Bronze Medal";
-            }
-            else if(ele == arr[i]){
-                res[i] =String.valueOf(k);
-            }
-        }
-    }
+    // public void pushEle(int ele,int[] arr,String[] res,int k){
+    //     for(int i=0;i<arr.length;i++){
+    //         if(ele == arr[i] && k==1){
+    //             res[i] = "Gold Medal";
+    //         }
+    //         else if(ele == arr[i] && k==2){
+    //             res[i] = "Silver Medal";
+    //         }
+    //         else if(ele == arr[i] && k==3){
+    //             res[i] = "Bronze Medal";
+    //         }
+    //         else if(ele == arr[i]){
+    //             res[i] =String.valueOf(k);
+    //         }
+    //     }
+    // }
 }
