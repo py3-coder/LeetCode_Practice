b005e512db18977ca54158ae433a9bc1896b3a5d
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-15 16:09:08 +0530

    Time: 2387 ms (5.33%), Space: 74.7 MB (9.02%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -1,19 +1,89 @@
 class Solution {
     public int maximumSafenessFactor(List < List < Integer >> grid) {
         int n = grid.size();
-        int m = grid.get(0).size();
 
         //edge case ::
-        if (grid.get(0).get(0) == 1 || grid.get(n - 1).get(m - 1) == 1) {
+        if (grid.get(0).get(0) == 1 || grid.get(n - 1).get(n - 1) == 1) {
             return 0;
         }
+        // n ~ 400
+        // TC : O(n^2) + log(n) * O(n^2)   ~ n^2 * log(n)
+        // SC : O(n^2) +2* O(n^2) 
+        
+        
+        int[][] res = bfs(grid);
+        // binary search - BS on ans pattern 
+        //
+        //return bs(res);
+        
+        
+        // Follow Up Question -
+        // if  n = 4000 
+        // will this work --- a big no 
+        // almost it will reach 10*8 and might we see TLE 
+        
+        
+        // So how to solve this --
+        
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         System.out.print(res[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        
+        // think of DSU :-
+        int[] dx = {0,1,0,-1};
+        int[] dy = {1,0,-1,0};
 
-        return bfs(grid);
-
+        DisjointSet ds = new DisjointSet(n*n);
+        for(int r=401;r>=0;r--){
+            
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    
+                    if(res[i][j]>=r){
+                        for(int k=0;k<4;k++){
+                            int nr = i+dx[k];
+                            int nc = j+dy[k];
+                            int pkey =i*n+j;
+                            int key  = nr*n+nc;
+                            if(isvalid(nr, nc, n) && res[nr][nc]>=r && ds.findPar(pkey)!=ds.findPar(key)){
+                                ds.unionbySize(pkey,key);
+                            }
+                        }
+                    }else{
+                        continue;
+                    }
+                }
+            }
+            if(ds.findPar(0)==ds.findPar(n*n-1)){
+                return r;
+            }
+        }
+        return -1;
 
     }
+    public int bs(int[][] res){
+        int l = 0;
+        int h = (int) 1e9;
+        int ans = 0;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (isCheck(mid, res)) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
 
-    public int bfs(List < List < Integer >> grid) {
+        return ans;
+    }
+
+    // distance matrix --
+    public int[][] bfs(List < List < Integer >> grid) {
         int n = grid.size();
 
 
@@ -60,24 +130,7 @@ class Solution {
             }
             dist+=1;
         }
-
-
-
-        // binary search - BS on ans pattern 
-        int l = 0;
-        int h = (int) 1e9;
-        int ans = 0;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (isCheck(mid, res)) {
-                ans = mid;
-                l = mid + 1;
-            } else {
-                h = mid - 1;
-            }
-        }
-
-        return ans;
+        return res;
     }
     public boolean isCheck(int t, int[][] res) {
         int n = res.length;
@@ -130,5 +183,43 @@ class Pair {
     Pair(int _x, int _y) {
         this.x = _x;
         this.y = _y;
+    }
+}
+
+class DisjointSet{
+    List<Integer> parent;
+    List<Integer> size;
+    
+    DisjointSet(int n){
+        parent  = new ArrayList<>();
+        size = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    
+    
+    public int findPar(int u){
+        if(parent.get(u)==u) return u;
+        
+        int pu  = findPar(parent.get(u));
+        parent.set(u,pu);
+        return parent.get(u);
+    }
+    
+    public void unionbySize(int u,int v){
+        int utp_u = findPar(u);
+        int utp_v = findPar(v);
+        
+        if(utp_u == utp_v) return ;
+        if(size.get(utp_u)>size.get(utp_v)){
+            parent.set(utp_v, utp_u);
+            size.set(utp_u,size.get(utp_u)+size.get(utp_v));
+        }else{
+            parent.set(utp_v, utp_u);
+            size.set(utp_u,size.get(utp_u)+size.get(utp_v));
+        }
     }
 }
