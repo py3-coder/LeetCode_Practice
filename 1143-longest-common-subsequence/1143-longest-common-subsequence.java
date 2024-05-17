b54512202ab11832ea901d9b389724d704c360eb
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-17 13:04:33 +0530

    Time: 24 ms (34.94%), Space: 53 MB (7.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,27 +1,22 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        return solveTab(text1,text2);
+    static int[][] memo;
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n = s1.length(),m=s2.length();
+        memo  = new int[n+1][m+1];
+        Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
+        return solveRec(s1,s2,n,m);
     }
-    public static int solveTab(String s,String t){
-		//Tabulation::
-		int n=s.length();
-		int m=t.length();
-		int tab[][] = new int[n+1][m+1];
-		for(int i=0;i<n+1;i++){
-			for(int j=0;j<m+1;j++){
-				if(i==0) tab[i][j]=0;
-				if(j==0) tab[i][j]=0;
-			}
-		}
-		for(int i=1;i<n+1;i++){
-			for(int j=1;j<m+1;j++){
-				if(s.charAt(i-1)==t.charAt(j-1)){
-					tab[i][j] =1+tab[i-1][j-1];
-				}else{
-					tab[i][j] =Math.max(tab[i][j-1],tab[i-1][j]);
-				}
-			}
-		}
-		return tab[n][m];
-	}
+    public int solveRec(String s1,String s2,int a,int b){
+        //base case ::-
+        if(a==0 || b==0) return 0;
+        
+        if(memo[a][b]!=-1){
+            return memo[a][b];
+        }
+        if(s1.charAt(a-1) == s2.charAt(b-1)){
+            return memo[a][b]= 1+ solveRec(s1,s2,a-1,b-1);
+        }else{
+            return memo[a][b]= Math.max(solveRec(s1,s2,a-1,b),solveRec(s1,s2,a,b-1));
+        }
+    }
 }
