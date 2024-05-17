9af14a920e313142128470301133d38bf4e76ac4
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-17 13:21:31 +0530

    Time: 23 ms (41.20%), Space: 57.5 MB (5.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,7 +4,9 @@ class Solution {
         int n = s1.length(),m=s2.length();
         memo  = new int[n+1][m+1];
         Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
-        return solveRec(s1,s2,n,m);
+        //return solveRec(s1,s2,n,m);
+        
+        return solveTab(s1,s2,n,m);
     }
     public int solveRec(String s1,String s2,int a,int b){
         //base case ::-
@@ -18,5 +20,38 @@ class Solution {
         }else{
             return memo[a][b]= Math.max(solveRec(s1,s2,a-1,b),solveRec(s1,s2,a,b-1));
         }
+    }
+    public int solveTab(String s1,String s2,int n,int m){
+        int[][] tab = new int[n+1][m+1];
+        
+        //base case ::
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1.charAt(i-1)== s2.charAt(j-1)){
+                    tab[i][j] = 1+tab[i-1][j-1];
+                }else{
+                    tab[i][j] = Math.max(tab[i][j-1],tab[i-1][j]);
+                }
+            }
+        }
+        return tab[n][m];
+    }
+    
+    public int solveOpt(String s1,String s2,int n,int m){
+        int[] curr = new int[m+1];
+        int[] prev = new int[m+1];
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1.charAt(i-1)== s2.charAt(j-1)){
+                    curr[j] = 1+prev[j-1];
+                }else{
+                    curr[j] = Math.max(curr[j-1],prev[j]);
+                }
+            }
+            
+        }
+        return curr[m];
     }
 }
