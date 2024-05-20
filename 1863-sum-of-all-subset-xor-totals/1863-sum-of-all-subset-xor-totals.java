884bc96d0daba2fcc013f79bbfc2a097846d8d5a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-20 11:15:01 +0530

    Time: 5 ms (35.74%), Space: 40.7 MB (58.25%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,16 +1,24 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        //Recursion :: O(2^n)
-        return solveRec(nums,0,0);
-    }
-    public static int solveRec(int[] nums,int indx,int xor){
-        if(indx==nums.length){
-            return xor;
+        int n = nums.length;
+        if(n==1){
+            return nums[0];
         }
-        
-        int pick = solveRec(nums,indx+1,xor^nums[indx]);
-        int notpick =solveRec(nums,indx+1,xor);
-        
-        return pick+notpick;
+        return solve(nums);  
+    }
+    
+    public int solve(int[] nums){
+        int n= nums.length;
+        int sum=0;
+        for(int i=0;i<(1<<n);i++){
+            int xor=0;
+            for(int j=0;j<n;j++){
+                if((i & (1<<j))!=0){
+                    xor^=nums[j];
+                }
+            }
+            sum+=xor;
+        }
+        return sum;
     }
 }
