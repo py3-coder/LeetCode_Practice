a0898783d1c8123773f09cca0e9f1e4d76e822a5
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-25 23:19:50 +0530

    Time: 2 ms (75.17%), Space: 44.1 MB (51.02%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,11 +2,8 @@ class Solution {
     int[][] memo;
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        
         memo = new int[n][n];
         Arrays.stream(memo).forEach(a-> Arrays.fill(a,-1));
-        
-
         return solveRec(0,0,triangle);
     }
     public int solveRec(int i ,int j,List<List<Integer>> tri){
@@ -15,11 +12,8 @@ class Solution {
         }
         if(memo[i][j]!=-1){
             return memo[i][j];
-        }
-        int curr = Integer.MAX_VALUE,next = Integer.MAX_VALUE;
-        curr =tri.get(j).get(i) + solveRec(i,j+1,tri);
-        next = tri.get(j).get(i) + solveRec(i+1,j+1,tri);   
-        return memo[i][j]=Math.min(curr,next);
+        }  
+        return memo[i][j]=tri.get(j).get(i)+Math.min(solveRec(i,j+1,tri),solveRec(i+1,j+1,tri));
     }
     
 }
