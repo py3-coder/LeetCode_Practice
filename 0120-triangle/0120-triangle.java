d96589c96e3123af85b1005d05cfab6c5bcfafa9
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-26 00:00:06 +0530

    Time: 10 ms (8.23%), Space: 45.3 MB (7.99%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -4,7 +4,9 @@ class Solution {
         int n = triangle.size();
         memo = new int[n][n];
         Arrays.stream(memo).forEach(a-> Arrays.fill(a,-1));
-        return solveRec(0,0,triangle);
+        //return solveRec(0,0,triangle);
+        
+        return solveTab(triangle);
     }
     public int solveRec(int i ,int j,List<List<Integer>> tri){
         if(j == tri.size()){
@@ -12,8 +14,30 @@ class Solution {
         }
         if(memo[i][j]!=-1){
             return memo[i][j];
-        }  
-        return memo[i][j]=tri.get(j).get(i)+Math.min(solveRec(i,j+1,tri),solveRec(i+1,j+1,tri));
+        }
+        int curr = Integer.MAX_VALUE,next = Integer.MAX_VALUE;
+        curr =tri.get(j).get(i) + solveRec(i,j+1,tri);
+        next = tri.get(j).get(i) + solveRec(i+1,j+1,tri);   
+        return memo[i][j]=tri.get(j).get(i)+Math.min(curr,next);
     }
     
+    public int solveTab(List<List<Integer>> triangle) {
+        // create  table by copying the input triangle
+        List<List<Integer>> table = new ArrayList<>();
+        for (List<Integer> row: triangle) 
+            table.add(new ArrayList<>(row));
+
+        
+        for (int row = triangle.size() - 2; row >= 0; row--) {
+            for (int col = 0; col < triangle.get(row).size(); col++) {
+                // either go to [row + 1, col] or [row + 1, col + 1]
+                int remain = Math.min(table.get(row + 1).get(col),
+                                      table.get(row + 1).get(col + 1));
+                
+
+                table.get(row).set(col, triangle.get(row).get(col) + remain);
+            }
+        }
+        return table.get(0).get(0);
+    }
 }
