1f6071a1fe5251bb7966495f86564b17c6e01cb7
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-28 12:40:38 +0530

    Time: 11 ms (10.09%), Space: 43 MB (51.09%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -1,14 +1,12 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
-        HashMap<Integer,Integer> smap = new HashMap<>();
         int start=0,end=0;
         int maxlen=0;
         while(end<s.length()){
             int val = Math.abs(s.charAt(end)-t.charAt(end));
-            smap.put(end,val);
             maxCost -=val;
-            while(maxCost<0 && end>=start){
-                maxCost+=smap.get(start);
+            if(maxCost<0 && end>=start){
+                maxCost+= Math.abs(s.charAt(start)-t.charAt(start));
                 start++;
             }
             maxlen = Math.max(maxlen,end-start+1);
