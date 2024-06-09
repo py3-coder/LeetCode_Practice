f1b63f6b3cb8a212780db21f41ed970ebca03c34
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-06-09 15:38:48 +0530

    Time: 2443 ms (100.00%), Space: 77.3 MB (100.00%) - LeetHub

--- 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.java
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.java
@@ -1,10 +1,11 @@
 class Solution {
-    public int solve(int []nums, int index, int k, int pre, int [][][]dp){
+    static  int[][][]dp = new int[501][501][26];
+    public int solve(int []nums, int index, int k, int pre){
         if(index == nums.length){
             return 0;
         }
         if(pre == -1){
-            return Math.max(1+solve(nums, index+1, k, index, dp), solve(nums, index+1, k, pre, dp));
+            return Math.max(1+solve(nums, index+1, k, index), solve(nums, index+1, k, pre));
         }
         if(dp[index][pre][k] != -1){
             return dp[index][pre][k];
@@ -12,31 +13,30 @@ class Solution {
 
         if(k == 0){
             if(nums[index] == nums[pre]){
-                dp[index][pre][k] = 1+solve(nums, index+1, k, index, dp);
+                dp[index][pre][k] = 1+solve(nums, index+1, k, index);
             }
             else{
-                dp[index][pre][k] = solve(nums, index+1, k, pre, dp);
+                dp[index][pre][k] = solve(nums, index+1, k, pre);
             }
         }
         else{
             if(nums[index] == nums[pre]){
-                dp[index][pre][k] = Math.max(1+solve(nums, index+1, k, index, dp), solve(nums, index+1, k, pre, dp));
+                dp[index][pre][k] = Math.max(1+solve(nums, index+1, k, index), solve(nums, index+1, k, pre));
             }
             else{
-                dp[index][pre][k] = Math.max(1+solve(nums, index+1, k-1, index, dp), solve(nums, index+1, k, pre, dp));
+                dp[index][pre][k] = Math.max(1+solve(nums, index+1, k-1, index), solve(nums, index+1,k, pre));
             }
         }
         return dp[index][pre][k];
     }
     public int maximumLength(int[] nums, int k) {
         int n = nums.length;
-        int[][][]dp = new int[n+1][n+1][k+1];
         for(int [][]a : dp){
             for(int []b : a){
                 Arrays.fill(b, -1);
             }
         }
         
-        return solve(nums, 0, k, -1, dp);
+        return solve(nums, 0, k, -1);
     }
 }
