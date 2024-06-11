6355ea01e59fa0710cb677dd560334721804317f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-06-11 10:09:47 +0530

    Time: 4 ms (23.80%), Space: 41.9 MB (74.51%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,32 +1,17 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Set<Integer> set = new HashSet<>();
         Map<Integer,Integer> map = new TreeMap<>();
         Arrays.stream(arr1).forEach(ele -> map.put(ele,map.getOrDefault(ele,0)+1));
-        int k=0;
-        for(int ele : arr2){
-            set.add(ele);
-            if(map.containsKey(ele)){
-                int times = map.get(ele);
-                while(times>0){
-                    arr1[k] = ele;
-                    k++;
-                    times--;
-                }
-            }else{
-                arr1[k] = ele;
-                k++;
+        int i = 0;
+        for(int n : arr2) {
+            for(int j = 0; j < map.get(n); j++) {
+                arr1[i++] = n;
             }
+            map.remove(n);
         }
-        for(int ele : map.keySet()){
-            if(!set.contains(ele)){
-                set.add(ele);
-                int times = map.get(ele);
-                while(times>0){
-                    arr1[k] = ele;
-                    k++;
-                    times--;
-                }
+        for(int n : map.keySet()){
+            for(int j = 0; j < map.get(n); j++) {
+                arr1[i++] = n;
             }
         }
         return arr1;
