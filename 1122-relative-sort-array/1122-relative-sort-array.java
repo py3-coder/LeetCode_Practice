ac2fb3e21006573969eafc05bdf8f45082b708ef
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-06-11 10:10:48 +0530

    Time: 4 ms (23.80%), Space: 42.1 MB (32.74%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -3,15 +3,15 @@ class Solution {
         Map<Integer,Integer> map = new TreeMap<>();
         Arrays.stream(arr1).forEach(ele -> map.put(ele,map.getOrDefault(ele,0)+1));
         int i = 0;
-        for(int n : arr2) {
-            for(int j = 0; j < map.get(n); j++) {
-                arr1[i++] = n;
+        for(int ele : arr2) {
+            for(int j = 0; j < map.get(ele); j++) {
+                arr1[i++] = ele;
             }
-            map.remove(n);
+            map.remove(ele);
         }
-        for(int n : map.keySet()){
-            for(int j = 0; j < map.get(n); j++) {
-                arr1[i++] = n;
+        for(int ele : map.keySet()){
+            for(int j = 0; j < map.get(ele); j++) {
+                arr1[i++] = ele;
             }
         }
         return arr1;
