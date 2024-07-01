c06108f1d90e674b9d0e300f27647603174df0a8
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-07-01 17:36:52 +0530

    Time: 2 ms (100.00%), Space: 41.9 MB (73.84%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.java
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        int cnt=0;
-        for(int ele : arr){
-            
-            //(cnt%2==0)?
-            if(ele%2==0){
-                cnt=0;
-            }else{
-                cnt+=1;
+//         int cnt=0;
+//         for(int ele : arr){
+//             if(ele%2==0){
+//                 cnt=0;
+//             }else{
+//                 cnt+=1;
                 
-                if(cnt == 3){
-                    return true;
-                }
-            }
-        }
-        return false;
+//                 if(cnt == 3){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+        
+      return  IntStream.range(0, arr.length - 2)
+      .anyMatch(i -> arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1);
     }
 }
