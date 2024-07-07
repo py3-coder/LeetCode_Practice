27dc348e2543394df08b63a5df8b57e12e5ca15a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-07-07 11:33:45 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (68.63%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,12 +1,15 @@
 class Solution {
-    public int numWaterBottles(int numB, int numE) {
-        int res =numB;
-        while(numB>=numE){
-            int rem = numB%numE;
-            res += numB/numE;
-            numB = numB/numE;
-            numB+=rem;
-        }
-        return res;
+    // public int numWaterBottles(int numB, int numE) {
+    //     int res =numB;
+    //     while(numB>=numE){
+    //         int rem = numB%numE;
+    //         res += numB/numE;
+    //         numB = numB/numE;
+    //         numB+=rem;
+    //     }
+    //     return res;
+    // }
+     public int numWaterBottles(int numBottles, int numExchange) {
+        return numBottles + (numBottles - 1) / (numExchange - 1);
     }
 }
