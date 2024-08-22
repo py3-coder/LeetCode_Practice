3e11751efe9043914db141875056c3303b760ee4
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-08-22 22:38:40 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (37.76%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,14 +1,9 @@
 class Solution {
     public int findComplement(int num) {
-        int i = 0;
-        int j = 0;
-        
-        while (i < num)
-        {
-            i += Math.pow(2, j);
-            j++;
+         int mask=1;
+        while(mask<num){
+            mask=(mask<<1)|1;
         }
-        
-        return i - num;
+        return num^mask;
     }
 }
