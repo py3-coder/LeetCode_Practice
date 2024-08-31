4825db0751602903b05d80741a07c52369b00095
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-08-31 11:03:59 +0530

    Time: 1 ms (96.32%), Space: 44.5 MB (98.44%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int len = nums.length;
-        int res[] = new int[len*2];
-        for(int i=0;i<len*2;i++){
-            res[i] = nums[i%len];
+        int n = nums.length;
+        int res[]  = new int[2*n];
+        for(int i=0;i<2*n;i++){
+            res[i] = nums[i%n];
         }
         return res;
-        
     }
 }
