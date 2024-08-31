db20e547521466e3ff0f552381ea7a97d0eb3d44
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-08-31 12:12:57 +0530

    Time: 2 ms (7.99%), Space: 45.2 MB (67.25%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,10 +1,15 @@
 class Solution {
+    // a+nb formula
     public int[] buildArray(int[] nums) {
-        int len = nums.length;
-        int res[] = new int[len];
-        for(int i=0;i<len;i++){
-            res[i] =nums[nums[i]];
+        int n = nums.length;
+        for(int i=0;i<n;++i){
+            nums[i] = nums[i]+ n*(nums[nums[i]]%n);
         }
-        return res;
+        for(int i=0;i<n;i++){
+            nums[i]/=n;
+        }
+        
+        
+        return nums;
     }
 }
