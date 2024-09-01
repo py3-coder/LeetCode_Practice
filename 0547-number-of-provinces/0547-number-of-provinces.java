666cf5f2dfcb856f2f0b16c0aef48f3cd9d6dfd1
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-09-01 07:52:42 +0530

    Time: 3 ms (38.02%), Space: 46.6 MB (89.60%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,38 +1,38 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = isConnected.length;
-        int m =isConnected[0].length;
-        for(int i=0;i<n;i++){
+        int m = isConnected[0].length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
-        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(i!=j && isConnected[i][j]==1){
-                    adj.get(i).add(j);
-                    //adj.get(j).add(i);
+                    adj.get(i+1).add(j+1);
+                    adj.get(j+1).add(i+1);
                 }
             }
         }
-        int[] vis = new int[n+1];
         int cnt=0;
-        for(int i=0;i<n;i++){
+       // System.out.println(adj);
+        int[] vis = new int[n+1];
+        for(int i=1;i<=n;i++){
             if(vis[i]!=1){
                 dfs(i,vis,adj);
                 cnt++;
             }
         }
-        //System.out.println(adj);
         return cnt;
-    }
-    public void dfs(int node,int[] vis,ArrayList<ArrayList<Integer>> adj){
-        vis[node]=1;
         
-        for(int nodes : adj.get(node)){
-            if(vis[nodes]!=1){
-                dfs(nodes,vis,adj);
-            }
+        
+    }
+    public void dfs(int node , int[] vis , ArrayList<ArrayList<Integer>> adj){
+        vis[node]=1;
+        for(int curr :adj.get(node)){
+           if(vis[curr]!=1){
+               dfs(curr , vis , adj);
+           }
         }
     }
 }
