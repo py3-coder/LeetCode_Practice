652ddb545c1945b17d9d94ceac79204568bb26da
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-10-07 23:13:23 +0530

    Time: 4 ms (85.00%), Space: 42.9 MB (91.72%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,24 @@
+class Solution {
+    // public int minLength(String s) {
+    //     while(s.contains("AB") || s.contains("CB")){
+    //         s = s.replaceAll("AB", "").replaceAll("CD", "");
+    //     }
+    //     return s.length();
+    // }
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        st.push(s.charAt(0));
+        for(int i=1;i<n;i++)
+        {
+            if(!st.isEmpty()&&((s.charAt(i)=='B'&&st.peek()=='A')||(s.charAt(i)=='D'&&st.peek()=='C')))
+            {
+                st.pop();
+            }
+            else
+                st.push(s.charAt(i));
+        }
+        return st.size();
+        
+    }
+}
