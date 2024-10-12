64c6df4c55c4cb2f4e3cbf5ec6ca66e302e751d5
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-10-13 00:20:04 +0530

    Time: 48 ms (71.47%), Space: 95.9 MB (11.68%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -1,14 +1,33 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // TC : O(nlogn) + O(n) 
+        // SC : O(n) 
+//         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+//         for(int[] interval : intervals){
+//             if(!pq.isEmpty() && pq.peek()<interval[0]){
+//                 pq.poll();
+//             }
+//             pq.offer(interval[1]);
+//         }
+//         return pq.size();
+        
+        
+        // Lets try with Sweep line algo.
+        int maxi=-1;
+        int[] line = new int[1000005];
         
         for(int[] interval : intervals){
-            if(!pq.isEmpty() && pq.peek()<interval[0]){
-                pq.poll();
-            }
-            pq.offer(interval[1]);
+            line[interval[0]]+=1;
+            line[interval[1]+1]-=1;
         }
-        return pq.size();
+        
+        for(int i=1;i<1000005;i++){
+            line[i]+=line[i-1];
+            maxi = Math.max(maxi , line[i]);
+        }
+        return maxi;
+        
     }
 }
