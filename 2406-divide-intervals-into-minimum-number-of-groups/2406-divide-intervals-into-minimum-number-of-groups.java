abbad9812a583d4a9effb6b5b096856fb53714d8
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-10-12 22:55:48 +0530

    Time: 95 ms (55.44%), Space: 75.4 MB (94.57%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minGroups(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int[] interval : intervals){
+            if(!pq.isEmpty() && pq.peek()<interval[0]){
+                pq.poll();
+            }
+            pq.offer(interval[1]);
+        }
+        return pq.size();
+    }
+}
