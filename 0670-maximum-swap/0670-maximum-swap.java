1346ec658bfe17b79a639778ba153d82d3f74bc3
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-10-17 15:14:29 +0530

    Time: 2 ms (8.36%), Space: 41 MB (8.25%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumSwap(int num) {
+        String st = String.valueOf(num);
+       
+        List<Integer> arr = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int k=0;
+        for(char ch : st.toCharArray()){
+            map.put(ch-'0',k);
+            arr.add(ch-'0');
+            k++;
+        }
+        //System.out.println(map);
+        Collections.sort(arr, Collections.reverseOrder());
+        //System.out.print(arr);
+        for(int i=0;i<arr.size();i++){
+            if(st.charAt(i)-'0'<arr.get(i)){
+                //swap
+                return Integer.valueOf(swap(st,i,map.get(arr.get(i))));
+            }
+        }
+        return Integer.valueOf(st);
+        
+    }
+    public String swap(String st , int indx , int pos){
+        char[] charArray = st.toCharArray();
+        char temp = charArray[indx];
+        charArray[indx] = charArray[pos];
+        charArray[pos] = temp;
+
+        return new String(charArray);
+        
+    }
+}
