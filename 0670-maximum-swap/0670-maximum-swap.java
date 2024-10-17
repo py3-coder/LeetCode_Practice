5e4113561bcdffb10b750d262885f8d7c0cafd55
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-10-17 16:28:38 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (14.12%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,25 +1,54 @@
 class Solution {
     public int maximumSwap(int num) {
-        String st = String.valueOf(num);
+        // TC : O(nlogn)
+        // SC : O(n) + O(n) 
+        
+//         String st = String.valueOf(num);
        
-        List<Integer> arr = new ArrayList<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int k=0;
-        for(char ch : st.toCharArray()){
-            map.put(ch-'0',k);
-            arr.add(ch-'0');
-            k++;
-        }
-        //System.out.println(map);
-        Collections.sort(arr, Collections.reverseOrder());
-        //System.out.print(arr);
-        for(int i=0;i<arr.size();i++){
-            if(st.charAt(i)-'0'<arr.get(i)){
-                //swap
-                return Integer.valueOf(swap(st,i,map.get(arr.get(i))));
+//         List<Integer> arr = new ArrayList<>();
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         int k=0;
+//         for(char ch : st.toCharArray()){
+//             map.put(ch-'0',k);
+//             arr.add(ch-'0');
+//             k++;
+//         }
+//         //System.out.println(map);
+//         Collections.sort(arr, Collections.reverseOrder());
+//         //System.out.print(arr);
+//         for(int i=0;i<arr.size();i++){
+//             if(st.charAt(i)-'0'<arr.get(i)){
+//                 //swap
+//                 return Integer.valueOf(swap(st,i,map.get(arr.get(i))));
+//             }
+//         }
+//         return Integer.valueOf(st);
+        
+        
+        
+        
+            String st = String.valueOf(num);
+            int arr[] = new int[st.length()];
+            int[] last = new int[10];
+           
+            for(int i=0;i<st.length();i++){
+                arr[i] = st.charAt(i)-'0';
+                last[arr[i]] =i;
             }
-        }
-        return Integer.valueOf(st);
+        
+            for(int i=0;i<arr.length;i++){
+                
+                for(int d=9;d>arr[i];d--){
+                    if(last[d]>i){
+                        //swap
+                        return Integer.valueOf(swap(st , i , last[d]));
+                    }
+                }
+            }
+            return Integer.valueOf(st);
+        
+        
+        
         
     }
     public String swap(String st , int indx , int pos){
