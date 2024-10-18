9db9e76861f9492764d5b0f059680523ac5803dd
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-10-18 20:25:47 +0530

    Time: 426 ms (5.03%), Space: 45.8 MB (17.86%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,19 +1,40 @@
 class MyCalendar {
 
-    TreeMap<Integer, Integer> calendar;
+    TreeMap<Integer, Integer> map;
 
     public MyCalendar() {
-        calendar = new TreeMap<>();
+        map = new TreeMap<>();
     }
 
     public boolean book(int start, int end) {
-        Integer floorKey = calendar.floorKey(start);
-        if (floorKey != null && calendar.get(floorKey) > start) return false;
-        Integer ceilingKey = calendar.ceilingKey(start);
-        if (ceilingKey != null && ceilingKey < end) return false;
+//         Integer floorKey = calendar.floorKey(start);
+//         System.out.println(floorKey);
+//         if (floorKey != null && calendar.get(floorKey) > start) return false;
+//         Integer ceilingKey = calendar.ceilingKey(start);
+//         if (ceilingKey != null && ceilingKey < end) return false;
 
-        calendar.put(start, end);
+//         calendar.put(start, end);
+//         return true;
+        
+        
+        // sweep line algo ::
+        map.put(start, map.getOrDefault(start,0)+1);
+        map.put(end, map.getOrDefault(end,0)-1);
+        int sum=0;
+        for(Map.Entry<Integer,Integer> itr :map.entrySet()){
+            sum += itr.getValue();
+            
+            if(sum>1){
+                map.put(start, map.get(start)-1);
+                map.put(end, map.get(end)+1);
+                
+                return false;
+            }
+        }
+        
         return true;
+        
+        
     }
 }
 
