25a5b50b6ece6b4fb90b4aa5ec807e7b9ac6ee55
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-10-18 21:24:12 +0530

    Time: 300 ms (5.09%), Space: 45.6 MB (36.32%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.java
+++ 0731-my-calendar-ii/0731-my-calendar-ii.java
@@ -1,22 +1,28 @@
 class MyCalendarTwo {
-    private List<int[]> books = new ArrayList<>();    
-    public boolean book(int s, int e) {
-        MyCalendar overlaps = new MyCalendar();
-        for (int[] b : books)
-            if (Math.max(b[0], s) < Math.min(b[1], e)) // overlap exist
-                if (!overlaps.book(Math.max(b[0], s), Math.min(b[1], e))) return false; // overlaps overlapped
-        books.add(new int[]{ s, e });
-        return true;
-    }
 
-    private static class MyCalendar {
-        List<int[]> books = new ArrayList<>();
-        public boolean book(int start, int end) {
-            for (int[] b : books)
-                if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
-            books.add(new int[]{ start, end });
-            return true;
+    TreeMap<Integer,Integer> map;
+    public MyCalendarTwo() {
+        map = new TreeMap<>();
+    }
+    
+    public boolean book(int start, int end) {
+        //sweep line algo:-
+        map.put(start , map.getOrDefault(start,0)+1);
+        map.put(end , map.getOrDefault(end,0)-1);
+        
+        int sum=0;
+        
+        for(Map.Entry<Integer,Integer> itr : map.entrySet()){
+             sum+=itr.getValue();
+             
+             if(sum>=3){
+                 map.put(start , map.getOrDefault(start,0)-1);
+                 map.put(end , map.getOrDefault(end,0)+1);
+                 
+                 return false;
+             }
         }
+        return true;
     }
 }
 
