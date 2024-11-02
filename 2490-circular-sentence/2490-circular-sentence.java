d309226bc02ae00a86f3502ed791ce774d30e445
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-11-02 23:53:37 +0530

    Time: 1 ms (96.81%), Space: 41.9 MB (54.87%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isCircularSentence(String s) {
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==' ')
+            {
+                if(s.charAt(i-1)!=s.charAt(i+1))
+                {
+                    return false;
+                }
+            }
+        }
+        return s.charAt(0) == s.charAt(s.length()-1);
+    }
+}
