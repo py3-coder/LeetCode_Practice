c4dba6c8d190c35c2b3bb0e63aeb1f1fda373c79
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-11-14 12:28:19 +0530

    Time: 116 ms (25.36%), Space: 56.7 MB (44.98%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,33 +1,37 @@
 class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
-        int maxi = Integer.MIN_VALUE;
+        
+        int maxi  = Integer.MIN_VALUE;
         for(int quantity : quantities){
-            maxi = Math.max(quantity , maxi) ;
+            maxi = Math.max(quantity,maxi);
         }
         
-        int ans=0;
-        int  l=1 , h = maxi;
-        while(l<=h){
-            int mid = l+(h-l)/2;
-            if(isPossible(n,mid,quantities)){
-                ans = mid;
-                h= mid-1;
+        //Edge Case ::
+        if(n == quantities.length){
+            return maxi ;
+        }
+  
+        int start = 1 ;
+        int end = maxi ;
+        int ans = Integer.MAX_VALUE;
+        while(start <= end ){
+            int mid = start +(end - start )/2;
+            if(isPossible(mid,quantities,n)){
+                ans = Math.min(mid , ans);
+                end = mid-1;
             }else{
-                l= mid+1;
+                start = mid+1;
             }
         }
         return ans ;
     }
     
-    public boolean isPossible(int n , int target , int[] quantities){
-        int cnt  = 0;
-        for(int ele : quantities){
-            if(ele%target == 0){
-                cnt += ele/target;
-            }else{
-                cnt += ele/target +1;
-            }
+    public static boolean isPossible(int mid , int[] quantities,int n){
+        int cnt=0;
+        for(int quantity : quantities){
+            cnt += Math.ceil(quantity*1.0/mid);
+            
         }
-        return cnt <= n ;
+        return cnt<=n;
     }
 }
