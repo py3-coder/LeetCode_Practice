bc1778db00e8aec37a5caa42eecdbf7fd5bb36a8
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-11-16 18:06:17 +0530

    Time: 1 ms (100.00%), Space: 45.5 MB (46.81%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,17 +1,38 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
+        // int n = nums.length;
+        // int start =0;
+        // int end = start+k-1;
+        // int[] res = new int[n-k+1]; 
+        // while(end<nums.length && start<n-k+1){
+        //     int sol = findMaxAndCheckSorted(nums, start , end);
+        //     res[start]=sol;
+        //     start++;
+        //     end++;
+        // }
+        // return res;
+        
+        
         int n = nums.length;
-        int start =0;
-        int end = start+k-1;
-        int[] res = new int[n-k+1]; 
-        while(end<nums.length && start<n-k+1){
-            int sol = findMaxAndCheckSorted(nums, start , end);
-            res[start]=sol;
-            start++;
-            end++;
-            
+        int[] p = new int[n];
+        for (int i = 1; i < n; i++)
+        {
+            p[i] = p[i - 1] + ((nums[i] == (nums[i - 1] + 1)) ? 1 : 0);
         }
-        return res;
+
+        var ans = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++)  
+        {
+            if (p[i + k - 1] == (p[i] + k - 1))
+            {
+                ans[i] = (nums[i + k - 1]);
+            }
+            else
+            {
+                ans[i] = -1;
+            }
+        }
+        return ans;
     }
     
     public int findMaxAndCheckSorted(int[] arr ,int start , int end){
