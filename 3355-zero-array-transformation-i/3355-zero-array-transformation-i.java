4a417a4784c746772fd4b1266c4c361cf9a8f186
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-11-18 14:39:20 +0530

    Time: 2 ms (100.00%), Space: 81.4 MB (100.00%) - LeetHub

--- 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
@@ -1,25 +1,23 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
         // Sweep Line ALgo :-
-        int n = nums.length;
+         int n = nums.length;
         
         int[] line = new int[n+1];
         
         for(int[] query : queries){
-            line[query[0]]--;
-            line[query[1]+1]++;
+            line[query[0]]++;
+            line[query[1]+1]--;
+        }
+        
+        if (line[0] < nums[0]) {
+            return false;
         }
         
         for(int i=1;i<n;i++){
             line[i] = line[i]+line[i-1];
-        }
             
-        for(int i=0;i<n;i++){
-            nums[i] = line[i]+nums[i];
-            if(nums[i]<0){
-                nums[i]=0;
-            }
-            if(nums[i]>0){
+            if(line[i]<nums[i]){
                 return false;
             }
         }
