541caa6f317b1d0b6cee08095e22fe77fc6c45af
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-11-19 16:43:38 +0530

    Time: 45 ms (52.96%), Space: 58.9 MB (36.28%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -2,20 +2,28 @@ class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
         //Sliding window ::
         int n = nums.length;
-        Set<Integer> set = new HashSet<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int start =0;
         int end =0;
         long sum=0, ans =0;
-        while(end<n){    
-            while (set.contains(nums[end]) || set.size() == k) {
-                set.remove(nums[start]);
-                sum -= nums[start++];
+        while(end<n){  
+            sum+=nums[end];
+            map.put(nums[end] , map.getOrDefault(nums[end],0)+1);
+            
+            while(end-start+1>k){
+                sum-=nums[start];
+                map.put(nums[start],map.get(nums[start])-1);
+                if(map.get(nums[start])==0){
+                     map.remove(nums[start]);
+                }
+               
+                start++;
             }
-            sum += nums[end];
-            set.add(nums[end]);
-            if (set.size() == k) {
-                ans = Math.max(ans, sum);
+            
+            if(end-start+1 == k && map.size()==k){
+                ans = Math.max(ans,sum);
             }
+            
             end++;
         }
         return ans;
