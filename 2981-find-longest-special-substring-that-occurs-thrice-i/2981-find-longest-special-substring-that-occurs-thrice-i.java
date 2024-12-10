b5e1e4c567def794c835eafa54f875047b05dca5
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-12-10 13:11:11 +0530

    Time: 4 ms (79.65%), Space: 43.5 MB (80.30%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,29 +1,73 @@
 class Solution {
     public int maximumLength(String s) {
+        // More optimal :
         
+        int[][] cnt = new int[26][3];
+        Arrays.stream(cnt).forEach(a->Arrays.fill(a,-1));
+        
+        char lastseen='*';
+        int winlen =0;
+        for(int i=0;i<s.length();i++){
+            if(lastseen == s.charAt(i)){
+                winlen +=1;
+            }else{
+                lastseen =s.charAt(i);
+                winlen=1;
+            }
+            findMin(s.charAt(i),winlen,cnt);
+        }
+        int mini=-1;
+        for(int i=0;i<26;i++){
+            mini = Math.max(mini ,findMinVal(i,cnt));
+        }
+        
+       
+        return mini;
         
         // TC : O(nlogn)
         // SC : O(1)
         
-        int n= s.length();
+//         int n= s.length();
         
-        //check for 1 size if not possible -1:
-        int start=1,end = n;
+//         //check for 1 size if not possible -1:
+//         int start=1,end = n;
         
-        if(!isPossible(1,s)) return -1;
+//         if(!isPossible(1,s)) return -1;
         
         
-        int ans=0;
-        while(start<=end){
-            int mid = (start+end) / 2;
-            if(isPossible(mid,s)){
-                ans = mid;
-                start = mid+1;
-            }else{
-                end=mid-1;
+//         int ans=0;
+//         while(start<end){
+//             int mid = (start+end) / 2;
+//             if(isPossible(mid,s)){
+//                 ans = mid;
+//                 start = mid+1;
+//             }else{
+//                 end=mid;
+//             }
+//         }
+//         return ans;
+    }
+    public void findMin(char ch , int winlen,int[][] cnt){
+        int findMini =Integer.MAX_VALUE,loc=-1;
+        int pos = ch-'a';
+        for(int i=0;i<3;i++){
+            if(findMini>cnt[pos][i]){
+                findMini = cnt[pos][i];
+                loc = i;
             }
         }
-        return ans;
+        if(cnt[pos][loc]<winlen){
+            cnt[pos][loc]=winlen;
+        }
+        
+        
+    }
+    public int findMinVal(int i,int[][] cnt){
+        int mini =Integer.MAX_VALUE;
+        for(int j=0;j<3;j++){
+           mini = Math.min(cnt[i][j] , mini);
+        }
+        return mini;
     }
     
     public boolean isPossible(int k , String s){
