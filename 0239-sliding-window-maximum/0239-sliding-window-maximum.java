45992981912e74a02bddd3b75cccd50d47eea1f9
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-12-10 19:21:34 +0530

    Time: 28 ms (89.85%), Space: 58.4 MB (72.47%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,34 +1,33 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] arr, int k) {
-        int n=arr.length;
-        if(n==1){
-            return new int[]{arr[0]};
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        //
+        int n = nums.length;
+        if(k==1){
+            return nums;
         }
-        int[] res = new int[n-k+1];
-        Deque<Integer> deque = new ArrayDeque<>();
-        int p=0;
-        int i=0,j=0;
-        while(j<n){
-            //remove all those who are lesser than current::
-            while(deque.size()>0 && deque.getLast()<arr[j]){
-                deque.removeLast();
-            }
-            //add at last
-            deque.offer(arr[j]);
-            //window size is lesss so j++;
-            if(j-i+1<k) j++;
-            else if(j-i+1==k){
-                //the first ele will be max for that window
-                res[p++]=deque.getFirst();
-                if(arr[i]==deque.getFirst()){
-                    deque.removeFirst();
-                }
-                i++;
-                j++;
-            }
-            
+        if(n==1){
+            return new int[]{0};
         }
         
+        int[] res = new int[n-k+1];
+        Deque<Integer> que = new ArrayDeque<>();
+        int start=0,end=0,pos=0;
+        
+        while(end<n){
+            while(que.size()>0 && que.getLast()<nums[end]){
+                que.removeLast();
+            }
+            que.offer(nums[end]);
+            if(end-start+1 == k ){
+                res[pos++] = que.getFirst();
+                
+                if(nums[start] == que.getFirst()){
+                    que.removeFirst();
+                }
+                start++;
+            }
+            end++;
+        }
         return res;
     }
 }
