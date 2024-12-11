17deedb35a7ac149bfb971f940c1aeda966a67f6
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-12-11 13:55:24 +0530

    Time: 3 ms (20.58%), Space: 42.2 MB (13.01%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,24 +1,44 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        // Line Sweep Algo.
+        // Sweep Line Algo.
         
-        int[] line = new int[2055];
+//         int[] line = new int[2055];
         
-        for(int[] log : logs){
-            line[log[0]]+=1;
-            line[log[1]]-=1;
+//         for(int[] log : logs){
+//             line[log[0]]+=1;
+//             line[log[1]+1]-=1;
+//         }
+        
+//         int maxi =-1;
+//         int year=1950;
+//         for(int i=1950;i<2055;i++){
+//             line[i]+=line[i-1];
+//             if(maxi<line[i]){
+//                 year=i;
+//                 maxi = line[i];
+//             }
+//         }
+//         return year ;
+        
+        
+        
+        // Tree Map Impl :
+        
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        
+        for(int[] log : logs ){
+            map.put(log[0], map.getOrDefault(log[0],0)+1);
+            map.put(log[1], map.getOrDefault(log[1],0)-1);
         }
         
-        int maxi =-1;
-        int year=1950;
-        for(int i=1950;i<2055;i++){
-            line[i]+=line[i-1];
-            if(maxi<line[i]){
-                //System.out.println(i+" "+line[i]);
-                year=i;
-                maxi=line[i];
+        int maxi =-1, year=0,pop=0;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            pop+=entry.getValue();
+            if(pop>maxi){
+                maxi = pop;
+                year = entry.getKey();
             }
         }
-        return year ;
+        return year;
     }
 }
