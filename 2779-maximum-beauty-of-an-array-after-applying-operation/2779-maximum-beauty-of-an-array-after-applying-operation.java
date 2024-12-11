dab9e583764fc53dab0513293c7afba1aa5ae88d
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-12-11 13:18:37 +0530

    Time: 39 ms (75.27%), Space: 60.4 MB (35.64%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,24 +1,43 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
-        int maxi=0;
+        // TC : nlogn
+        // SC : constant
         
-        for(int ele :nums){
-            maxi =Math.max(maxi , ele);
-        }
         
-        maxi +=k;
-        int[] line = new int[maxi+2];
+        Arrays.sort(nums);
+        int start=0,end=0,ans=0;
+        while(end<nums.length){
+            if(nums[end]-nums[start]>2*k){
+                start++;
+            }
+            ans = Math.max(end-start+1,ans);
+            
+            end++;
+        }
+        return ans;
         
-        for(int ele : nums){
-            int val = ele-k<0?0:ele-k;
-            line[val]++;
-            line[ele+k+1]--;
-        }
-        int cnt=0;
-        for(int i=1;i<=maxi;i++){
-            line[i]+=line[i-1];
-            cnt = Math.max(cnt , line[i]);
-        }
-        return cnt;
+        // TC : O(n)
+        // SC : O(max+k)
+        
+//         int maxi=0;
+        
+//         for(int ele :nums){
+//             maxi =Math.max(maxi , ele);
+//         }
+        
+//         maxi +=k;
+//         int[] line = new int[maxi+2];
+        
+//         for(int ele : nums){
+//             int val = ele-k<0?0:ele-k;
+//             line[val]++;
+//             line[ele+k+1]--;
+//         }
+//         int cnt=0;
+//         for(int i=1;i<=maxi;i++){
+//             line[i]+=line[i-1];
+//             cnt = Math.max(cnt , line[i]);
+//         }
+//         return cnt;
     }
 }
