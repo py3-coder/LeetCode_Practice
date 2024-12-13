2931fb34e1a03af8463fb2d363bf2eba0c47319a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-12-14 00:03:44 +0530

    Time: 1370 ms (5.04%), Space: 133 MB (5.59%) - LeetHub

--- 3347-maximum-frequency-of-an-element-after-performing-operations-ii/3347-maximum-frequency-of-an-element-after-performing-operations-ii.java
+++ 3347-maximum-frequency-of-an-element-after-performing-operations-ii/3347-maximum-frequency-of-an-element-after-performing-operations-ii.java
@@ -1,4 +1,27 @@
 class Solution {
+    // public int maxFrequency(int[] nums, int k, int numOperations) {
+    //     // MLE since 1 <= nums[i] <= 10^9 & 0 <= k <= 10^9
+    //     HashMap<Integer,Integer> freq = new HashMap<>();
+    //     int maxi = 0; 
+    //     for(int ele : nums){ 
+    //         freq.put(ele , freq.getOrDefault(ele,0)+1);
+    //         maxi = Math.max(ele , maxi) ;
+    //     } 
+    //     int[] line = new int[maxi+k+2];
+    //     for(int ele : nums){
+    //         int val = (ele-k)<0?0:ele-k;
+    //         line[val]++;
+    //         line[ele+k+1]--;
+    //     }
+    //     int ans =-1;
+    //     for(int i=1;i<line.length;i++){
+    //         line[i]+=line[i-1];
+    //         int frequency = freq.getOrDefault(i, 0);
+    //         ans = Math.max(ans ,frequency+Math.min(line[i]-frequency ,numOperations));
+    //     }
+    //     return ans;
+    // }
+    
     public int maxFrequency(int[] nums, int k, int numOperations) {
         HashMap<Integer,Integer> freq = new HashMap<>();
         TreeMap<Integer,Integer> map = new TreeMap<>();
